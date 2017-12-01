427d65a17ce56a217b8ad23a71ae351a536b5284
Andrii Braslavskyi <abraslavskyi@rubiconproject.com>
2017-12-01 18:20:09 +0200

    Support for Influxdb metrics report was added

--- src/main/java/org/rtb/vexing/metric/ReporterFactory.java
+++ src/main/java/org/rtb/vexing/metric/ReporterFactory.java
@@ -4,6 +4,9 @@ import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.ScheduledReporter;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
+import com.izettle.metrics.influxdb.InfluxDbHttpSender;
+import com.izettle.metrics.influxdb.InfluxDbReporter;
+import com.izettle.metrics.influxdb.InfluxDbSender;
 import org.apache.commons.lang3.StringUtils;
 import org.rtb.vexing.config.ApplicationConfig;
 
@@ -27,25 +30,42 @@ public class ReporterFactory {
             final String hostAndPort = config.getString("metrics.host");
             final String host = StringUtils.substringBefore(hostAndPort, ":");
             final int port = Integer.parseInt(StringUtils.substringAfter(hostAndPort, ":"));
-
+            final ScheduledReporter reporter;
             switch (ReporterType.valueOf(reporterType)) {
                 case graphite:
                     final Graphite graphite = new Graphite(host, port);
-                    final GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry)
+                    reporter = GraphiteReporter.forRegistry(metricRegistry)
                             .prefixedWith(config.getString("metrics.prefix"))
                             .build(graphite);
-                    reporter.start(config.getInteger("metrics.interval"), TimeUnit.SECONDS);
-
-                    return Optional.of(reporter);
+                    break;
+                case influxdb:
+                    final InfluxDbSender influxDbSender;
+                    try {
+                        influxDbSender = new InfluxDbHttpSender(config.getString("metrics.protocol"),
+                                host, port,
+                                config.getString("metrics.database"),
+                                config.getString("metrics.auth"),
+                                TimeUnit.SECONDS,
+                                config.getInteger("metrics.connectTimeout"),
+                                config.getInteger("metrics.readTimeout"),
+                                config.getString("metrics.prefix"));
+                    } catch (Exception e) {
+                        throw new IllegalArgumentException("Could not initialize influx http sender", e);
+                    }
+                    reporter = InfluxDbReporter.forRegistry(metricRegistry).build(influxDbSender);
+                    break;
                 default:
                     throw new IllegalStateException("Should never happen");
             }
+            reporter.start(config.getInteger("metrics.interval"), TimeUnit.SECONDS);
+            return Optional.of(reporter);
         }
 
         return Optional.empty();
     }
 
     private enum ReporterType {
-        graphite
+        graphite,
+        influxdb
     }
 }
