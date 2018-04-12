287d02535015a10fcc5c68fed543a906b3a32c94
Ruslan <rpanchyk@users.noreply.github.com>
2018-04-12 16:07:43 +0300

    Fixed Facebook bidder initialization (#13)

---
+++ src/test/java/org/prebid/server/bidder/DisabledBidderTest.java
@@ -0,0 +1,47 @@
+package org.prebid.server.bidder;
+
+import com.iab.openrtb.request.BidRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class DisabledBidderTest {
+
+    private DisabledBidder disabledBidder;
+
+    @Before
+    public void setUp() {
+        disabledBidder = new DisabledBidder("error message");
+    }
+
+    @Test
+    public void makeHttpRequestsShouldRespondWithExpectedError() {
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = disabledBidder.makeHttpRequests(null);
+
+        // then
+        assertThat(result.getValue()).isEmpty();
+        assertThat(result.getErrors()).hasSize(1)
+                .extracting(BidderError::getMessage)
+                .containsOnly("error message");
+    }
+
+    @Test
+    public void makeBidsShouldBeUnsupported() {
+        assertThatExceptionOfType(UnsupportedOperationException.class)
+                .isThrownBy(() -> disabledBidder.makeBids(null, null));
+    }
+
+    @Test
+    public void extractTargetingShouldBeUnsupported() {
+        assertThatExceptionOfType(UnsupportedOperationException.class)
+                .isThrownBy(() -> disabledBidder.extractTargeting(null));
+    }
+}
