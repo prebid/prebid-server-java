d51fd4218762f6d3bd498f56093c1f384bc43948
Ruslan Panchyk <rpanchyk@rubiconproject.com>
2018-04-18 15:15:38 +0300

    Merge branch 'master' into add-multiple-backends-for-settings

--- src/test/java/org/prebid/server/validation/BidderParamValidatorTest.java
+++ src/test/java/org/prebid/server/validation/BidderParamValidatorTest.java
@@ -9,9 +9,6 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.prebid.server.VertxTest;
 import org.prebid.server.bidder.BidderCatalog;
-import org.prebid.server.bidder.openx.proto.ExtImpOpenx;
-import org.prebid.server.bidder.sovrn.proto.ExtImpSovrn;
-import org.prebid.server.bidder.facebook.proto.ExtImpFacebook;
 import org.prebid.server.proto.openrtb.ext.request.adform.ExtImpAdform;
 import org.prebid.server.proto.openrtb.ext.request.adtelligent.ExtImpAdtelligent;
 import org.prebid.server.proto.openrtb.ext.request.appnexus.ExtImpAppnexus;
@@ -26,9 +23,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static java.util.Arrays.*;
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
 
 public class BidderParamValidatorTest extends VertxTest {
 
