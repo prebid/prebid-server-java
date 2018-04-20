572fabf6a27f9fe52299d5a4b04b17f2280b3b6c
Bret Gorsline <bgorsline@rubiconproject.com>
2018-04-20 12:04:20 -0400

    re-adding for review

---
+++ src/main/java/org/prebid/server/bidder/conversant/ConversantBidder.java
@@ -0,0 +1,34 @@
+package org.prebid.server.bidder.conversant;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.iab.openrtb.request.BidRequest;
+import org.prebid.server.bidder.Bidder;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.HttpCall;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Conversant {@link Bidder} implementation.
+ */
+public class ConversantBidder implements Bidder {
+
+    @Override
+    public Result<List<HttpRequest>> makeHttpRequests(BidRequest bidRequest) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Result<List<BidderBid>> makeBids(HttpCall httpCall, BidRequest bidRequest) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Map<String, String> extractTargeting(ObjectNode ext) {
+        return Collections.emptyMap();
+    }
+}
