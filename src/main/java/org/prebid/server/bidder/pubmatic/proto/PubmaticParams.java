c58e3d273585c2fdde4c0161064d0e789b754104
Bret Gorsline <bgorsline@rubiconproject.com>
2018-04-20 11:58:34 -0400

    re-added for review

---
+++ src/main/java/org/prebid/server/bidder/pubmatic/proto/PubmaticParams.java
@@ -0,0 +1,16 @@
+package org.prebid.server.bidder.pubmatic.proto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@AllArgsConstructor(staticName = "of")
+@Value
+public class PubmaticParams {
+
+    @JsonProperty("publisherId")
+    String publisherId;
+
+    @JsonProperty("adSlot")
+    String adSlot;
+}
