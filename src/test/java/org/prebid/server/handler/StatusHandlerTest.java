42bee5ad7052eaca5e07b81d71c3f2d4931ef48a
Andrii Braslavskyi <abraslavskyi@rubiconproject.com>
2018-05-07 11:33:09 +0300

    configured status response message implemented

--- src/test/java/org/prebid/server/handler/StatusHandlerTest.java
+++ src/test/java/org/prebid/server/handler/StatusHandlerTest.java
@@ -8,11 +8,10 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.only;
 import static org.mockito.Mockito.verify;
-
 
 public class StatusHandlerTest {
 
@@ -24,15 +23,31 @@ public class StatusHandlerTest {
     @Mock
     private HttpServerResponse httpResponse;
 
+    private StatusHandler statusHandler;
+
     @Test
-    public void shouldRespondReadyAndHttp200Ok() {
+    public void shouldRespondHttp200OkWithMessage() {
         // given
+        statusHandler = new StatusHandler("Response message");
         given(routingContext.response()).willReturn(httpResponse);
 
         // when
-        new StatusHandler().handle(routingContext);
+        statusHandler.handle(routingContext);
 
         // then
-        verify(httpResponse, only()).end(eq("ready"));
+        verify(httpResponse, only()).end(eq("Response message"));
+    }
+
+    @Test
+    public void shouldRespondWithNoContentWhenMessageWasNotDefined() {
+        statusHandler = new StatusHandler(null);
+        given(routingContext.response()).willReturn(httpResponse);
+        given(httpResponse.setStatusCode(eq(204))).willReturn(httpResponse);
+
+        // when
+        statusHandler.handle(routingContext);
+
+        // then
+        verify(httpResponse).setStatusCode(eq(204));
     }
 }
