0fb2acf87b348a377b04338826237d4c626d9dac
Sergii Chernysh <schernysh@rubiconproject.com>
2018-06-13 14:05:50 +0300

    Fixed startup

--- src/main/java/org/prebid/server/Application.java
+++ src/main/java/org/prebid/server/Application.java
@@ -7,16 +7,12 @@ import io.vertx.core.Verticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.prebid.server.spring.config.DeployVerticleException;
-import org.prebid.server.spring.config.SettingsConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.event.EventListener;
 
+import javax.annotation.PostConstruct;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -26,16 +22,16 @@ public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-    private final ApplicationContext applicationContext;
+    private final PrebidVerticle prebidVerticle;
     private final Vertx vertx;
     private final int verticleInstances;
     private final long verticleDeployTimeout;
 
     @Autowired
-    public Application(ApplicationContext applicationContext, Vertx vertx,
+    public Application(PrebidVerticle prebidVerticle, Vertx vertx,
                        @Value("${vertx.verticle.instances}") int verticleInstances,
                        @Value("${vertx.verticle.deploy-timeout-ms}") long verticleDeployTimeout) {
-        this.applicationContext = applicationContext;
+        this.prebidVerticle = prebidVerticle;
         this.vertx = vertx;
         this.verticleInstances = verticleInstances;
         this.verticleDeployTimeout = verticleDeployTimeout;
@@ -45,24 +41,25 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @EventListener
-    public void deployVerticles(ApplicationReadyEvent event) {
-        deployVerticle(verticleInstances, PrebidVerticle.class);
+    @PostConstruct
+    public void deployVerticles() {
+        deployVerticle(verticleInstances, prebidVerticle);
 
         // skip deploy if bean doesn't exist in application context
-        if (!applicationContext.getBeansOfType(SettingsConfiguration.CacheNotificationConfiguration.class).isEmpty()) {
-            deployVerticle(1, CacheNotificationVerticle.class);
-        }
+        //        if (!prebidVerticle.getBeansOfType(SettingsConfiguration.CacheNotificationConfiguration.class)
+        // .isEmpty()) {
+        //            deployVerticle(1, CacheNotificationVerticle.class);
+        //        }
     }
 
-    private void deployVerticle(int numInstances, Class<? extends Verticle> verticleClass) {
-        final String verticleName = verticleClass.getSimpleName();
+    private void deployVerticle(int numInstances, Verticle verticle) {
+        final String verticleName = verticle.getClass().getSimpleName();
 
         logger.info("Prebid-server is starting {0} instances of {1}", numInstances, verticleName);
 
         runOnNewVertxContext(numInstances, future -> {
             try {
-                applicationContext.getBean(verticleClass).start(future);
+                verticle.start(future);
             } catch (Exception e) {
                 // should never happen
             }
@@ -79,6 +76,7 @@ public class Application {
             final Context context = vertx.getOrCreateContext();
             context.runOnContext(v -> action.handle(Future.<Void>future().setHandler(ar -> {
                 if (ar.failed()) {
+                    logger.fatal("Fatal error occurred while running startup action", ar.cause());
                     actionFailed.compareAndSet(false, true);
                 }
                 completionLatch.countDown();
@@ -87,13 +85,13 @@ public class Application {
 
         try {
             if (!completionLatch.await(verticleDeployTimeout, TimeUnit.MILLISECONDS)) {
-                throw new DeployVerticleException(
+                throw new RuntimeException(
                         "Prebid-server failed due to timeout while waiting for verticle deployments");
             } else if (actionFailed.get()) {
-                throw new DeployVerticleException("Prebid-server failed while deploying verticles");
+                throw new RuntimeException("Prebid-server failed while deploying verticles");
             }
         } catch (InterruptedException e) {
-            throw new DeployVerticleException(e);
+            throw new RuntimeException(e);
         }
     }
 }
