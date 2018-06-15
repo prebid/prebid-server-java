0df48741e7fb9967a988929b39a53afffa24f2b4
jordillachmrf <jordi.llach@marfeel.com>
2018-06-15 09:15:43 +0200

    Being able to extract user's cookie sync status with each bidder
    - Related issue https://github.com/rubicon-project/prebid-server-java/issues/74
    - As per discussed in the original PR https://github.com/rubicon-project/prebid-server-java/pull/75 we adapt BidResponsePostProcessor

--- src/main/java/org/prebid/server/auction/BidResponsePostProcessor.java
+++ src/main/java/org/prebid/server/auction/BidResponsePostProcessor.java
@@ -3,6 +3,8 @@ package org.prebid.server.auction;
 import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.response.BidResponse;
 import io.vertx.core.Future;
+import org.prebid.server.cookie.UidsCookie;
+import org.prebid.server.cookie.proto.Uids;
 
 /**
  * A hook that is pulled once auction has been held. It allows companies that host Prebid Server to define and apply
@@ -13,11 +15,12 @@ public interface BidResponsePostProcessor {
     /**
      * This method is called when auction is finished.
      *
-     * @param bidRequest  original auction request
+     * @param bidRequest original auction request
+     * @param uidsCookie auction request {@link Uids} container
      * @param bidResponse auction result
      * @return a {@link Future} with (possibly modified) auction result
      */
-    Future<BidResponse> postProcess(BidRequest bidRequest, BidResponse bidResponse);
+    Future<BidResponse> postProcess(BidRequest bidRequest, UidsCookie uidsCookie, BidResponse bidResponse);
 
     /**
      * Returns {@link NoOpBidResponsePostProcessor} instance that just does nothing to original auction result.
@@ -31,7 +34,7 @@ public interface BidResponsePostProcessor {
      */
     class NoOpBidResponsePostProcessor implements BidResponsePostProcessor {
         @Override
-        public Future<BidResponse> postProcess(BidRequest bidRequest, BidResponse bidResponse) {
+        public Future<BidResponse> postProcess(BidRequest bidRequest, UidsCookie uidsCookie, BidResponse bidResponse) {
             return Future.succeededFuture(bidResponse);
         }
     }
