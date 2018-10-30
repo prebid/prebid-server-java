51f732d790cbd3a7929ec52b5c19ed59fc2d6979
rgoncharuk <rgoncharuk@rubiconproject.com>
2018-10-30 13:43:28 +0200

    Added AdkernelAdn bidder

--- src/test/java/org/prebid/server/bidder/brightroll/BrightrollUsersyncerTest.java
+++ src/test/java/org/prebid/server/bidder/brightroll/BrightrollUsersyncerTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BrightrollUsersyncerTest {
 
     @Test
-    public void creationShouldInitExpectedUsercyncInfo() {
+    public void creationShouldInitExpectedUsersyncInfo() {
         assertThat(new BrightrollUsersyncer("//usersync.org/", "http://external.org/").usersyncInfo())
                 .isEqualTo(UsersyncInfo.of(
                         "//usersync.org/http%3A%2F%2Fexternal.org%2F%2Fsetuid%3Fbidder%3Dbrightroll"
