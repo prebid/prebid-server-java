616e26c5602929d361693abe36cd08921adba1bf
rpanchyk <rpanchyk@rubiconproject.com>
2018-11-07 16:00:28 +0200

    Fix CookieSyncHandler to check Host Vendor ID against GDPR - fix by review

--- src/test/java/org/prebid/server/ApplicationTest.java
+++ src/test/java/org/prebid/server/ApplicationTest.java
@@ -41,7 +41,13 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToIgnoreCase;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
