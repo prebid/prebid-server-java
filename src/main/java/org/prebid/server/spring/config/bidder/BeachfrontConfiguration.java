ff987765def3790d00eb09efe85af8c801293ce4
Sergii Chernysh <schernysh@rubiconproject.com>
2018-12-24 12:02:13 +0200

    Simplify Beachfront bidder configuration - use POJO instead of raw properties

--- src/main/java/org/prebid/server/spring/config/bidder/BeachfrontConfiguration.java
+++ src/main/java/org/prebid/server/spring/config/bidder/BeachfrontConfiguration.java
@@ -1,5 +1,7 @@
 package org.prebid.server.spring.config.bidder;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.prebid.server.bidder.Adapter;
 import org.prebid.server.bidder.Bidder;
 import org.prebid.server.bidder.BidderDeps;
@@ -9,11 +11,16 @@ import org.prebid.server.bidder.beachfront.BeachfrontBidder;
 import org.prebid.server.bidder.beachfront.BeachfrontMetaInfo;
 import org.prebid.server.bidder.beachfront.BeachfrontUsersyncer;
 import org.prebid.server.spring.env.YamlPropertySourceFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Configuration
@@ -22,29 +29,15 @@ public class BeachfrontConfiguration extends BidderConfiguration {
 
     private static final String BIDDER_NAME = "beachfront";
 
-    @Value("${adapters.beachfront.enabled}")
-    private boolean enabled;
+    @Autowired
+    @Qualifier("beachfrontConfigurationProperties")
+    private BeachfrontConfigurationProperties configProperties;
 
-    @Value("${adapters.beachfront.banner-endpoint}")
-    private String bannerEndpoint;
-
-    @Value("${adapters.beachfront.video-endpoint}")
-    private String videoEndpoint;
-
-    @Value("${adapters.beachfront.usersync-url}")
-    private String usersyncUrl;
-
-    @Value("${adapters.beachfront.platform-id}")
-    private String platformId;
-
-    @Value("${adapters.beachfront.pbs-enforces-gdpr}")
-    private boolean pbsEnforcesGdpr;
-
-    @Value("${adapters.beachfront.deprecated-names}")
-    private List<String> deprecatedNames;
-
-    @Value("${adapters.beachfront.aliases}")
-    private List<String> aliases;
+    @Bean("beachfrontConfigurationProperties")
+    @ConfigurationProperties("adapters.beachfront")
+    BeachfrontConfigurationProperties configurationProperties() {
+        return new BeachfrontConfigurationProperties();
+    }
 
     @Bean
     BidderDeps beachfrontBidderDeps() {
@@ -58,27 +51,27 @@ public class BeachfrontConfiguration extends BidderConfiguration {
 
     @Override
     protected List<String> deprecatedNames() {
-        return deprecatedNames;
+        return configProperties.getDeprecatedNames();
     }
 
     @Override
     protected List<String> aliases() {
-        return aliases;
+        return configProperties.getAliases();
     }
 
     @Override
     protected MetaInfo createMetaInfo() {
-        return new BeachfrontMetaInfo(enabled, pbsEnforcesGdpr);
+        return new BeachfrontMetaInfo(configProperties.getEnabled(), configProperties.getPbsEnforcesGdpr());
     }
 
     @Override
     protected Usersyncer createUsersyncer() {
-        return new BeachfrontUsersyncer(usersyncUrl, platformId);
+        return new BeachfrontUsersyncer(configProperties.getUsersyncUrl(), configProperties.getPlatformId());
     }
 
     @Override
     protected Bidder<?> createBidder(MetaInfo metaInfo) {
-        return new BeachfrontBidder(bannerEndpoint, videoEndpoint);
+        return new BeachfrontBidder(configProperties.getBannerEndpoint(), configProperties.getVideoEndpoint());
     }
 
     @Override
@@ -86,4 +79,33 @@ public class BeachfrontConfiguration extends BidderConfiguration {
         return null;
     }
 
+    @Validated
+    @Data
+    @NoArgsConstructor
+    private static class BeachfrontConfigurationProperties {
+
+        @NotNull
+        private Boolean enabled;
+
+        @NotBlank
+        private String bannerEndpoint;
+
+        @NotBlank
+        private String videoEndpoint;
+
+        @NotBlank
+        private String platformId;
+
+        @NotBlank
+        private String usersyncUrl;
+
+        @NotNull
+        private Boolean pbsEnforcesGdpr;
+
+        @NotNull
+        private List<String> deprecatedNames;
+
+        @NotNull
+        private List<String> aliases;
+    }
 }
