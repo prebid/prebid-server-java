f7ecf305326e20c52b016cb75d83f25e3ade53dc
Dmitriy <DimaGarbar49@gmail.com>
2019-07-16 11:24:42 +0300

    Extract privacy enforcement (#398)

---
+++ src/main/java/org/prebid/server/auction/model/PrivacyEnforcementResult.java
@@ -0,0 +1,19 @@
+package org.prebid.server.auction.model;
+
+import com.iab.openrtb.request.Device;
+import com.iab.openrtb.request.Regs;
+import com.iab.openrtb.request.User;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@AllArgsConstructor(staticName = "of")
+@Value
+public class PrivacyEnforcementResult {
+
+    User user;
+
+    Device device;
+
+    Regs regs;
+}
+
