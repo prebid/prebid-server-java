61831eac21b99dbb19724526cef7bab38c37fb83
DGarbar <dimagarbar49@gmail.com>
2019-07-26 13:23:07 +0300

    Add test for empty inventory and visitor

--- src/test/java/org/prebid/server/bidder/rubicon/RubiconBidderTest.java
+++ src/test/java/org/prebid/server/bidder/rubicon/RubiconBidderTest.java
@@ -336,6 +336,29 @@ public class RubiconBidderTest extends VertxTest {
     }
 
     @Test
+    public void makeHttpRequestsShouldNotFillUserExtRpWhenVisitorAndInventoryIsEmpty() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(
+                builder -> builder.user(User.builder().id("id").build()),
+                builder -> builder.video(Video.builder().build()),
+                builder -> {
+                    builder.visitor(mapper.valueToTree("{}"));
+                    builder.inventory(mapper.valueToTree("{}"));
+                    return builder;
+                });
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = rubiconBidder.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).hasSize(1).doesNotContainNull()
+                .extracting(httpRequest -> mapper.readValue(httpRequest.getBody(), BidRequest.class))
+                .extracting(BidRequest::getUser).doesNotContainNull()
+                .containsOnly(User.builder().id("id").build());
+    }
+
+    @Test
     public void makeHttpRequestsShouldFillUserExtIfUserAndDigiTrustPresent() {
         // given
         final BidRequest bidRequest = givenBidRequest(
