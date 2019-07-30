25234cf8e9eaaf5258e24044e4c21d71f4ece95e
rgoncaruk <rgoncharuk@rubiconproject.com>
2019-07-30 12:55:52 +0300

    Fix ext model json property

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/ExtPublisher.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/ExtPublisher.java
@@ -1,5 +1,6 @@
 package org.prebid.server.proto.openrtb.ext.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
@@ -14,5 +15,6 @@ public class ExtPublisher {
      * parentAccount would define the legal entity (publisher owner or network) that has the direct relationship
      * with the PBS host. As such, the definition depends on the PBS hosting entity.
      */
+    @JsonProperty("parentAccount")
     String parentAccount;
 }
