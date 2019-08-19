927ddadb3a1d0ef1a48574f28abe251f85213472
rgoncaruk <rgoncharuk@rubiconproject.com>
2019-08-19 16:02:34 +0300

    Implement VerizonMedia bidder

---
+++ src/test/java/org/prebid/server/it/VerizonmediaTest.java
@@ -0,0 +1,54 @@
+package org.prebid.server.it;
+
+import io.restassured.response.Response;
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static io.restassured.RestAssured.given;
+import static java.util.Collections.singletonList;
+
+@RunWith(SpringRunner.class)
+public class VerizonmediaTest extends IntegrationTest {
+
+    @Test
+    public void openrtb2AuctionShouldRespondWithBidsFromVerizonmedia() throws IOException, JSONException {
+        // given
+        // Verizonmedia bid response for imp 001
+        wireMockRule.stubFor(post(urlPathEqualTo("/verizonmedia-exchange"))
+                .withRequestBody(equalToJson(jsonFrom("openrtb2/verizonmedia/test-verizonmedia-bid-request-1.json")))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/verizonmedia/test-verizonmedia-bid-response-1.json"))));
+
+        // pre-bid cache
+        wireMockRule.stubFor(post(urlPathEqualTo("/cache"))
+                .withRequestBody(equalToJson(jsonFrom("openrtb2/verizonmedia/test-cache-verizonmedia-request.json")))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/verizonmedia/test-cache-verizonmedia-response.json"))));
+
+        // when
+        final Response response = given(spec)
+                .header("Referer", "http://www.example.com")
+                .header("X-Forwarded-For", "192.168.244.1")
+                .header("User-Agent", "userAgent")
+                .header("Origin", "http://www.example.com")
+                // this uids cookie value stands for {"uids":{"verizon":"VM-UID"}}
+                .cookie("uids", "eyJ1aWRzIjp7InZlcml6b24iOiJWTS1VSUQifX0=")
+                .body(jsonFrom("openrtb2/verizonmedia/test-auction-verizonmedia-request.json"))
+                .post("/openrtb2/auction");
+
+        // then
+        final String expectedAuctionResponse = openrtbAuctionResponseFrom(
+                "openrtb2/verizonmedia/test-auction-verizonmedia-response.json",
+                response, singletonList("verizonmedia"));
+
+        JSONAssert.assertEquals(expectedAuctionResponse, response.asString(), JSONCompareMode.NON_EXTENSIBLE);
+    }
+}
