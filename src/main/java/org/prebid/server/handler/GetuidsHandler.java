1ccca1e6efd1c854298cb6d88081daebe70a5bf9
rpanchyk <rpanchyk@rubiconproject.com>
2019-08-29 14:50:39 +0300

    Improve handling "Response is closed" error

--- src/main/java/org/prebid/server/handler/GetuidsHandler.java
+++ src/main/java/org/prebid/server/handler/GetuidsHandler.java
@@ -3,6 +3,8 @@ package org.prebid.server.handler;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.vertx.core.Handler;
 import io.vertx.core.json.Json;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
 import lombok.AllArgsConstructor;
 import lombok.Value;
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 
 public class GetuidsHandler implements Handler<RoutingContext> {
 
+    private static final Logger logger = LoggerFactory.getLogger(GetuidsHandler.class);
+
     private final UidsCookieService uidsCookieService;
 
     public GetuidsHandler(UidsCookieService uidsCookieService) {
@@ -22,14 +26,21 @@ public class GetuidsHandler implements Handler<RoutingContext> {
     }
 
     @Override
-    public void handle(RoutingContext routingContext) {
-        final UidsCookie uidsCookie = uidsCookieService.parseFromRequest(routingContext);
+    public void handle(RoutingContext context) {
+        final UidsCookie uidsCookie = uidsCookieService.parseFromRequest(context);
         final Map<String, String> uids = uidsCookie.getCookieUids().getUids().entrySet().stream()
                 .collect(Collectors.toMap(Map.Entry::getKey,
                         // Extract just the uid for each bidder
                         uidEntry -> uidEntry.getValue().getUid()));
 
-        routingContext.response().end(Json.encode(BuyerUids.of(uids)));
+        final String body = Json.encode(BuyerUids.of(uids));
+
+        // don't send the response if client has gone
+        if (context.response().closed()) {
+            logger.warn("The client already closed connection, response will be skipped");
+            return;
+        }
+        context.response().end(body);
     }
 
     @AllArgsConstructor(staticName = "of")
