77cd71481e3105343ea1729bc5f7c6c5fdeddc0f
rgoncharuk <rgoncharuk@rubiconproject.com>
2019-09-24 19:06:57 +0300

    Update The MediaGrid bidder to add a video support

--- src/main/java/org/prebid/server/bidder/grid/GridBidder.java
+++ src/main/java/org/prebid/server/bidder/grid/GridBidder.java
@@ -1,10 +1,31 @@
 package org.prebid.server.bidder.grid;
 
+import com.iab.openrtb.request.Imp;
 import org.prebid.server.bidder.OpenrtbBidder;
+import org.prebid.server.exception.PreBidException;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
+
+import java.util.List;
 
 public class GridBidder extends OpenrtbBidder<Void> {
 
     public GridBidder(String endpointUrl) {
         super(endpointUrl, RequestCreationStrategy.SINGLE_REQUEST, Void.class);
+    }
+
+    @Override
+    protected BidType getBidType(String impId, List<Imp> imps) {
+        for (Imp imp : imps) {
+            if (imp.getId().equals(impId)) {
+                if (imp.getBanner() != null) {
+                    return BidType.banner;
+                }
+                if (imp.getVideo() != null) {
+                    return BidType.video;
+                }
+                throw new PreBidException(String.format("Unknown impression type for ID: %s", impId));
+            }
+        }
+        throw new PreBidException(String.format("Failed to find impression for ID: %s", impId));
     }
 }
