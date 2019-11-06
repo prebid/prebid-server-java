6fcb43a9a40b570c5dfa0813cc40ef9aef92159e
Dmitriy <DimaGarbar49@gmail.com>
2019-11-06 07:56:41 +0200

    Sharethrough blocked advertiser domains (#496)

--- src/main/java/org/prebid/server/bidder/sharethrough/SharethroughBidder.java
+++ src/main/java/org/prebid/server/bidder/sharethrough/SharethroughBidder.java
@@ -9,12 +9,14 @@ import com.iab.openrtb.response.Bid;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.Json;
+import org.apache.commons.lang3.BooleanUtils;
 import org.prebid.server.bidder.Bidder;
 import org.prebid.server.bidder.model.BidderBid;
 import org.prebid.server.bidder.model.BidderError;
 import org.prebid.server.bidder.model.HttpCall;
 import org.prebid.server.bidder.model.HttpRequest;
 import org.prebid.server.bidder.model.Result;
+import org.prebid.server.bidder.sharethrough.model.SharethroughRequestBody;
 import org.prebid.server.bidder.sharethrough.model.Size;
 import org.prebid.server.bidder.sharethrough.model.StrUriParameters;
 import org.prebid.server.bidder.sharethrough.model.UserInfo;
@@ -26,19 +28,24 @@ import org.prebid.server.proto.openrtb.ext.response.BidType;
 import org.prebid.server.util.HttpUtil;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class SharethroughBidder implements Bidder<Void> {
+public class SharethroughBidder implements Bidder<SharethroughRequestBody> {
 
     private static final String VERSION = "4";
     private static final String SUPPLY_ID = "FGMrCMMc";
     private static final String DEFAULT_BID_CURRENCY = "USD";
     private static final BidType DEFAULT_BID_TYPE = BidType.xNative;
+    private static final Long DEFAULT_TIMEOUT = 10000L;
+    private static final Date TEST_TIME = new Date(1604455678999L);
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
 
     private static final TypeReference<ExtPrebid<?, ExtImpSharethrough>> SHARETHROUGH_EXT_TYPE_REFERENCE =
             new TypeReference<ExtPrebid<?, ExtImpSharethrough>>() {
@@ -55,7 +62,7 @@ public class SharethroughBidder implements Bidder<Void> {
      * Creates POST http request with all parameters in url and headers with empty body.
      */
     @Override
-    public Result<List<HttpRequest<Void>>> makeHttpRequests(BidRequest request) {
+    public Result<List<HttpRequest<SharethroughRequestBody>>> makeHttpRequests(BidRequest request) {
         final String page = SharethroughRequestUtil.getPage(request.getSite());
 
         // site.page validation is already performed by {@link RequestValidator#validate}
@@ -70,12 +77,14 @@ public class SharethroughBidder implements Bidder<Void> {
             return Result.emptyWithError(BidderError.badInput(
                     String.format("Error occurred parsing sharethrough parameters %s", e.getMessage())));
         }
-
+        final boolean test = Objects.equals(request.getTest(), 1);
+        final Date date = test ? TEST_TIME : new Date();
         final MultiMap headers = makeHeaders(request.getDevice(), page);
-        final List<HttpRequest<Void>> httpRequests = strUriParameters.stream()
+        final SharethroughRequestBody body = makeBody(request, date, test);
+        final List<HttpRequest<SharethroughRequestBody>> httpRequests = strUriParameters.stream()
                 .map(strUriParameter -> SharethroughUriBuilderUtil.buildSharethroughUrl(
-                        endpointUrl, SUPPLY_ID, VERSION, strUriParameter))
-                .map(uri -> makeHttpRequest(uri, headers))
+                        endpointUrl, SUPPLY_ID, VERSION, DATE_FORMAT.format(date), strUriParameter))
+                .map(uri -> makeHttpRequest(uri, headers, body))
                 .collect(Collectors.toList());
 
         return Result.of(httpRequests, Collections.emptyList());
@@ -127,7 +136,7 @@ public class SharethroughBidder implements Bidder<Void> {
     /**
      * Make Headers for request.
      */
-    private MultiMap makeHeaders(Device device, String page) {
+    private static MultiMap makeHeaders(Device device, String page) {
         final MultiMap headers = HttpUtil.headers()
                 .add("Origin", SharethroughRequestUtil.getHost(page))
                 .add("Referer", page);
@@ -143,21 +152,31 @@ public class SharethroughBidder implements Bidder<Void> {
         return headers;
     }
 
+    private static SharethroughRequestBody makeBody(BidRequest request, Date date, boolean test) {
+        Long tmax = request.getTmax();
+        if (tmax == null) {
+            tmax = DEFAULT_TIMEOUT;
+        }
+        final Date deadLine = new Date(date.getTime() + tmax);
+        return SharethroughRequestBody.of(request.getBadv(), tmax, DATE_FORMAT.format(deadLine), test);
+    }
+
     /**
      * Make {@link HttpRequest} from uri and headers.
      */
-    private HttpRequest<Void> makeHttpRequest(String uri, MultiMap headers) {
-        return HttpRequest.<Void>builder()
+    private static HttpRequest<SharethroughRequestBody> makeHttpRequest(String uri, MultiMap headers,
+                                                                        SharethroughRequestBody body) {
+        return HttpRequest.<SharethroughRequestBody>builder()
                 .method(HttpMethod.POST)
                 .uri(uri)
-                .body(null)
+                .body(Json.encode(body))
                 .headers(headers)
-                .payload(null)
+                .payload(body)
                 .build();
     }
 
     @Override
-    public Result<List<BidderBid>> makeBids(HttpCall<Void> httpCall, BidRequest bidRequest) {
+    public Result<List<BidderBid>> makeBids(HttpCall<SharethroughRequestBody> httpCall, BidRequest bidRequest) {
         try {
             final String responseBody = httpCall.getResponse().getBody();
             final ExtImpSharethroughResponse sharethroughBid = Json.mapper.readValue(responseBody,
@@ -173,7 +192,7 @@ public class SharethroughBidder implements Bidder<Void> {
      * Converts {@link ExtImpSharethroughResponse} to {@link List} of {@link BidderBid}.
      */
     private List<BidderBid> toBidderBid(String responseBody, ExtImpSharethroughResponse sharethroughBid,
-                                        HttpRequest request) {
+                                        HttpRequest<SharethroughRequestBody> request) {
         if (sharethroughBid.getCreatives().isEmpty()) {
             throw new IllegalArgumentException("No creative provided");
         }
@@ -181,7 +200,9 @@ public class SharethroughBidder implements Bidder<Void> {
         final StrUriParameters strUriParameters = SharethroughUriBuilderUtil
                 .buildSharethroughUrlParameters(request.getUri());
 
-        final String adMarkup = SharethroughMarkupUtil.getAdMarkup(responseBody, sharethroughBid, strUriParameters);
+        final Date date = BooleanUtils.toBoolean(request.getPayload().getTest()) ? TEST_TIME : new Date();
+        final String adMarkup = SharethroughMarkupUtil.getAdMarkup(responseBody, sharethroughBid, strUriParameters,
+                date);
 
         final ExtImpSharethroughCreative creative = sharethroughBid.getCreatives().get(0);
         return Collections.singletonList(
