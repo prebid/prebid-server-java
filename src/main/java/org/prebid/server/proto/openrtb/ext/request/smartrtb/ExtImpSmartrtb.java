64de8ca9af47047e360e8827816b6186f3212786
apavlyuk <apavlyuk@rubiconproject.com>
2020-01-31 18:26:25 +0200

    Small changes to ExtImpSmartrtb and ExtRequestSmartrtb

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/smartrtb/ExtImpSmartrtb.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/smartrtb/ExtImpSmartrtb.java
@@ -1,6 +1,5 @@
 package org.prebid.server.proto.openrtb.ext.request.smartrtb;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
@@ -8,15 +7,11 @@ import lombok.Value;
 @Value
 public class ExtImpSmartrtb {
 
-    @JsonProperty("pub_id")
     String pubId;
 
-    @JsonProperty("med_id")
     String medId;
 
-    @JsonProperty("zone_id")
     String zoneId;
 
-    @JsonProperty("force_bid")
     Boolean forceBid;
 }
--- src/main/java/org/prebid/server/proto/openrtb/ext/request/smartrtb/ExtRequestSmartrtb.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/smartrtb/ExtRequestSmartrtb.java
@@ -1,6 +1,5 @@
 package org.prebid.server.proto.openrtb.ext.request.smartrtb;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
@@ -8,12 +7,9 @@ import lombok.Value;
 @Value
 public class ExtRequestSmartrtb {
 
-    @JsonProperty("pub_id")
     String pubId;
 
-    @JsonProperty("zone_id")
     String zoneId;
 
-    @JsonProperty("force_bid")
     Boolean forceBid;
 }
