1c0a08b37e78001366554fa1dd7fa7c5546ee71f
Andrew Paliy <59088410+apaliy@users.noreply.github.com>
2020-02-14 14:12:13 +0200

    Add Time condition to Conditional Logger (#605)

--- src/main/java/org/prebid/server/log/ConditionalLogger.java
+++ src/main/java/org/prebid/server/log/ConditionalLogger.java
@@ -1,43 +1,93 @@
 package org.prebid.server.log;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import io.vertx.core.logging.Logger;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 public class ConditionalLogger {
-    private ConcurrentHashMap<String, AtomicInteger> messageToCount;
+
+    private static final int EXPIRE_CACHE_DURATION = 1;
+    private static final int CACHE_MAXIMUM_SIZE = 10_000;
+
+    private ConcurrentMap<String, AtomicInteger> messageToCount;
+    private ConcurrentMap<String, Long> messageToWait;
+
     private final Logger logger;
 
     public ConditionalLogger(Logger logger) {
-        this.logger = logger;
-        this.messageToCount = new ConcurrentHashMap<>();
+        this.logger = Objects.requireNonNull(logger);
+        messageToWait = Caffeine.newBuilder()
+                .maximumSize(CACHE_MAXIMUM_SIZE)
+                .expireAfterWrite(EXPIRE_CACHE_DURATION, TimeUnit.HOURS)
+                .<String, Long>build()
+                .asMap();
+        messageToCount = Caffeine.newBuilder()
+                .maximumSize(CACHE_MAXIMUM_SIZE)
+                .expireAfterWrite(EXPIRE_CACHE_DURATION, TimeUnit.HOURS)
+                .<String, AtomicInteger>build()
+                .asMap();
     }
 
     public void info(String message, Integer maxValue) {
         log(message, maxValue, logger -> logger.info(message));
     }
 
+    public void info(String message, long amount, TimeUnit unit) {
+        log(message, amount, unit, logger -> logger.info(message));
+    }
+
     public void error(String message, Integer maxValue) {
         log(message, maxValue, logger -> logger.error(message));
+    }
+
+    public void error(String message, long amount, TimeUnit unit) {
+        log(message, amount, unit, logger -> logger.error(message));
     }
 
     public void debug(String message, Integer maxValue) {
         log(message, maxValue, logger -> logger.debug(message));
     }
 
+    public void debug(String message, long amount, TimeUnit unit) {
+        log(message, amount, unit, logger -> logger.debug(message));
+    }
+
     public void warn(String message, Integer maxValue) {
         log(message, maxValue, logger -> logger.warn(message));
     }
 
+    public void warn(String message, long amount, TimeUnit unit) {
+        log(message, amount, unit, logger -> logger.warn(message));
+    }
+
     private void log(String key, Integer maxValue, Consumer<Logger> consumer) {
-        final AtomicInteger currentValue = messageToCount.compute(
-                key, (currentKey, currentCounter) -> currentCounter != null ? currentCounter : new AtomicInteger(0)
-        );
-        if (currentValue.incrementAndGet() >= maxValue) {
-            currentValue.set(0);
+        final AtomicInteger value = messageToCount.computeIfAbsent(key, ignored -> new AtomicInteger());
+        if (value.incrementAndGet() >= maxValue) {
+            value.set(0);
             consumer.accept(logger);
         }
     }
+
+    private void log(String key, long amount, TimeUnit unit, Consumer<Logger> consumer) {
+        final long currentTime = Instant.now().toEpochMilli();
+        final Long value = messageToWait.computeIfAbsent(key, ignored -> recalculateDate(amount, unit));
+
+        if (currentTime >= value) {
+            messageToWait.replace(key, value, recalculateDate(amount, unit));
+            consumer.accept(logger);
+        }
+    }
+
+    private static long recalculateDate(long amount, TimeUnit unit) {
+        final long amountInMillis = unit.toMillis(amount);
+        final Instant resultInstant = Instant.now().plusMillis(amountInMillis);
+        return resultInstant.toEpochMilli();
+    }
+
 }
--- src/test/java/org/prebid/server/log/ConditionalLoggerTest.java
+++ src/test/java/org/prebid/server/log/ConditionalLoggerTest.java
@@ -1,17 +1,25 @@
 package org.prebid.server.log;
 
+import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import static org.mockito.ArgumentMatchers.any;
+import java.util.concurrent.TimeUnit;
+
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+@RunWith(VertxUnitRunner.class)
 public class ConditionalLoggerTest {
 
     @Rule
@@ -20,20 +28,45 @@ public class ConditionalLoggerTest {
     @Mock
     private Logger logger;
 
+    private Vertx vertx;
+
     private ConditionalLogger conditionalLogger;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        vertx = Vertx.vertx();
         conditionalLogger = new ConditionalLogger(logger);
     }
 
+    @After
+    public void tearDown(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
+    }
+
     @Test
-    public void log() {
+    public void infoShouldCallLoggerWithExpectedCount() {
         // when
         for (int i = 0; i < 100; i++) {
-            conditionalLogger.info("Hello", 20);
+            conditionalLogger.info("Log Message", 20);
         }
         // then
-        verify(logger, times(5)).info(any());
+        verify(logger, times(5)).info("Log Message");
+    }
+
+    @Test
+    public void infoShouldCallLoggerWithExpectedTimeout(TestContext context) {
+        // when
+        for (int i = 0; i < 5; i++) {
+            conditionalLogger.info("Log Message", 1, TimeUnit.SECONDS);
+            doWait(context, 500);
+        }
+        // then
+        verify(logger, times(2)).info("Log Message");
+    }
+
+    private void doWait(TestContext context, long timeout) {
+        final Async async = context.async();
+        vertx.setTimer(timeout, id -> async.complete());
+        async.await();
     }
 }
