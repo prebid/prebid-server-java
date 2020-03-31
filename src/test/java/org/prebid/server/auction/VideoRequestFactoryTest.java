68b574324fc4356af8ab1eec0df410c4ff942bb3
Andrey Paliy <apaliy@rubiconproject.com>
2020-03-31 10:33:01 +0300

    change request

--- src/test/java/org/prebid/server/auction/VideoRequestFactoryTest.java
+++ src/test/java/org/prebid/server/auction/VideoRequestFactoryTest.java
@@ -37,7 +37,10 @@ import java.util.Arrays;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
