1fb9dbb07fbfbdb02b6fa326ed6e6130ffee4d5c
DGarbar <dimagarbar49@gmail.com>
2020-04-09 16:33:28 +0300

    Add protected endpoint to invalidate account from cache
    `/cache/invalidate?account={accountId}`
    config:
    `settings.in-memory-cache.account-invalidation-enabled` = true

---
+++ src/main/java/org/prebid/server/handler/AccountCacheInvalidationHandler.java
@@ -0,0 +1,44 @@
+package org.prebid.server.handler;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.RoutingContext;
+import org.apache.commons.lang3.StringUtils;
+import org.prebid.server.json.DecodeException;
+import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.settings.CacheNotificationListener;
+import org.prebid.server.settings.CachingApplicationSettings;
+import org.prebid.server.settings.proto.request.InvalidateSettingsCacheRequest;
+import org.prebid.server.settings.proto.request.UpdateSettingsCacheRequest;
+import org.prebid.server.util.HttpUtil;
+
+import java.util.Objects;
+
+/**
+ * Handles HTTP requests for updating/invalidating settings cache.
+ */
+public class AccountCacheInvalidationHandler implements Handler<RoutingContext> {
+
+    private static final String ACCOUNT_ID_PARAM = "account";
+
+    private final CachingApplicationSettings cachingApplicationSettings;
+
+    public AccountCacheInvalidationHandler(CachingApplicationSettings cachingApplicationSettings) {
+        this.cachingApplicationSettings = Objects.requireNonNull(cachingApplicationSettings);
+    }
+
+    @Override
+    public void handle(RoutingContext context) {
+        final HttpServerRequest request = context.request();
+        final String accountId = request.getParam(ACCOUNT_ID_PARAM);
+
+        if (StringUtils.isBlank(accountId)) {
+            HttpUtil.respondWith(context, HttpResponseStatus.BAD_REQUEST, "Account id is not defined");
+        } else {
+            cachingApplicationSettings.invalidateAccountCache(accountId);
+            HttpUtil.respondWith(context, HttpResponseStatus.OK, null);
+        }
+    }
+}
