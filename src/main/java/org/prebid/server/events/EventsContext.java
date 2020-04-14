9ebb3a91451a3392e92911d7b09c6b10995c7111
rpanchyk <rpanchyk@users.noreply.github.com>
2020-04-14 10:55:29 +0300

    Add EventsContext to CacheService instead of auctionTimeout (#669)

---
+++ src/main/java/org/prebid/server/events/EventsContext.java
@@ -0,0 +1,14 @@
+package org.prebid.server.events;
+
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * Accumulates information for proceeding events.
+ */
+@Builder
+@Value
+public class EventsContext {
+
+    Long auctionTimestamp;
+}
