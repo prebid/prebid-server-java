55d6bbcfe512beaa11877d47761ff02ddb374838
rpanchyk <rpanchyk@rubiconproject.com>
2020-06-17 14:51:06 +0300

    Fix incorrect client input when FPD site.domain is an ARRAY - fix conditional logger call in handlers

--- src/main/java/org/prebid/server/handler/openrtb2/AmpHandler.java
+++ src/main/java/org/prebid/server/handler/openrtb2/AmpHandler.java
@@ -316,8 +316,7 @@ public class AmpHandler implements Handler<RoutingContext> {
                 // TODO adminManager: enable when admin endpoints can be bound on application port
                 //adminManager.accept(AdminManager.COUNTER_KEY, logger,
                 //        logMessageFrom(invalidRequestException, message, context));
-                conditionalLogger.info(String.format("%s, Referer: %s", message,
-                        context.request().headers().get(HttpUtil.REFERER_HEADER)), 100);
+                conditionalLogger.info(message, 100);
 
                 status = HttpResponseStatus.BAD_REQUEST.code();
                 body = message;
