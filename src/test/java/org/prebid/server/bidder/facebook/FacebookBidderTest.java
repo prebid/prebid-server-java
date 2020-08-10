f8def857a6c1367b65cde9f7e34394d7c6a24bb8
Andrii Braslavskyi <abraslavskyi@rubiconproject.com>
2020-08-10 16:17:05 +0300

    Set facebook banner width to -1 in all cases when interstitial is 0

--- src/test/java/org/prebid/server/bidder/facebook/FacebookBidderTest.java
+++ src/test/java/org/prebid/server/bidder/facebook/FacebookBidderTest.java
@@ -326,7 +326,7 @@ public class FacebookBidderTest extends VertxTest {
                 .extracting(httpRequest -> mapper.readValue(httpRequest.getBody(), BidRequest.class))
                 .flatExtracting(BidRequest::getImp)
                 .extracting(Imp::getBanner)
-                .containsOnly(Banner.builder().h(50).w(0).build());
+                .containsOnly(Banner.builder().h(50).w(-1).build());
     }
 
     @Test
