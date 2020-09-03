fed6c31454a5e4014f7d32407b5e7c38f1de7093
DGarbar <dimagarbar49@gmail.com>
2020-09-03 15:27:12 +0300

    fix style

--- src/main/java/org/prebid/server/validation/RequestValidator.java
+++ src/main/java/org/prebid/server/validation/RequestValidator.java
@@ -60,7 +60,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
