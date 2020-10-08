02fc50d7743005d03c1a8db475bc27a0f0879795
Braslavskyi Andrii <abraslavskyi@rubiconproject.com>
2020-10-08 17:07:03 +0300

    Add custom cache key (#947)
    * add deals support for category mapping
    
    * Custom cache key for vast with catDur

--- src/main/java/org/prebid/server/spring/config/ServiceConfiguration.java
+++ src/main/java/org/prebid/server/spring/config/ServiceConfiguration.java
@@ -104,6 +104,7 @@ public class ServiceConfiguration {
                 eventsService,
                 metrics,
                 clock,
+                new UUIDIdGenerator(),
                 mapper);
     }
 
