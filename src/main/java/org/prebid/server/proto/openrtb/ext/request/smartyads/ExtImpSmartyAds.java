91f8228aca6bba53985946cfd1f6d9247817094f
Serhii Nahornyi <snahornyi@rubiconproject.com>
2020-10-16 15:36:39 +0300

    Add Smartyads bidder

---
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/smartyads/ExtImpSmartyAds.java
@@ -0,0 +1,19 @@
+package org.prebid.server.proto.openrtb.ext.request.smartyads;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor(staticName = "of")
+public class ExtImpSmartyAds {
+
+    @JsonProperty("accountid")
+    String accountId;
+
+    @JsonProperty("sourceid")
+    String sourceId;
+
+    @JsonProperty("host")
+    String host;
+}
