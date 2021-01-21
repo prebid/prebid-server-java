882b4ef0c4d6994fae55bb51bf27f40a4383797f
Sergii Chernysh <schernysh@rubiconproject.com>
2021-01-21 16:27:30 +0200

    Add per-account /cookie_sync endpoint configuration

--- src/test/java/org/prebid/server/settings/FileApplicationSettingsTest.java
+++ src/test/java/org/prebid/server/settings/FileApplicationSettingsTest.java
@@ -11,9 +11,10 @@ import org.mockito.junit.MockitoRule;
 import org.prebid.server.settings.model.Account;
 import org.prebid.server.settings.model.AccountAnalyticsConfig;
 import org.prebid.server.settings.model.AccountBidValidationConfig;
+import org.prebid.server.settings.model.AccountCookieSyncConfig;
 import org.prebid.server.settings.model.AccountGdprConfig;
-import org.prebid.server.settings.model.BidValidationEnforcement;
 import org.prebid.server.settings.model.AccountStatus;
+import org.prebid.server.settings.model.BidValidationEnforcement;
 import org.prebid.server.settings.model.EnabledForRequestType;
 import org.prebid.server.settings.model.EnforcePurpose;
 import org.prebid.server.settings.model.Purpose;
@@ -111,7 +112,8 @@ public class FileApplicationSettingsTest {
                         + "bidValidations: {"
                         + "banner-creative-max-size: 'enforce'"
                         + "},"
-                        + "status: 'active'"
+                        + "status: 'active',"
+                        + "cookie-sync: {default-limit: 5,max-limit: 8,default-coop-sync: true}"
                         + "}"
                         + "]"));
 
@@ -149,6 +151,7 @@ public class FileApplicationSettingsTest {
                 .analyticsConfig(AccountAnalyticsConfig.of(singletonMap("amp", true)))
                 .bidValidations(AccountBidValidationConfig.of(BidValidationEnforcement.enforce))
                 .status(AccountStatus.active)
+                .cookieSync(AccountCookieSyncConfig.of(5, 8, true))
                 .build());
     }
 
