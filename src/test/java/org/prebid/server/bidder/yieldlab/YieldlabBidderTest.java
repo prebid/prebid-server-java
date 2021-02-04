407bbc7bc959341a371d354c2ab18cffba894fd0
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2021-02-04 15:17:55 +0100

    [GD-2625] Don't use java11 APIs

--- src/test/java/org/prebid/server/bidder/yieldlab/YieldlabBidderTest.java
+++ src/test/java/org/prebid/server/bidder/yieldlab/YieldlabBidderTest.java
@@ -27,6 +27,7 @@ import org.prebid.server.proto.openrtb.ext.response.BidType;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
@@ -116,32 +117,35 @@ public class YieldlabBidderTest extends VertxTest {
         // given
         final Map<String, String> targeting = new HashMap<>();
         targeting.put("key1", "value1");
+
+        final List<Imp> imps = new ArrayList<Imp>();
+        imps.add(Imp.builder()
+                .banner(Banner.builder().w(1).h(1).build())
+                .ext(mapper.valueToTree(ExtPrebid.of(null,
+                        ExtImpYieldlab.builder()
+                                .adslotId("1")
+                                .supplyId("2")
+                                .adSize("adSize")
+                                .targeting(targeting)
+                                .extId("extId")
+                                .build())))
+                .build()
+        );
+        imps.add(Imp.builder()
+                .banner(Banner.builder().w(1).h(1).build())
+                .ext(mapper.valueToTree(ExtPrebid.of(null,
+                        ExtImpYieldlab.builder()
+                                .adslotId("2")
+                                .supplyId("2")
+                                .adSize("adSize")
+                                .targeting(targeting)
+                                .extId("extId")
+                                .build())))
+                .build()
+        );
+
         final BidRequest bidRequest = BidRequest.builder()
-                .imp(List.of(
-                        Imp.builder()
-                                .banner(Banner.builder().w(1).h(1).build())
-                                .ext(mapper.valueToTree(ExtPrebid.of(null,
-                                        ExtImpYieldlab.builder()
-                                                .adslotId("1")
-                                                .supplyId("2")
-                                                .adSize("adSize")
-                                                .targeting(targeting)
-                                                .extId("extId")
-                                                .build())))
-                                .build(),
-                        Imp.builder()
-                                .banner(Banner.builder().w(1).h(1).build())
-                                .ext(mapper.valueToTree(ExtPrebid.of(null,
-                                        ExtImpYieldlab.builder()
-                                                .adslotId("2")
-                                                .supplyId("2")
-                                                .adSize("adSize")
-                                                .targeting(targeting)
-                                                .extId("extId")
-                                                .build())))
-                                .build()
-                        )
-                )
+                .imp(imps)
                 .device(Device.builder().ip("ip").ua("Agent").language("fr").devicetype(1).build())
                 .regs(Regs.of(1, ExtRegs.of(1, "usPrivacy")))
                 .user(User.builder().buyeruid("buyeruid").ext(ExtUser.builder().consent("consent").build()).build())
