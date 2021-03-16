6aecf5035936ad4c57352139dddec8a831bdb000
Serhii Nahornyi <snahornyi@rubiconproject.com>
2021-03-16 11:08:24 +0200

    Style fix

--- src/test/java/org/prebid/server/it/PangleTest.java
+++ src/test/java/org/prebid/server/it/PangleTest.java
@@ -23,18 +23,18 @@ public class PangleTest extends IntegrationTest {
     @Test
     public void openrtb2AuctionShouldRespondWithBidsFromPangle() throws IOException, JSONException {
         // given
-        // Adman bid response for imp 001
+        // Pangle bid response for imp 001
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/pangle-exchange"))
-                .withRequestBody(equalToJson(jsonFrom("openrtb2/pangle/test-adman-bid-request-1.json")))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/pangle/test-adman-bid-response-1.json"))));
+                .withRequestBody(equalToJson(jsonFrom("openrtb2/pangle/test-pangle-bid-request-1.json")))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/pangle/test-pangle-bid-response-1.json"))));
 
         // pre-bid cache
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/cache"))
                 .withRequestBody(equalToBidCacheRequest(
-                        jsonFrom("openrtb2/pangle/test-cache-adman-request.json")))
+                        jsonFrom("openrtb2/pangle/test-cache-pangle-request.json")))
                 .willReturn(aResponse()
                         .withTransformers("cache-response-transformer")
-                        .withTransformerParameter("matcherName", "openrtb2/pangle/test-cache-matcher-adman.json")));
+                        .withTransformerParameter("matcherName", "openrtb2/pangle/test-cache-matcher-pangle.json")));
 
         // when
         final Response response = given(SPEC)
@@ -44,12 +44,12 @@ public class PangleTest extends IntegrationTest {
                 .header("Origin", "http://www.example.com")
                 // this uids cookie value stands for {"uids":{"pangle":"PG-UID"}}
                 .cookie("uids", "eyJ1aWRzIjp7InBhbmdsZSI6IlBHLVVJRCJ9fQ==")
-                .body(jsonFrom("openrtb2/pangle/test-auction-adman-request.json"))
+                .body(jsonFrom("openrtb2/pangle/test-auction-pangle-request.json"))
                 .post("/openrtb2/auction");
 
         // then
         final String expectedAuctionResponse = openrtbAuctionResponseFrom(
-                "openrtb2/pangle/test-auction-adman-response.json",
+                "openrtb2/pangle/test-auction-pangle-response.json",
                 response, singletonList("pangle"));
 
         JSONAssert.assertEquals(expectedAuctionResponse, response.asString(), JSONCompareMode.NON_EXTENSIBLE);
