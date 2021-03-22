412af926ad608c074f127e887a7de54b9fe6f37a
Sergii Chernysh <schernysh@rubiconproject.com>
2021-03-22 17:14:55 +0200

    Rename "endpoint hook" to "entrypoint hook"

--- src/main/java/org/prebid/server/hooks/execution/HookStageExecutor.java
+++ src/main/java/org/prebid/server/hooks/execution/HookStageExecutor.java
@@ -3,12 +3,12 @@ package org.prebid.server.hooks.execution;
 import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
 import org.prebid.server.hooks.execution.model.HookStageExecutionResult;
-import org.prebid.server.hooks.v1.endpoint.EndpointPayload;
+import org.prebid.server.hooks.v1.entrypoint.EntrypointPayload;
 import org.prebid.server.model.Endpoint;
 
 public class HookStageExecutor {
 
-    public Future<HookStageExecutionResult<EndpointPayload>> executeEndpointHooks(
+    public Future<HookStageExecutionResult<EntrypointPayload>> executeEndpointHooks(
             MultiMap queryParams,
             MultiMap headers,
             String body,
--- src/main/java/org/prebid/server/hooks/v1/endpoint/EndpointHook.java
+++
@@ -1,8 +0,0 @@
-package org.prebid.server.hooks.v1.endpoint;
-
-import org.prebid.server.hooks.v1.Hook;
-import org.prebid.server.hooks.v1.InvocationContext;
-
-public interface EndpointHook extends Hook<EndpointPayload, InvocationContext> {
-
-}
