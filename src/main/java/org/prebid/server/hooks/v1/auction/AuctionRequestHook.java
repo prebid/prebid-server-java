217aca13cc578462e9411d093d5ddf129425c9f3
Sergii Chernysh <schernysh@rubiconproject.com>
2021-03-22 12:25:53 +0200

    Renamed few hooks to better reflect their meaning

--- src/main/java/org/prebid/server/hooks/v1/auction/AuctionRequestHook.java
+++
@@ -1,7 +0,0 @@
-package org.prebid.server.hooks.v1.auction;
-
-import org.prebid.server.hooks.v1.Hook;
-
-public interface AuctionRequestHook extends Hook<AuctionRequestPayload, AuctionInvocationContext> {
-
-}
--- src/main/java/org/prebid/server/hooks/v1/bidder/BidderResponseHook.java
+++
@@ -1,7 +0,0 @@
-package org.prebid.server.hooks.v1.bidder;
-
-import org.prebid.server.hooks.v1.Hook;
-
-public interface BidderResponseHook extends Hook<BidderResponsePayload, BidderInvocationContext> {
-
-}
