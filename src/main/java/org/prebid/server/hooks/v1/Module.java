8d841c3b8f72ad82adc5bb937eb465f545a03ea1
rpanchyk <rpanchyk@rubiconproject.com>
2021-03-26 11:36:43 +0200

    Fix after review

--- src/main/java/org/prebid/server/hooks/v1/Module.java
+++ src/main/java/org/prebid/server/hooks/v1/Module.java
@@ -3,11 +3,20 @@ package org.prebid.server.hooks.v1;
 import java.util.Set;
 
 /**
- * Cares of the module identification among other modules and supplies a collection of available {@Hook}s.
+ * Cares of the module identification among other modules and supplies a collection of available {@link Hook}s.
+ * <p>
+ * This interface is used to keep knowledge of which {@link Hook} is belongs to certain {@link Module}
+ * while running execution plan.
  */
 public interface Module {
 
+    /**
+     * An identifier that should be unique among other available modules.
+     */
     String code();
 
+    /**
+     * Collection of hooks available through the module.
+     */
     Set<Hook> hooks();
 }
