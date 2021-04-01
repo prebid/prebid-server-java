6101ca19dd3622fa69f35efb70dfd7cec4950b46
Sergii Chernysh <schernysh@rubiconproject.com>
2021-04-01 15:57:56 +0300

    Insert auction response hooks invocation logic

--- src/main/java/org/prebid/server/hooks/execution/HookStageExecutor.java
+++ src/main/java/org/prebid/server/hooks/execution/HookStageExecutor.java
@@ -1,11 +1,13 @@
 package org.prebid.server.hooks.execution;
 
 import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.response.BidResponse;
 import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
 import org.prebid.server.auction.model.BidderRequest;
 import org.prebid.server.hooks.execution.model.HookExecutionContext;
 import org.prebid.server.hooks.execution.model.HookStageExecutionResult;
+import org.prebid.server.hooks.v1.auction.AuctionResponsePayload;
 import org.prebid.server.hooks.v1.bidder.BidderRequestPayload;
 import org.prebid.server.hooks.v1.entrypoint.EntrypointPayload;
 
@@ -43,6 +45,18 @@ public class HookStageExecutor {
             @Override
             public BidRequest bidRequest() {
                 return bidderRequest.getBidRequest();
+            }
+        }));
+    }
+
+    public Future<HookStageExecutionResult<AuctionResponsePayload>> executeAuctionResponseStage(
+            BidResponse bidResponse,
+            HookExecutionContext context) {
+
+        return Future.succeededFuture(HookStageExecutionResult.of(false, new AuctionResponsePayload() {
+            @Override
+            public BidResponse bidResponse() {
+                return bidResponse;
             }
         }));
     }
