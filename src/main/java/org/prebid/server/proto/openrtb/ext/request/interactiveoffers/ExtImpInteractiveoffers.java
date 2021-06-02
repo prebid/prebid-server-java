65880df17ebbcca433d0457580c4ed55942e4ce5
nickluck8 <msteliuk@magnite.com>
2021-06-02 12:53:14 +0300

    add interactive offers model

---
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/interactiveoffers/ExtImpInteractiveoffers.java
@@ -0,0 +1,15 @@
+package org.prebid.server.proto.openrtb.ext.request.interactiveoffers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+/**
+ * Defines the contract for bidrequest.imp[i].ext.interactiveoffers
+ */
+@AllArgsConstructor(staticName = "of")
+@Value
+public class ExtImpInteractiveoffers {
+    @JsonProperty("pubid")
+    Integer pubId;
+}
