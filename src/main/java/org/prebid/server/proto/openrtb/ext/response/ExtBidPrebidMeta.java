ea8c5a3b8ed96d6ae07b1fbdfdc2a455d73d78bb
Serhii Nahornyi <sergiy3344@gmail.com>
2021-06-16 16:51:34 +0300

    Update case of ext prebid meta (#1325)

--- src/main/java/org/prebid/server/proto/openrtb/ext/response/ExtBidPrebidMeta.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/response/ExtBidPrebidMeta.java
@@ -1,5 +1,6 @@
 package org.prebid.server.proto.openrtb.ext.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 
@@ -9,25 +10,36 @@ import java.util.List;
 @Value
 public class ExtBidPrebidMeta {
 
+    @JsonProperty("advertiserId")
     Integer advertiserId;
 
+    @JsonProperty("advertiserName")
     String advertiserName;
 
+    @JsonProperty("agencyId")
     Integer agencyId;
 
+    @JsonProperty("agencyName")
     String agencyName;
 
+    @JsonProperty("brandId")
     Integer brandId;
 
+    @JsonProperty("brandName")
     String brandName;
 
+    @JsonProperty("mediaType")
     String mediaType;
 
+    @JsonProperty("networkId")
     Integer networkId;
 
+    @JsonProperty("networkName")
     String networkName;
 
+    @JsonProperty("primaryCatId")
     String primaryCatId;
 
+    @JsonProperty("secondaryCatIds")
     List<String> secondaryCatIds;
 }
