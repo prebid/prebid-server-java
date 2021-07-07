420bba9b246d43dfc80f8756fc44bcbb8ca17d0d
Alex Maltsev <and1sscsgo@gmail.com>
2021-07-07 15:05:51 +0300

    Resolved conversation

--- src/test/java/org/prebid/server/bidder/outbrain/OutbrainBidderTest.java
+++ src/test/java/org/prebid/server/bidder/outbrain/OutbrainBidderTest.java
@@ -82,14 +82,6 @@ public class OutbrainBidderTest extends VertxTest {
                         null, singletonList("testBcat"), singletonList("testBadv")))))
                 .build();
 
-        final Imp secondImp = Imp.builder()
-                .id("456")
-                .tagid("456")
-                .ext(mapper.valueToTree(ExtPrebid.of(null, ExtImpOutbrain.of(
-                        ExtImpOutbrainPublisher.of("testId", "testName", "testDomain"),
-                        "", singletonList("testBcat"), singletonList("testBadv")))))
-                .build();
-
         final Imp thirdImp = Imp.builder()
                 .id("789")
                 .tagid("789")
@@ -98,7 +90,7 @@ public class OutbrainBidderTest extends VertxTest {
                         "098", singletonList("testBcat"), singletonList("testBadv")))))
                 .build();
 
-        final BidRequest bidRequest = BidRequest.builder().imp(asList(firstImp, secondImp, thirdImp)).build();
+        final BidRequest bidRequest = BidRequest.builder().imp(asList(firstImp, thirdImp)).build();
 
         // when
         final Result<List<HttpRequest<BidRequest>>> result = outbrainBidder.makeHttpRequests(bidRequest);
@@ -108,7 +100,7 @@ public class OutbrainBidderTest extends VertxTest {
         assertThat(result.getValue())
                 .extracting(HttpRequest::getPayload)
                 .flatExtracting(BidRequest::getImp)
-                .containsExactlyInAnyOrder(firstImp, secondImp, thirdImp.toBuilder().tagid("098").build());
+                .containsExactlyInAnyOrder(firstImp, thirdImp.toBuilder().tagid("098").build());
     }
 
     @Test
