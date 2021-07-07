b93c88b51c4e50bab60d26c3ecf4ed1f3b2a42a1
Tushar Singh <tushar.sing@media.net>
2021-07-07 11:57:20 +0530

    Resolving PMD and Checkstyle Errors

--- src/main/java/org/prebid/server/bidder/medianet/MedianetBidder.java
+++ src/main/java/org/prebid/server/bidder/medianet/MedianetBidder.java
@@ -1,10 +1,13 @@
 package org.prebid.server.bidder.medianet;
 
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.prebid.server.bidder.Bidder;
 import org.prebid.server.bidder.OpenrtbBidder;
 import org.prebid.server.json.JacksonMapper;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -13,6 +16,7 @@ import java.nio.charset.StandardCharsets;
  */
 public class MedianetBidder extends OpenrtbBidder<Void> {
 
+    private static final Logger logger = LoggerFactory.getLogger(MedianetBidder.class);
     private static final String EXTERNAL_URL_MACRO = "{{PREBID_SERVER_ENDPOINT}}";
 
     public MedianetBidder(String endpointUrl, JacksonMapper mapper, String externalUrl) {
@@ -23,7 +27,11 @@ public class MedianetBidder extends OpenrtbBidder<Void> {
     }
 
     private static String getEndpoint(String originalEndpoint, String replacement) {
-        replacement = URLEncoder.encode(replacement, StandardCharsets.UTF_8);
+        try {
+            replacement = URLEncoder.encode(replacement, StandardCharsets.UTF_8.displayName());
+        } catch (UnsupportedEncodingException e) {
+            logger.error("Unable to replace params in medianet endpoint");
+        }
         return StringUtils.replace(originalEndpoint, EXTERNAL_URL_MACRO, replacement);
     }
 }
