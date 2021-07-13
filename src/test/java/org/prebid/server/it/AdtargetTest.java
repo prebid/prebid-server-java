800c1220d22ab5df110fa9b9dfd546a97dd8dc4d
Alex Maltsev <and1sscsgo@gmail.com>
2021-07-13 17:25:48 +0300

    Work in progress

--- src/test/java/org/prebid/server/it/AdtargetTest.java
+++ src/test/java/org/prebid/server/it/AdtargetTest.java
@@ -4,8 +4,7 @@ import io.restassured.response.Response;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.prebid.server.model.Endpoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -16,7 +15,6 @@ import static com.github.tomakehurst.wiremock.client.WireMock.equalToIgnoreCase;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static io.restassured.RestAssured.given;
 import static java.util.Collections.singletonList;
 
 @RunWith(SpringRunner.class)
@@ -34,21 +32,11 @@ public class AdtargetTest extends IntegrationTest {
                         jsonFrom("openrtb2/adtarget/test-adtarget-bid-response-1.json"))));
 
         // when
-        final Response response = given(SPEC)
-                .header("Referer", "http://www.example.com")
-                .header("X-Forwarded-For", "193.168.244.1")
-                .header("User-Agent", "userAgent")
-                .header("Origin", "http://www.example.com")
-                // this uids cookie value stands for {"uids":{"adtarget":"AD-UID"}}
-                .cookie("uids", "eyJ1aWRzIjp7ImFkdGFyZ2V0IjoiQUQtVUlEIn19")
-                .body(jsonFrom("openrtb2/adtarget/test-auction-adtarget-request.json"))
-                .post("/openrtb2/auction");
+        final Response response = responseFor("openrtb2/adtarget/test-auction-adtarget-request.json",
+                Endpoint.openrtb2_auction);
 
         // then
-        final String expectedAuctionResponse = openrtbAuctionResponseFrom(
-                "openrtb2/adtarget/test-auction-adtarget-response.json",
-                response, singletonList("adtarget"));
-
-        JSONAssert.assertEquals(expectedAuctionResponse, response.asString(), JSONCompareMode.NON_EXTENSIBLE);
+        assertJsonEquals("openrtb2/adtarget/test-auction-adtarget-response.json", response,
+                singletonList("adtarget"));
     }
 }
--- src/test/java/org/prebid/server/it/AdtelligentTest.java
+++ src/test/java/org/prebid/server/it/AdtelligentTest.java
@@ -4,8 +4,7 @@ import io.restassured.response.Response;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.prebid.server.model.Endpoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -15,7 +14,6 @@ import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static io.restassured.RestAssured.given;
 import static java.util.Collections.singletonList;
 
 @RunWith(SpringRunner.class)
@@ -31,21 +29,11 @@ public class AdtelligentTest extends IntegrationTest {
                         jsonFrom("openrtb2/adtelligent/test-adtelligent-bid-response-1.json"))));
 
         // when
-        final Response response = given(SPEC)
-                .header("Referer", "http://www.example.com")
-                .header("X-Forwarded-For", "193.168.244.1")
-                .header("User-Agent", "userAgent")
-                .header("Origin", "http://www.example.com")
-                // this uids cookie value stands for {"uids":{"adtelligent":"AT-UID"}}
-                .cookie("uids", "eyJ1aWRzIjp7ImFkdGVsbGlnZW50IjoiQVQtVUlEIn19")
-                .body(jsonFrom("openrtb2/adtelligent/test-auction-adtelligent-request.json"))
-                .post("/openrtb2/auction");
+        final Response response = responseFor("openrtb2/adtelligent/test-auction-adtelligent-request.json",
+                Endpoint.openrtb2_auction);
 
         // then
-        final String expectedAuctionResponse = openrtbAuctionResponseFrom(
-                "openrtb2/adtelligent/test-auction-adtelligent-response.json",
-                response, singletonList("adtelligent"));
-
-        JSONAssert.assertEquals(expectedAuctionResponse, response.asString(), JSONCompareMode.NON_EXTENSIBLE);
+        assertJsonEquals("openrtb2/adtelligent/test-auction-adtelligent-response.json", response,
+                singletonList("adtelligent"));
     }
 }
