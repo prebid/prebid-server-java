062bfd3e536571a5b64e9f976b830ff494d0eaf4
Alex Maltsev <and1sscsgo@gmail.com>
2021-07-14 12:25:33 +0300

    Work in progress

--- src/test/java/org/prebid/server/it/BeintooTest.java
+++ src/test/java/org/prebid/server/it/BeintooTest.java
@@ -4,6 +4,7 @@ import io.restassured.response.Response;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.prebid.server.model.Endpoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -14,7 +15,6 @@ import static com.github.tomakehurst.wiremock.client.WireMock.equalToIgnoreCase;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static io.restassured.RestAssured.given;
 import static java.util.Collections.singletonList;
 
 @RunWith(SpringRunner.class)
@@ -26,25 +26,15 @@ public class BeintooTest extends IntegrationTest {
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/beintoo-exchange"))
                 .withHeader("Accept", equalTo("application/json"))
                 .withHeader("Content-Type", equalToIgnoreCase("application/json;charset=utf-8"))
-                .withHeader("User-Agent", equalTo("Android Chrome/60"))
+                .withHeader("User-Agent", equalTo("userAgent"))
                 .withHeader("X-Forwarded-For", equalTo("193.168.244.1"))
-                .withHeader("Referer", equalTo("http://www.example.com"))
-                .withHeader("DNT", equalTo("2"))
-                .withHeader("Accept-Language", equalTo("en"))
                 .withRequestBody(equalToJson(jsonFrom("openrtb2/beintoo/test-beintoo-bid-request.json"),
                         true, true))
                 .willReturn(aResponse().withBody(jsonFrom("openrtb2/beintoo/test-beintoo-bid-response.json"))));
 
         // when
-        final Response response = given(SPEC)
-                .header("Referer", "http://www.example.com")
-                .header("X-Forwarded-For", "193.168.244.1")
-                .header("User-Agent", "userAgent")
-                .header("Origin", "http://www.example.com")
-                // this uids cookie value stands for {"uids":{"beintoo":"BT-UID"}}
-                .cookie("uids", "eyJ1aWRzIjp7ImJlaW50b28iOiJCVC1VSUQifX0=")
-                .body(jsonFrom("openrtb2/beintoo/test-auction-beintoo-request.json"))
-                .post("/openrtb2/auction");
+        final Response response = responseFor("openrtb2/beintoo/test-auction-beintoo-request.json",
+                Endpoint.openrtb2_auction);
 
         // then
         assertJsonEquals("openrtb2/beintoo/test-auction-beintoo-response.json", response, singletonList("beintoo"));
