03c80bcd4d7a96952e60fdb99d23bb84bbb35b03
Alex Maltsev <and1sscsgo@gmail.com>
2021-07-16 17:18:24 +0300

    Work in progress

--- src/test/java/org/prebid/server/it/YieldoneTest.java
+++ src/test/java/org/prebid/server/it/YieldoneTest.java
@@ -4,18 +4,15 @@ import io.restassured.response.Response;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.prebid.server.model.Endpoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static io.restassured.RestAssured.given;
 import static java.util.Collections.singletonList;
 
 @RunWith(SpringRunner.class)
@@ -25,26 +22,14 @@ public class YieldoneTest extends IntegrationTest {
     public void openrtb2AuctionShouldRespondWithBidsFromYieldone() throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/yieldone-exchange"))
-                .withHeader("Accept", equalTo("application/json"))
-                .withHeader("Content-Type", equalTo("application/json;charset=UTF-8"))
                 .withRequestBody(equalToJson(jsonFrom("openrtb2/yieldone/test-yieldone-bid-request.json")))
                 .willReturn(aResponse().withBody(jsonFrom("openrtb2/yieldone/test-yieldone-bid-response.json"))));
 
         // when
-        final Response response = given(SPEC)
-                .header("Referer", "http://www.example.com")
-                .header("X-Forwarded-For", "193.168.244.1")
-                .header("User-Agent", "userAgent")
-                .header("Origin", "http://www.example.com")
-                .cookie("uids", "eyJ1aWRzIjp7InlpZWxkb25lIjoiWUQtVUlEIn19")
-                .body(jsonFrom("openrtb2/yieldone/test-auction-yieldone-request.json"))
-                .post("/openrtb2/auction");
+        final Response response = responseFor("openrtb2/yieldone/test-auction-yieldone-request.json",
+                Endpoint.openrtb2_auction);
 
         // then
-        final String expectedAuctionResponse = openrtbAuctionResponseFrom(
-                "openrtb2/yieldone/test-auction-yieldone-response.json",
-                response, singletonList("yieldone"));
-
-        JSONAssert.assertEquals(expectedAuctionResponse, response.asString(), JSONCompareMode.NON_EXTENSIBLE);
+        assertJsonEquals("openrtb2/yieldone/test-auction-yieldone-response.json", response, singletonList("yieldone"));
     }
 }
