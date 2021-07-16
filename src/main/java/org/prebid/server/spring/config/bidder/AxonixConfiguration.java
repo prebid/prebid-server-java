6dd0ddfa0f5ae5ce742dfa93044c300f5b745483
Alex Maltsev <and1sscsgo@gmail.com>
2021-07-16 18:14:38 +0300

    Added bidder configuration

---
+++ src/main/java/org/prebid/server/spring/config/bidder/AxonixConfiguration.java
@@ -0,0 +1,52 @@
+package org.prebid.server.spring.config.bidder;
+
+import org.prebid.server.bidder.BidderDeps;
+import org.prebid.server.bidder.axonix.AxonixBidder;
+import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.spring.config.bidder.model.BidderConfigurationProperties;
+import org.prebid.server.spring.config.bidder.util.BidderDepsAssembler;
+import org.prebid.server.spring.config.bidder.util.UsersyncerCreator;
+import org.prebid.server.spring.env.YamlPropertySourceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.validation.constraints.NotBlank;
+
+@Configuration
+@PropertySource(value = "classpath:/bidder-config/axonix.yaml", factory = YamlPropertySourceFactory.class)
+public class AxonixConfiguration {
+
+    private static final String BIDDER_NAME = "axonix";
+
+    @Value("${external-url}")
+    @NotBlank
+    private String externalUrl;
+
+    @Autowired
+    private JacksonMapper mapper;
+
+    @Autowired
+    @Qualifier("axonixConfigurationProperties")
+    private BidderConfigurationProperties configProperties;
+
+    @Bean("axonixConfigurationProperties")
+    @ConfigurationProperties("adapters.axonix")
+    BidderConfigurationProperties configurationProperties() {
+        return new BidderConfigurationProperties();
+    }
+
+    @Bean
+    BidderDeps axonixBidderDeps() {
+        return BidderDepsAssembler.forBidder(BIDDER_NAME)
+                .withConfig(configProperties)
+                .usersyncerCreator(UsersyncerCreator.create(externalUrl))
+                .bidderCreator(config -> new AxonixBidder(config.getEndpoint(), mapper))
+                .assemble();
+    }
+}
+
