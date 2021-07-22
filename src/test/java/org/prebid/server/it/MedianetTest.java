a1b878aafd8f0c42ad85646401083e57845b4384
Tushar Singh <tushar.edm@gmail.com>
2021-07-22 13:42:21 +0530

    Refactored Unit Tests

--- src/test/java/org/prebid/server/it/MedianetTest.java
+++ src/test/java/org/prebid/server/it/MedianetTest.java
@@ -4,8 +4,7 @@ import io.restassured.response.Response;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.prebid.server.model.Endpoint;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -14,7 +13,6 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static io.restassured.RestAssured.given;
 import static java.util.Collections.singletonList;
 
 @RunWith(SpringRunner.class)
@@ -24,25 +22,15 @@ public class MedianetTest extends IntegrationTest {
     public void openrtb2AuctionShouldRespondWithBidsFromTheMedianet() throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/medianet-exchange"))
-                .withRequestBody(equalToJson(jsonFrom("openrtb2/medianet/test-medianet-bid-request-1.json")))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/medianet/test-medianet-bid-response-1.json"))));
+                .withRequestBody(equalToJson(jsonFrom("openrtb2/medianet/test-medianet-bid-request.json")))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/medianet/test-medianet-bid-response.json"))));
 
         // when
-        final Response response = given(SPEC)
-                .header("Referer", "http://www.example.com")
-                .header("X-Forwarded-For", "193.168.244.1")
-                .header("User-Agent", "userAgent")
-                .header("Origin", "http://www.example.com")
-                // this uids cookie value stands for {"uids":{"medianet":"MNET-UID"}}
-                .cookie("uids", "eyJ1aWRzIjp7Im1lZGlhbmV0IjoiTU5FVC1VSUQifX0=")
-                .body(jsonFrom("openrtb2/medianet/test-auction-medianet-request.json"))
-                .post("/openrtb2/auction");
+        final Response response = responseFor("openrtb2/rtbhouse/test-auction-medianet-request.json",
+                Endpoint.openrtb2_auction);
 
         // then
-        final String expectedAuctionResponse = openrtbAuctionResponseFrom(
-                "openrtb2/medianet/test-auction-medianet-response.json",
-                response, singletonList("medianet"));
-
-        JSONAssert.assertEquals(expectedAuctionResponse, response.asString(), JSONCompareMode.NON_EXTENSIBLE);
+        assertJsonEquals("openrtb2/rtbhouse/test-auction-medianet-response.json", response,
+                singletonList("medianet"));
     }
 }
