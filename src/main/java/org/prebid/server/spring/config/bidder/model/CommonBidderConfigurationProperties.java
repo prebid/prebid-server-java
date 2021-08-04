6ed6e6b463611443e53a348e1346016c947e6b86
rpanchyk <rpanchyk@rubiconproject.com>
2021-08-04 14:40:21 +0300

    Proposal to check adapter-defaults

--- src/main/java/org/prebid/server/spring/config/bidder/model/CommonBidderConfigurationProperties.java
+++ src/main/java/org/prebid/server/spring/config/bidder/model/CommonBidderConfigurationProperties.java
@@ -1,6 +1,9 @@
 package org.prebid.server.spring.config.bidder.model;
 
 import lombok.Data;
+import org.apache.commons.lang3.ObjectUtils;
+import org.prebid.server.exception.PreBidException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.validation.annotation.Validated;
 
 import java.util.Collections;
@@ -9,7 +12,7 @@ import java.util.Map;
 
 @Validated
 @Data
-public class CommonBidderConfigurationProperties {
+public class CommonBidderConfigurationProperties implements InitializingBean {
 
     private Boolean enabled;
 
@@ -22,4 +25,26 @@ public class CommonBidderConfigurationProperties {
     private List<String> deprecatedNames;
 
     private Map<String, Object> aliases = Collections.emptyMap();
+
+    /**
+     * Since this class is
+     * inherited by {@see org.prebid.server.spring.config.bidder.model.BidderConfigurationProperties}
+     * it is not possible to use {@see javax.validation.constraints.NotNull} constraint for its fields.
+     * Thus, manual fields checking is applied.
+     * <p>
+     * Note: Please, don't forget to enlist new fields in verification list.
+     */
+    @Override
+    public void afterPropertiesSet() {
+        if (getClass().isAssignableFrom(CommonBidderConfigurationProperties.class) && !ObjectUtils.allNotNull(
+                enabled,
+                pbsEnforcesGdpr,
+                pbsEnforcesCcpa,
+                modifyingVastXmlAllowed,
+                deprecatedNames,
+                aliases)) {
+
+            throw new PreBidException("Adapter default properties missing or partially configured");
+        }
+    }
 }
