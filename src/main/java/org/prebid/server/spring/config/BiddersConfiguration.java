04d082774b318a631c0d3d9df36a2671c4f50e0d
rpanchyk <rpanchyk@rubiconproject.com>
2021-08-12 16:17:56 +0300

    Removed BPP

--- src/main/java/org/prebid/server/spring/config/BiddersConfiguration.java
+++ src/main/java/org/prebid/server/spring/config/BiddersConfiguration.java
@@ -1,7 +1,6 @@
 package org.prebid.server.spring.config;
 
-import org.prebid.server.spring.config.bidder.model.CommonBidderConfigurationProperties;
-import org.prebid.server.spring.config.bidder.postprocessor.BidderConfigurationBeanPostProcessor;
+import org.prebid.server.spring.config.bidder.model.DefaultBidderConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +10,7 @@ public class BiddersConfiguration {
 
     @Bean
     @ConfigurationProperties(prefix = "adapter-defaults")
-    CommonBidderConfigurationProperties commonBidderConfigurationProperties() {
-        return new CommonBidderConfigurationProperties();
-    }
-
-    @Bean
-    BidderConfigurationBeanPostProcessor bidderConfigurationBeanPostProcessor(
-            CommonBidderConfigurationProperties commonBidderConfigurationProperties) {
-        return new BidderConfigurationBeanPostProcessor(commonBidderConfigurationProperties);
+    DefaultBidderConfigurationProperties defaultBidderConfigurationProperties() {
+        return new DefaultBidderConfigurationProperties();
     }
 }
--- src/main/java/org/prebid/server/spring/config/bidder/model/BidderConfigurationProperties.java
+++ src/main/java/org/prebid/server/spring/config/bidder/model/BidderConfigurationProperties.java
@@ -1,17 +1,41 @@
 package org.prebid.server.spring.config.bidder.model;
 
+import lombok.AccessLevel;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 
+import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @Data
 @Validated
-@EqualsAndHashCode(callSuper = true)
-public class BidderConfigurationProperties extends CommonBidderConfigurationProperties {
+public class BidderConfigurationProperties {
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    @Autowired
+    DefaultBidderConfigurationProperties defaultProperties;
+
+    private Boolean enabled;
+
+    private Boolean pbsEnforcesGdpr;
+
+    private Boolean pbsEnforcesCcpa;
+
+    private Boolean modifyingVastXmlAllowed;
+
+    private List<String> deprecatedNames;
+
+    private Map<String, Object> aliases = Collections.emptyMap();
 
     @NotBlank
     private String endpoint;
@@ -28,5 +52,16 @@ public class BidderConfigurationProperties extends CommonBidderConfigurationProp
 
     public BidderConfigurationProperties() {
         selfClass = this.getClass();
+    }
+
+    @PostConstruct
+    private void init() {
+        enabled = ObjectUtils.defaultIfNull(enabled, defaultProperties.getEnabled());
+        pbsEnforcesGdpr = ObjectUtils.defaultIfNull(pbsEnforcesGdpr, defaultProperties.getPbsEnforcesGdpr());
+        pbsEnforcesCcpa = ObjectUtils.defaultIfNull(pbsEnforcesCcpa, defaultProperties.getPbsEnforcesCcpa());
+        modifyingVastXmlAllowed = ObjectUtils.defaultIfNull(modifyingVastXmlAllowed,
+                defaultProperties.getModifyingVastXmlAllowed());
+        deprecatedNames = ObjectUtils.defaultIfNull(deprecatedNames, defaultProperties.getDeprecatedNames());
+        aliases = MapUtils.isNotEmpty(aliases) ? aliases : defaultProperties.getAliases();
     }
 }
