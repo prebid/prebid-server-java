0f4bebf6e24c7b7b64001c480c4033deacaaafbe
laarchenko <fkirill440@gmail.com>
2021-08-18 01:15:27 +0300

    added unit tests

--- src/main/java/org/prebid/server/bidder/adagio/AdagioBidder.java
+++ src/main/java/org/prebid/server/bidder/adagio/AdagioBidder.java
@@ -1,5 +1,7 @@
 package org.prebid.server.bidder.adagio;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Device;
 import com.iab.openrtb.request.Imp;
@@ -8,6 +10,8 @@ import com.iab.openrtb.response.SeatBid;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.prebid.server.bidder.Bidder;
 import org.prebid.server.bidder.model.HttpCall;
 import org.prebid.server.bidder.model.HttpRequest;
@@ -18,12 +22,10 @@ import org.prebid.server.exception.PreBidException;
 import org.prebid.server.json.DecodeException;
 import org.prebid.server.json.JacksonMapper;
 import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.proto.openrtb.ext.response.ExtBidPrebid;
 import org.prebid.server.util.HttpUtil;
 
-import java.util.Collections;
-import java.util.Objects;
-import java.util.List;
-import java.util.Collection;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AdagioBidder implements Bidder<BidRequest> {
@@ -32,17 +34,8 @@ public class AdagioBidder implements Bidder<BidRequest> {
     private final JacksonMapper mapper;
 
     public AdagioBidder(String endpointUrl, JacksonMapper mapper) {
-        this.endpointUrl = endpointUrl;
-        this.mapper = mapper;
-    }
-
-    private static MultiMap resolveHeaders(Device device) {
-        final MultiMap headers = HttpUtil.headers();
-
-        if (device != null) {
-            HttpUtil.addHeaderIfValueIsNotEmpty(headers, HttpUtil.X_FORWARDED_FOR_HEADER, device.getIp());
-        }
-        return headers;
+        this.endpointUrl = HttpUtil.validateUrl(Objects.requireNonNull(endpointUrl));
+        this.mapper = Objects.requireNonNull(mapper);
     }
 
     @Override
@@ -56,42 +49,63 @@ public class AdagioBidder implements Bidder<BidRequest> {
                 .build());
     }
 
+    private static MultiMap resolveHeaders(Device device) {
+        final MultiMap headers = HttpUtil.headers();
+
+        if (device != null) {
+            if(StringUtils.isEmpty(device.getIpv6())) {
+            HttpUtil.addHeaderIfValueIsNotEmpty(headers, HttpUtil.X_FORWARDED_FOR_HEADER, device.getIp());
+            }
+            else {
+                HttpUtil.addHeaderIfValueIsNotEmpty(headers, HttpUtil.X_FORWARDED_FOR_HEADER, device.getIpv6());
+            }
+        }
+        return headers;
+    }
+
     @Override
     public final Result<List<BidderBid>> makeBids(HttpCall<BidRequest> httpCall, BidRequest bidRequest) {
+        final List<BidderError> errors = new ArrayList<>();
+
         try {
             final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
-            return Result.of(extractBids(httpCall.getRequest().getPayload(), bidResponse), Collections.emptyList());
+            return Result.of(extractBids(httpCall.getRequest().getPayload(), bidResponse, errors), errors);
         } catch (DecodeException | PreBidException e) {
             return Result.withError(BidderError.badServerResponse(e.getMessage()));
         }
     }
 
-    private static List<BidderBid> extractBids(BidRequest bidRequest, BidResponse bidResponse) {
+    private List<BidderBid> extractBids(BidRequest bidRequest, BidResponse bidResponse,  List<BidderError> errors) {
         if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
             return Collections.emptyList();
         }
-        return bidsFromResponse(bidRequest, bidResponse);
+        return bidsFromResponse(bidRequest, bidResponse, errors);
     }
 
-    private static List<BidderBid> bidsFromResponse(BidRequest bidRequest, BidResponse bidResponse) {
-        return bidResponse.getSeatbid().stream()
+    private List<BidderBid> bidsFromResponse(BidRequest bidRequest, BidResponse bidResponse, List<BidderError> errors) {
+        List<BidderBid> biddersList =  bidResponse.getSeatbid().stream()
                 .filter(Objects::nonNull)
                 .map(SeatBid::getBid)
                 .filter(Objects::nonNull)
                 .flatMap(Collection::stream)
-                .map(bid -> BidderBid.of(bid, getBidType(bidRequest.getImp().get(0)), bidResponse.getCur()))
+                .map(bid -> BidderBid.of(bid, getBidType(bid.getExt(), errors), bidResponse.getCur()))
                 .collect(Collectors.toList());
+
+        return biddersList.stream().filter(i ->  Objects.nonNull(i.getType())).collect(Collectors.toList());
     }
 
-    private static BidType getBidType(Imp imp) {
-        if (imp.getBanner() != null) {
-            return BidType.banner;
-        } else if (imp.getVideo() != null) {
-            return BidType.video;
-        } else if (imp.getXNative() != null) {
-            return BidType.xNative;
+    private BidType getBidType(ObjectNode bidExt, List<BidderError> errors) {
+        ExtBidPrebid prebid = null;
+        try {
+             prebid = mapper.mapper().treeToValue(bidExt.get("Prebid"), ExtBidPrebid.class);
+        } catch (JsonProcessingException | NullPointerException e) {
+            e.printStackTrace();
+            errors.add(BidderError.badInput(e.getMessage()));
         }
-        throw new PreBidException(String.format("Failed to find impression %s", imp.getId()));
+        if (prebid!=null) {
+            return prebid.getType();
+        }
+        return null;
     }
 
 }
--- src/test/java/org/prebid/server/bidder/adagio/AdagioBidderTest.java
+++ src/test/java/org/prebid/server/bidder/adagio/AdagioBidderTest.java
@@ -1,9 +1,13 @@
 package org.prebid.server.bidder.adagio;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Device;
 import com.iab.openrtb.request.Imp;
 import com.iab.openrtb.request.Site;
+import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
 import io.netty.handler.codec.http.HttpHeaderValues;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,17 +18,22 @@ import org.prebid.server.bidder.model.HttpCall;
 import org.prebid.server.bidder.model.HttpRequest;
 import org.prebid.server.bidder.model.HttpResponse;
 import org.prebid.server.bidder.model.Result;
-import org.prebid.server.proto.openrtb.ext.ExtPrebid;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.proto.openrtb.ext.response.ExtBidPrebid;
 import org.prebid.server.util.HttpUtil;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.singletonList;
 import static java.util.function.Function.identity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class AdagioBidderTest extends VertxTest {
 
@@ -35,6 +44,11 @@ public class AdagioBidderTest extends VertxTest {
     @Before
     public void setUp() {
         adagioBidder = new AdagioBidder(ENDPOINT_URL, jacksonMapper);
+    }
+
+    @Test
+    public void creationShouldFailOnInvalidEndpointUrl() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new AdagioBidder("invalid_url", jacksonMapper));
     }
 
     @Test
@@ -61,19 +75,6 @@ public class AdagioBidderTest extends VertxTest {
     }
 
     @Test
-    public void makeHttpRequestsShouldReturnNoErrorsIfNotValidImps() {
-        // given
-        final BidRequest bidRequest = givenBidRequest(
-                impBuilder -> impBuilder
-                        .ext(mapper.valueToTree(ExtPrebid.of(null, mapper.createArrayNode()))));
-        // when
-        final Result<List<HttpRequest<BidRequest>>> result = adagioBidder.makeHttpRequests(bidRequest);
-
-        // then
-        assertThat(result.getErrors()).hasSize(0);
-    }
-
-    @Test
     public void makeBidsShouldReturnErrorIfResponseBodyCouldNotBeParsed() {
         // given
         final HttpCall<BidRequest> httpCall = givenHttpCall(null, "invalid");
@@ -90,6 +91,105 @@ public class AdagioBidderTest extends VertxTest {
         assertThat(result.getValue()).isEmpty();
     }
 
+    @Test
+    public void makeBidsShouldReturnEmptyListIfBidResponseIsNull() throws JsonProcessingException {
+        // given
+        final HttpCall<BidRequest> httpCall = givenHttpCall(null, mapper.writeValueAsString(null));
+
+        // when
+        final Result<List<BidderBid>> result = adagioBidder.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeBidsShouldReturnEmptyListIfBidResponseSeatBidIsNull() throws JsonProcessingException {
+        // given
+        final HttpCall<BidRequest> httpCall = givenHttpCall(null,
+                mapper.writeValueAsString(BidResponse.builder().build()));
+
+        // when
+        final Result<List<BidderBid>> result = adagioBidder.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeBidsShouldReturnErrorAndEmptyValuesIfExtIsNull() throws JsonProcessingException {
+        // given
+        final HttpCall<BidRequest> httpCall = givenHttpCall(givenBidRequest(identity()), mapper.writeValueAsString(givenBidResponse(builder -> builder.ext(null))));
+
+        // when
+        final Result<List<BidderBid>> result = adagioBidder.makeBids(httpCall,  null);
+
+        // then
+        assertThat(result.getErrors()).hasSize(1)
+                .allSatisfy(error -> {
+                    assertThat(error.getType()).isEqualTo(BidderError.Type.bad_input);
+                });
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeBidsShouldReturnNoErrorIfExtIsEmpty() throws JsonProcessingException {
+        //givenBidResponse(e-> e.id("123"), e-> e.id("456").ext( mapper.createObjectNode().set("Prebid", mapper.valueToTree(ExtBidPrebid.builder().type(BidType.banner).build()))));
+        // given
+        final HttpCall<BidRequest> httpCall = givenHttpCall(givenBidRequest(identity()), mapper.writeValueAsString(givenBidResponse(builder -> builder.ext(mapper.createObjectNode()))));
+
+        // when
+        final Result<List<BidderBid>> result = adagioBidder.makeBids(httpCall,  null);
+
+        // then
+        assertThat(result.getErrors()).hasSize(0);
+        //assertThat(result.getValue()).hasSize(1);
+    }
+
+    @Test
+    public void makeBidsShouldProccedWithErrorsAndValues() throws JsonProcessingException {
+        // given
+        BidType checkType = BidType.banner;
+        String checkId = "123";
+
+        final HttpCall<BidRequest> httpCall = givenHttpCall(givenBidRequest(identity()),
+                mapper.writeValueAsString(givenBidResponse(
+                        firstBuilder -> firstBuilder
+                                .ext(mapper
+                                        .createObjectNode()
+                                        .set("Prebid", mapper.valueToTree(ExtBidPrebid.builder()
+                                                .type(checkType)
+                                                .build())))
+                                .id(checkId),
+
+                        secondBuilder -> secondBuilder
+                                .ext(mapper
+                                        .createObjectNode()
+                                        .set("Prebid", mapper.valueToTree(ExtBidPrebid.builder()
+                                        .type(checkType)
+                                        .build())))
+                                .id(checkId),
+
+                        thirdBuilder -> thirdBuilder.ext(null))));
+
+        // when
+        final Result<List<BidderBid>> result = adagioBidder.makeBids(httpCall,  null);
+
+        // then
+        assertThat(result.getErrors()).hasSize(1)
+                .allSatisfy(error -> {
+                    assertThat(error.getType()).isEqualTo(BidderError.Type.bad_input);
+                });
+
+        assertThat(result.getValue()).hasSize(2)
+                .allSatisfy(value -> {
+                    assertThat(value.getType()).isEqualTo(checkType);
+                    assertThat(value.getBid().getId()).isEqualTo(checkId);
+                });
+    }
+
     private static BidRequest givenBidRequest(
             Function<BidRequest.BidRequestBuilder, BidRequest.BidRequestBuilder> bidRequestCustomizer,
             Function<Imp.ImpBuilder, Imp.ImpBuilder> impCustomizer) {
@@ -99,8 +199,20 @@ public class AdagioBidderTest extends VertxTest {
                 .build();
     }
 
+
     private static BidRequest givenBidRequest(Function<Imp.ImpBuilder, Imp.ImpBuilder> impCustomizer) {
         return givenBidRequest(identity(), impCustomizer);
+    }
+
+    private static BidResponse givenBidResponse(UnaryOperator<Bid.BidBuilder>... bidCustomizers) {
+        return BidResponse.builder()
+                .cur("USD")
+                .seatbid(singletonList(SeatBid.builder()
+                        .bid(Arrays.stream(bidCustomizers)
+                                .map(bidCustomizer -> bidCustomizer.apply(Bid.builder()).build())
+                                .collect(Collectors.toList()))
+                        .build()))
+                .build();
     }
 
     private static Imp givenImp(Function<Imp.ImpBuilder, Imp.ImpBuilder> impCustomizer) {
