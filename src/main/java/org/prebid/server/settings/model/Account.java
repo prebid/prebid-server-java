43a2040c9c29016302e7e2f19ab1d9635d76f3a8
Sergii Chernysh <schernysh@rubiconproject.com>
2021-08-17 14:28:10 +0300

    Remove obsolete code related to old implementation of default account configuration support

--- src/main/java/org/prebid/server/settings/model/Account.java
+++ src/main/java/org/prebid/server/settings/model/Account.java
@@ -3,7 +3,6 @@ package org.prebid.server.settings.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
-import org.apache.commons.lang3.ObjectUtils;
 
 @Builder(toBuilder = true)
 @Value
@@ -23,17 +22,6 @@ public class Account {
     AccountCookieSyncConfig cookieSync;
 
     AccountHooksConfiguration hooks;
-
-    public Account merge(Account another) {
-        return Account.builder()
-                .id(ObjectUtils.defaultIfNull(id, another.id))
-                .status(ObjectUtils.defaultIfNull(status, another.status))
-                .auction(ObjectUtils.defaultIfNull(auction, another.auction))
-                .privacy(ObjectUtils.defaultIfNull(privacy, another.privacy))
-                .analytics(ObjectUtils.defaultIfNull(analytics, another.analytics))
-                .cookieSync(ObjectUtils.defaultIfNull(cookieSync, another.cookieSync))
-                .build();
-    }
 
     public static Account empty(String id) {
         return Account.builder()
