03fb3dcac94577d7242729e9800cfcba4e2344e1
Alex Maltsev <and1sscsgo@gmail.com>
2021-09-13 11:15:08 +0300

    Removed unused imports

--- src/main/java/org/prebid/server/bidder/visx/VisxBidder.java
+++ src/main/java/org/prebid/server/bidder/visx/VisxBidder.java
@@ -20,7 +20,6 @@ import org.prebid.server.json.JacksonMapper;
 import org.prebid.server.proto.openrtb.ext.response.BidType;
 import org.prebid.server.util.HttpUtil;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
