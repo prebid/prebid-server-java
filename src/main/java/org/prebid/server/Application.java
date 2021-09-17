09d9e8adf78a513f2e35b55a330a9f9b6836d52d
Alex Maltsev <and1sscsgo@gmail.com>
2021-09-17 14:28:11 +0300

    Removed redundant dependencies

--- src/main/java/org/prebid/server/Application.java
+++ src/main/java/org/prebid/server/Application.java
@@ -1,8 +1,5 @@
 package org.prebid.server;
 
-import com.iab.openrtb.request.BidRequest;
-import io.protostuff.Schema;
-import io.protostuff.runtime.RuntimeSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
