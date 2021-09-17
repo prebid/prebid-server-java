be2a807fdbbc9f62a4e50f93ffbe9e060644b859
rpanchyk <rpanchyk@users.noreply.github.com>
2021-09-17 14:57:27 +0300

    Proposal to check adapter-defaults (#1402)
    * Proposal to check adapter-defaults
    
    * Removed BPP

--- src/main/java/org/prebid/server/spring/config/bidder/model/DefaultBidderConfigurationProperties.java
+++ src/main/java/org/prebid/server/spring/config/bidder/model/DefaultBidderConfigurationProperties.java
@@ -3,23 +3,30 @@ package org.prebid.server.spring.config.bidder.model;
 import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 @Validated
 @Data
-public class CommonBidderConfigurationProperties {
+public class DefaultBidderConfigurationProperties {
 
+    @NotNull
     private Boolean enabled;
 
+    @NotNull
     private Boolean pbsEnforcesGdpr;
 
+    @NotNull
     private Boolean pbsEnforcesCcpa;
 
+    @NotNull
     private Boolean modifyingVastXmlAllowed;
 
+    @NotNull
     private List<String> deprecatedNames;
 
+    @NotNull
     private Map<String, Object> aliases = Collections.emptyMap();
 }
