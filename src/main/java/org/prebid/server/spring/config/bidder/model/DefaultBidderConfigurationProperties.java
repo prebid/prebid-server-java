8f0c99930e97892745660156b3e4e578ae927fba
Alex Maltsev <and1sscsgo@gmail.com>
2021-09-22 11:49:49 +0300

    Extracted implicit bidder configuration defaults to DefaultBidderConfigurationProperties

--- src/main/java/org/prebid/server/spring/config/bidder/model/DefaultBidderConfigurationProperties.java
+++ src/main/java/org/prebid/server/spring/config/bidder/model/DefaultBidderConfigurationProperties.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Validated
 @Data
@@ -20,4 +23,10 @@ public class DefaultBidderConfigurationProperties {
 
     @NotNull
     private Boolean modifyingVastXmlAllowed;
+
+    private final Map<String, Object> aliases = Collections.emptyMap();
+
+    private final List<String> deprecatedNames = Collections.emptyList();
+
+    private final Map<String, String> extraInfo = Collections.emptyMap();
 }
