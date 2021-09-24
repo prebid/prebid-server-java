bef6671bf5d56eace9a5030b68bde1d81cb1d3bc
Alex Maltsev <and1sscsgo@gmail.com>
2021-09-24 20:27:08 +0300

    Fixed integration test, updated properties

--- src/test/java/org/prebid/server/it/ImprovedigitalTest.java
+++ src/test/java/org/prebid/server/it/ImprovedigitalTest.java
@@ -20,36 +20,19 @@ public class ImprovedigitalTest extends IntegrationTest {
 
     @Test
     public void openrtb2AuctionShouldRespondWithBidsFromImproveDigital() throws IOException, JSONException {
-        // #1
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/improvedigital-exchange"))
                 .withRequestBody(equalToJson(
-                        jsonFrom("openrtb2/improvedigital/test-improvedigital-bid-request-1.json")))
+                        jsonFrom("openrtb2/improvedigital/test-improvedigital-bid-request.json")))
                 .willReturn(aResponse().withBody(
-                        jsonFrom("openrtb2/improvedigital/test-improvedigital-bid-response-1.json"))));
+                        jsonFrom("openrtb2/improvedigital/test-improvedigital-bid-response.json"))));
 
         // when
-        final Response response1 = responseFor("openrtb2/improvedigital/test-auction-improvedigital-request-1.json",
+        final Response response = responseFor("openrtb2/improvedigital/test-auction-improvedigital-request.json",
                 Endpoint.openrtb2_auction);
 
         // then
-        assertJsonEquals("openrtb2/improvedigital/test-auction-improvedigital-response-1.json", response1,
-                singletonList("improvedigital"));
-
-        // #2
-        // given
-        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/improvedigital-exchange"))
-                .withRequestBody(equalToJson(
-                        jsonFrom("openrtb2/improvedigital/test-improvedigital-bid-request-2.json")))
-                .willReturn(aResponse().withBody(
-                        jsonFrom("openrtb2/improvedigital/test-improvedigital-bid-response-2.json"))));
-
-        // when
-        final Response response2 = responseFor("openrtb2/improvedigital/test-auction-improvedigital-request-2.json",
-                Endpoint.openrtb2_auction);
-
-        // then
-        assertJsonEquals("openrtb2/improvedigital/test-auction-improvedigital-response-2.json", response2,
+        assertJsonEquals("openrtb2/improvedigital/test-auction-improvedigital-response.json", response,
                 singletonList("improvedigital"));
     }
 }
