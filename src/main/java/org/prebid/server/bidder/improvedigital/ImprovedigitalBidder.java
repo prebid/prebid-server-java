c9e3544e793df35f9e954381ed5c2c2000d99bc4
Serhii Nahornyi <snahornyi@rubiconproject.com>
2021-09-27 16:25:36 +0300

    ImproveDigital: Split request

--- src/main/java/org/prebid/server/bidder/improvedigital/ImprovedigitalBidder.java
+++ src/main/java/org/prebid/server/bidder/improvedigital/ImprovedigitalBidder.java
@@ -50,9 +50,11 @@ public class ImprovedigitalBidder implements Bidder<BidRequest> {
     @Override
     public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest request) {
         final List<BidderError> errors = new ArrayList<>();
+        final List<HttpRequest<BidRequest>> httpRequests = new ArrayList<>();
         for (Imp imp : request.getImp()) {
             try {
                 parseAndValidateImpExt(imp);
+                httpRequests.add(resolveRequest(request, imp));
             } catch (PreBidException e) {
                 errors.add(BidderError.badInput(e.getMessage()));
             }
@@ -62,13 +64,7 @@ public class ImprovedigitalBidder implements Bidder<BidRequest> {
             return Result.withErrors(errors);
         }
 
-        return Result.withValue(HttpRequest.<BidRequest>builder()
-                .method(HttpMethod.POST)
-                .uri(endpointUrl)
-                .headers(HttpUtil.headers())
-                .payload(request)
-                .body(mapper.encode(request))
-                .build());
+        return Result.withValues(httpRequests);
     }
 
     private void parseAndValidateImpExt(Imp imp) {
@@ -83,6 +79,20 @@ public class ImprovedigitalBidder implements Bidder<BidRequest> {
         if (placementId == null) {
             throw new PreBidException("No placementId provided");
         }
+    }
+
+    private HttpRequest<BidRequest> resolveRequest(BidRequest bidRequest, Imp imp) {
+        final BidRequest modifiedRequest = bidRequest.toBuilder()
+                .imp(Collections.singletonList(imp))
+                .build();
+
+        return HttpRequest.<BidRequest>builder()
+                .method(HttpMethod.POST)
+                .uri(endpointUrl)
+                .headers(HttpUtil.headers())
+                .payload(modifiedRequest)
+                .body(mapper.encode(modifiedRequest))
+                .build();
     }
 
     @Override
