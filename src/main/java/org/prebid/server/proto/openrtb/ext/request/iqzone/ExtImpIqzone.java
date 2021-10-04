5afe346f4cd02f24b7c67349019c2d970be5e224
Alex Maltsev <and1sscsgo@gmail.com>
2021-10-04 16:12:52 +0300

    Removed unused import

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/iqzone/ExtImpIqzone.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/iqzone/ExtImpIqzone.java
@@ -1,7 +1,6 @@
 package org.prebid.server.proto.openrtb.ext.request.iqzone;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
