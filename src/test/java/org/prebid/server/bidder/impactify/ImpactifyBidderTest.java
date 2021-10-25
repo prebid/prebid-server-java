300e7db5e9bc8ebdad81967671c70a24baa2ce76
Yevhenii Viktorov <eugeniy.viktorov@gmail.com>
2021-10-25 18:56:39 +0300

    Add test for currency conversion

--- src/test/java/org/prebid/server/bidder/impactify/ImpactifyBidderTest.java
+++ src/test/java/org/prebid/server/bidder/impactify/ImpactifyBidderTest.java
@@ -1,9 +1,13 @@
 package org.prebid.server.bidder.impactify;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.iab.openrtb.request.Banner;
 import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Imp;
 import com.iab.openrtb.request.Video;
+import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,18 +15,27 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.prebid.server.VertxTest;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.HttpCall;
 import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.HttpResponse;
 import org.prebid.server.bidder.model.Result;
 import org.prebid.server.currency.CurrencyConversionService;
 import org.prebid.server.proto.openrtb.ext.ExtPrebid;
 import org.prebid.server.proto.openrtb.ext.request.impactify.ExtImpImpactify;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.function.Function;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
 public class ImpactifyBidderTest extends VertxTest {
 
@@ -50,6 +63,7 @@ public class ImpactifyBidderTest extends VertxTest {
 
     private static Imp givenImpressionWithValidData() {
         return Imp.builder()
+                .id("123")
                 .bidfloorcur("USD")
                 .bidfloor(BigDecimal.ONE)
                 .banner(Banner.builder().build())
@@ -59,8 +73,37 @@ public class ImpactifyBidderTest extends VertxTest {
                 .build();
     }
 
+
+    private static Imp givenImpressionWithNonBidderCurrency() {
+        return Imp.builder()
+                .id("123")
+                .bidfloorcur("EUR")
+                .bidfloor(BigDecimal.ONE)
+                .banner(Banner.builder().build())
+                .video(Video.builder().build())
+                .ext(mapper.valueToTree(ExtPrebid.of(
+                        ExtImpImpactify.of("appId", "format", "style"), null)))
+                .build();
+    }
+
+    private static HttpCall<BidRequest> givenHttpCall(BidRequest bidRequest, String body) {
+        return HttpCall.success(
+                HttpRequest.<BidRequest>builder().payload(bidRequest).build(),
+                HttpResponse.of(200, null, body),
+                null);
+    }
+
+    private static BidResponse givenBidResponse(Function<Bid.BidBuilder, Bid.BidBuilder> bidCustomizer) {
+        return BidResponse.builder()
+                .cur("USD")
+                .seatbid(singletonList(SeatBid.builder()
+                        .bid(singletonList(bidCustomizer.apply(Bid.builder()).build()))
+                        .build()))
+                .build();
+    }
+
     @Test
-    public void makeHttpRequestsShouldCheckIfImpressionHasCorrectBidFloorAndBidFloorCur() {
+    public void makeHttpRequestsShouldCheckIfValidDataInImpressionHasCorrectBidFloorAndBidFloorCur() {
         // given
         final BidRequest bidRequest = BidRequest.builder()
                 .imp(List.of(givenImpressionWithValidData()))
@@ -76,5 +119,56 @@ public class ImpactifyBidderTest extends VertxTest {
                 .flatExtracting(BidRequest::getImp)
                 .extracting(Imp::getBidfloor, Imp::getBidfloorcur)
                 .containsExactly(tuple(BigDecimal.ONE, "USD"));
+    }
+
+    @Test
+    public void makeHttpRequestsShouldCheckIfImpressionHasCorrectBidFloorAndBidFloorCurChangeItOtherwise() {
+        // given
+        given(currencyConversionService.convertCurrency(any(), any(), anyString(), anyString()))
+                .willReturn(BigDecimal.TEN);
+        final BidRequest bidRequest = BidRequest.builder()
+                .imp(List.of(givenImpressionWithNonBidderCurrency()))
+                .build();
+
+        //when
+        Result<List<HttpRequest<BidRequest>>> result = impactifyBidder.makeHttpRequests(bidRequest);
+
+        //then
+        assertThat(result.getErrors()).hasSize(0);
+        assertThat(result.getValue()).hasSize(1)
+                .extracting(httpRequest -> mapper.readValue(httpRequest.getBody(), BidRequest.class))
+                .flatExtracting(BidRequest::getImp)
+                .extracting(Imp::getBidfloor, Imp::getBidfloorcur)
+                .containsExactly(tuple(BigDecimal.TEN, "USD"));
+    }
+
+    @Test
+    public void makeBidsShouldReturnValidBidResponse() throws JsonProcessingException {
+        //given
+        final BidRequest bidRequest = BidRequest.builder()
+                .imp(List.of(givenImpressionWithValidData()))
+                .build();
+
+        final HttpCall<BidRequest> httpCall = givenHttpCall(
+                bidRequest,
+                mapper.writeValueAsString(
+                        givenBidResponse(bidBuilder -> bidBuilder.impid("123"))));
+
+        //when
+        final Result<List<BidderBid>> result = impactifyBidder.makeBids(httpCall, bidRequest);
+
+        //then
+        assertThat(result.getErrors()).hasSize(0);
+        assertThat(result.getValue())
+                .extracting(BidderBid::getBid)
+                .containsOnly(Bid.builder()
+                        .impid("123")
+                        .build());
+
+        assertThat(result.getValue())
+                .extracting(BidderBid::getType)
+                .containsExactly(BidType.banner);
+
+
     }
 }
