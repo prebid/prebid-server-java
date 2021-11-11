ff25499fe441be7b1717de30f9d0385f934eff44
mtuchkova <mtuchkova@magnite.com>
2021-11-11 16:18:43 +0200

    Update tests for PBC metrics

--- src/test/groovy/org/prebid/server/functional/BaseSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/BaseSpec.groovy
@@ -53,4 +53,16 @@ abstract class BaseSpec extends Specification {
     protected static int getRandomTimeout() {
         PBSUtils.getRandomNumber(MIN_TIMEOUT, MAX_TIMEOUT)
     }
+
+    protected static int getCurrentMetricValue(String name) {
+        def response = defaultPbsService.sendCollectedMetricsRequest()
+        response[name] as Integer ?: 0
+    }
+
+    protected static void flushMetrics() {
+        // flushing PBS metrics by receiving collected metrics so that each new test works with a fresh state
+        defaultPbsService.sendCollectedMetricsRequest()
+    }
+
+
 }
