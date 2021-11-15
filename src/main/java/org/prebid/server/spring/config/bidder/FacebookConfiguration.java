28ef392dbe86e6bbc8dc6442074acad7d415d240
Alex Maltsev <and1sscsgo@gmail.com>
2021-11-15 14:37:24 +0200

    Actualize Appnexus bidder changes (#1570)

--- src/main/java/org/prebid/server/spring/config/bidder/FacebookConfiguration.java
+++ src/main/java/org/prebid/server/spring/config/bidder/FacebookConfiguration.java
@@ -3,6 +3,7 @@ package org.prebid.server.spring.config.bidder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.prebid.server.bidder.Bidder;
 import org.prebid.server.bidder.BidderDeps;
 import org.prebid.server.bidder.facebook.FacebookBidder;
 import org.prebid.server.json.JacksonMapper;
@@ -10,17 +11,15 @@ import org.prebid.server.spring.config.bidder.model.BidderConfigurationPropertie
 import org.prebid.server.spring.config.bidder.util.BidderDepsAssembler;
 import org.prebid.server.spring.config.bidder.util.UsersyncerCreator;
 import org.prebid.server.spring.env.YamlPropertySourceFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.function.Function;
 
 @Configuration
 @PropertySource(value = "classpath:/bidder-config/facebook.yaml", factory = YamlPropertySourceFactory.class)
@@ -28,34 +27,22 @@ public class FacebookConfiguration {
 
     private static final String BIDDER_NAME = "audienceNetwork";
 
-    @Autowired
-    private JacksonMapper mapper;
-
-    @Autowired
-    @Qualifier("facebookConfigurationProperties")
-    private FacebookConfigurationProperties configProperties;
-
-    @Bean("facebookConfigurationProperties")
-    @ConfigurationProperties("adapters.facebook")
-    FacebookConfigurationProperties configurationProperties() {
-        return new FacebookConfigurationProperties();
-    }
-
     @Bean
-    BidderDeps facebookBidderDeps(BidderConfigurationProperties facebookConfigurationProperties,
-                                  @NotBlank @Value("${external-url}") String externalUrl,
+    BidderDeps facebookBidderDeps(FacebookConfigurationProperties facebookConfigurationProperties,
                                   JacksonMapper mapper) {
 
-        return BidderDepsAssembler.<FacebookConfigurationProperties>forBidder(BIDDER_NAME)
-                .withConfig(configProperties)
-                .usersyncerCreator(UsersyncerCreator.create(null))
-                .bidderCreator(configProperties.getEnabled()
-                        ? config -> new FacebookBidder(
+        final Function<FacebookConfigurationProperties, Bidder<?>> bidderCreator =
+                config -> new FacebookBidder(
                         config.getEndpoint(),
                         config.getPlatformId(),
                         config.getAppSecret(),
-                        configProperties.getTimeoutNotificationUrlTemplate(), mapper)
-                        : null)
+                        facebookConfigurationProperties.getTimeoutNotificationUrlTemplate(),
+                        mapper);
+
+        return BidderDepsAssembler.<FacebookConfigurationProperties>forBidder(BIDDER_NAME)
+                .withConfig(facebookConfigurationProperties)
+                .usersyncerCreator(UsersyncerCreator.create(null))
+                .bidderCreator(facebookConfigurationProperties.getEnabled() ? bidderCreator : null)
                 .assemble();
     }
 
@@ -63,6 +50,8 @@ public class FacebookConfiguration {
     @Data
     @EqualsAndHashCode(callSuper = true)
     @NoArgsConstructor
+    @Component("facebookConfigurationProperties")
+    @ConfigurationProperties("adapters.facebook")
     private static class FacebookConfigurationProperties extends BidderConfigurationProperties {
 
         @NotNull
