c879b767afe091d439c63f24b3834cbd4e002b80
Serhii Nahornyi <snahornyi@rubiconproject.com>
2021-11-24 19:47:09 +0200

    Price Floors: Model requirements updates

--- src/main/java/org/prebid/server/floors/model/PriceFloorData.java
+++ src/main/java/org/prebid/server/floors/model/PriceFloorData.java
@@ -3,9 +3,7 @@ package org.prebid.server.floors.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
 
 @Value
 public class PriceFloorData {
@@ -26,8 +24,4 @@ public class PriceFloorData {
 
     @JsonProperty("modelGroups")
     List<PriceFloorModelGroup> modelGroups;
-
-    PriceFloorSchema schema;
-
-    Map<String, BigDecimal> values;
 }
