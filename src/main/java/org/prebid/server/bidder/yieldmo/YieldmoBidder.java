8a6a4c01c7e6f67ed721823183f785843b679654
Dubyk Danylo <pointbay2001@gmail.com>
2021-12-09 15:25:25 +0200

    Rid Yieldmo bidder of OpenrtbBidder.

--- src/main/java/org/prebid/server/bidder/yieldmo/YieldmoBidder.java
+++ src/main/java/org/prebid/server/bidder/yieldmo/YieldmoBidder.java
@@ -1,52 +1,130 @@
 package org.prebid.server.bidder.yieldmo;
 
+import com.fasterxml.jackson.core.JsonPointer;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Imp;
-import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
+import io.vertx.core.http.HttpMethod;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.prebid.server.bidder.OpenrtbBidder;
+import org.prebid.server.bidder.Bidder;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpCall;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
 import org.prebid.server.bidder.yieldmo.proto.YieldmoImpExt;
 import org.prebid.server.exception.PreBidException;
+import org.prebid.server.json.DecodeException;
 import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.proto.openrtb.ext.ExtPrebid;
 import org.prebid.server.proto.openrtb.ext.request.yieldmo.ExtImpYieldmo;
 import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.util.HttpUtil;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class YieldmoBidder extends OpenrtbBidder<ExtImpYieldmo> {
+public class YieldmoBidder implements Bidder<BidRequest> {
+
+    private static final JsonPointer EXT_DATA_PBADSLOT_POINTER_EXPRESSION = JsonPointer.valueOf("/data/pbadslot");
+    private static final TypeReference<ExtPrebid<?, ExtImpYieldmo>> YIELDMO_EXT_TYPE_REFERENCE =
+            new TypeReference<>() {
+            };
+
+    private final String endpointUrl;
+    private final JacksonMapper mapper;
 
     public YieldmoBidder(String endpointUrl, JacksonMapper mapper) {
-        super(endpointUrl, RequestCreationStrategy.SINGLE_REQUEST, ExtImpYieldmo.class, mapper);
+        this.endpointUrl = HttpUtil.validateUrl(Objects.requireNonNull(endpointUrl));
+        this.mapper = Objects.requireNonNull(mapper);
     }
 
     @Override
-    protected Imp modifyImp(Imp imp, ExtImpYieldmo impExt) throws PreBidException {
-        final Imp.ImpBuilder modifiedImp = imp.toBuilder();
+    public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest bidRequest) {
+        final List<Imp> modifiedImps = new ArrayList<>();
+        final List<BidderError> errors = new ArrayList<>();
 
-        final JsonNode pbadslotNode = imp.getExt().at("/data/pbadslot");
-        final String gpid = !pbadslotNode.isMissingNode()
-                ? StringUtils.defaultIfEmpty(pbadslotNode.asText(), null)
-                : null;
+        for (Imp imp : bidRequest.getImp()) {
+            try {
+                final ExtImpYieldmo impExt = parseImpExt(imp);
+                modifiedImps.add(modifyImp(imp, impExt));
+            } catch (PreBidException e) {
+                errors.add(BidderError.badInput(e.getMessage()));
+            }
+        }
 
+        if (modifiedImps.isEmpty()) {
+            return Result.withErrors(errors);
+        }
+
+        final BidRequest modifiedRequest = bidRequest.toBuilder().imp(modifiedImps).build();
+        return Result.of(Collections.singletonList(makeRequest(modifiedRequest)), errors);
+    }
+
+    private ExtImpYieldmo parseImpExt(Imp imp) throws PreBidException {
         try {
-            modifiedImp.ext(mapper.mapper().valueToTree(YieldmoImpExt.of(impExt.getPlacementId(), gpid)));
+            return mapper.mapper().convertValue(imp.getExt(), YIELDMO_EXT_TYPE_REFERENCE).getBidder();
         } catch (IllegalArgumentException e) {
             throw new PreBidException(e.getMessage(), e);
         }
+    }
 
-        return modifiedImp.build();
+    private Imp modifyImp(Imp imp, ExtImpYieldmo ext) {
+        final JsonNode pbadslotNode = imp.getExt().at(EXT_DATA_PBADSLOT_POINTER_EXPRESSION);
+        final String gpid = StringUtils.defaultIfBlank(pbadslotNode.asText(), null);
+        final YieldmoImpExt modifiedExt = YieldmoImpExt.of(ext.getPlacementId(), gpid);
+        return imp.toBuilder().ext(mapper.mapper().valueToTree(modifiedExt)).build();
+    }
+
+    private HttpRequest<BidRequest> makeRequest(BidRequest bidRequest) {
+        return HttpRequest.<BidRequest>builder()
+                .method(HttpMethod.POST)
+                .headers(HttpUtil.headers())
+                .uri(endpointUrl)
+                .body(mapper.encodeToBytes(bidRequest))
+                .payload(bidRequest)
+                .build();
     }
 
     @Override
-    protected BidType getBidType(Bid bid, List<Imp> imps) {
-        final String impId = bid.getImpid();
-        for (Imp imp : imps) {
-            if (imp.getId().equals(impId)) {
-                if (imp.getBanner() != null) {
-                    return BidType.banner;
-                }
-            }
+    public final Result<List<BidderBid>> makeBids(HttpCall<BidRequest> httpCall, BidRequest bidRequest) {
+        try {
+            final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
+            return Result.withValues(extractBids(httpCall.getRequest().getPayload(), bidResponse));
+        } catch (DecodeException | PreBidException e) {
+            return Result.withError(BidderError.badServerResponse(e.getMessage()));
         }
-        return BidType.video;
+    }
+
+    private static List<BidderBid> extractBids(BidRequest bidRequest, BidResponse bidResponse) {
+        if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
+            return Collections.emptyList();
+        }
+
+        final String currency = bidResponse.getCur();
+        final Map<String, BidType> impIdToBidType = bidRequest.getImp().stream()
+                .collect(Collectors.toMap(Imp::getId, YieldmoBidder::resolveBidType));
+
+        return bidResponse.getSeatbid().stream()
+                .filter(Objects::nonNull)
+                .map(SeatBid::getBid)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .map(bid -> BidderBid.of(bid, impIdToBidType.getOrDefault(bid.getImpid(), BidType.video), currency))
+                .collect(Collectors.toList());
+    }
+
+    private static BidType resolveBidType(Imp imp) {
+        return imp.getBanner() != null ? BidType.banner : BidType.video;
     }
 }
