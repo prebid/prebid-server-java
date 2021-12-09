14244ec71094b0538aa4abdb51d1d19e6d0a01a8
rpanchyk <rpanchyk@users.noreply.github.com>
2021-12-09 14:10:06 +0200

    Price Floors: Bidder enforcement proposal (#1614)

--- src/main/java/org/prebid/server/util/BidderUtil.java
+++ src/main/java/org/prebid/server/util/BidderUtil.java
@@ -1,6 +1,15 @@
 package org.prebid.server.util;
 
+import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.response.Bid;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.prebid.server.bidder.model.PriceFloorInfo;
+
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
 
 public class BidderUtil {
 
@@ -9,5 +18,32 @@ public class BidderUtil {
 
     public static boolean isValidPrice(BigDecimal price) {
         return price != null && price.compareTo(BigDecimal.ZERO) > 0;
+    }
+
+    public static PriceFloorInfo resolvePriceFloor(Bid bid, BidRequest bidRequest) {
+        final String bidImpId = ObjectUtil.getIfNotNull(bid, Bid::getImpid);
+        if (StringUtils.isEmpty(bidImpId)) {
+            return null;
+        }
+
+        final List<Imp> imps = ObjectUtil.getIfNotNull(bidRequest, BidRequest::getImp);
+        if (CollectionUtils.isEmpty(imps)) {
+            return null;
+        }
+
+        return imps.stream()
+                .filter(imp -> Objects.equals(bidImpId, imp.getId()))
+                .findFirst()
+                .map(BidderUtil::createFloorInfo)
+                .orElse(null);
+    }
+
+    private static PriceFloorInfo createFloorInfo(Imp imp) {
+        final BigDecimal floor = imp.getBidfloor();
+        final String currency = imp.getBidfloorcur();
+
+        return floor != null || currency != null
+                ? PriceFloorInfo.of(floor, currency)
+                : null;
     }
 }
