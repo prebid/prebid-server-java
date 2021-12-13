653ad3b811c91f13c62c0ed1000354366c776185
Dubyk Danylo <pointbay2001@gmail.com>
2021-12-13 14:03:03 +0200

    Resolve conversations

--- src/main/java/org/prebid/server/bidder/rtbhouse/RtbhouseBidder.java
+++ src/main/java/org/prebid/server/bidder/rtbhouse/RtbhouseBidder.java
@@ -1,7 +1,6 @@
 package org.prebid.server.bidder.rtbhouse;
 
 import com.iab.openrtb.request.BidRequest;
-import com.iab.openrtb.request.Imp;
 import com.iab.openrtb.response.BidResponse;
 import com.iab.openrtb.response.SeatBid;
 import io.vertx.core.http.HttpMethod;
@@ -20,7 +19,6 @@ import org.prebid.server.util.HttpUtil;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -49,20 +47,16 @@ public class RtbhouseBidder implements Bidder<BidRequest> {
     public Result<List<BidderBid>> makeBids(HttpCall<BidRequest> httpCall, BidRequest bidRequest) {
         try {
             final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
-            return Result.withValues(extractBids(httpCall.getRequest().getPayload(), bidResponse));
+            return Result.withValues(extractBids(bidResponse));
         } catch (DecodeException e) {
             return Result.withError(BidderError.badServerResponse(e.getMessage()));
         }
     }
 
-    private static List<BidderBid> extractBids(BidRequest bidRequest, BidResponse bidResponse) {
+    private static List<BidderBid> extractBids(BidResponse bidResponse) {
         if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
             return Collections.emptyList();
         }
-
-        final String currency = bidResponse.getCur();
-        final Map<String, BidType> impIdToBidType = bidRequest.getImp().stream()
-                .collect(Collectors.toMap(Imp::getId, RtbhouseBidder::resolveBidType));
 
         return bidResponse.getSeatbid().stream()
                 .filter(Objects::nonNull)
@@ -70,15 +64,7 @@ public class RtbhouseBidder implements Bidder<BidRequest> {
                 .filter(Objects::nonNull)
                 .flatMap(Collection::stream)
                 .filter(Objects::nonNull)
-                .map(bid -> BidderBid.of(bid, impIdToBidType.getOrDefault(bid.getImpid(), BidType.banner), currency))
+                .map(bid -> BidderBid.of(bid, BidType.banner, bidResponse.getCur()))
                 .collect(Collectors.toList());
-    }
-
-    private static BidType resolveBidType(Imp imp) {
-        return imp.getBanner() != null ? BidType.banner
-                : imp.getVideo() != null ? BidType.video
-                : imp.getXNative() != null ? BidType.xNative
-                : imp.getAudio() != null ? BidType.audio
-                : BidType.banner;
     }
 }
