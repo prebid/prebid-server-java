710001dfe29a37476ca9b945fc083708f7ece9b9
rpanchyk <rpanchyk@users.noreply.github.com>
2021-12-15 16:04:43 +0200

    Fix Stored vs Default request merging (#1629)

--- src/test/groovy/org/prebid/server/functional/tests/AuctionSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/AuctionSpec.groovy
@@ -4,10 +4,13 @@ import org.prebid.server.functional.model.db.StoredRequest
 import org.prebid.server.functional.model.request.auction.BidRequest
 import org.prebid.server.functional.model.request.auction.PrebidStoredRequest
 import org.prebid.server.functional.service.PrebidServerService
+import org.prebid.server.functional.testcontainers.container.PrebidServerContainer
 import org.prebid.server.functional.util.PBSUtils
+import org.testcontainers.utility.MountableFile
 import spock.lang.Shared
 import spock.lang.Unroll
 
+import static org.prebid.server.functional.testcontainers.container.PrebidServerContainer.APP_WORKDIR
 import static org.prebid.server.functional.util.SystemProperties.PBS_VERSION
 
 class AuctionSpec extends BaseSpec {
@@ -19,7 +22,6 @@ class AuctionSpec extends BaseSpec {
     PrebidServerService prebidServerService = pbsServiceFactory.getService(["auction.max-timeout-ms"    : MAX_TIMEOUT as String,
                                                                             "auction.default-timeout-ms": DEFAULT_TIMEOUT as String])
 
-    @Unroll
     def "PBS should return version in response header for auction request for #description"() {
 
         when: "PBS processes auction request"
@@ -38,7 +40,7 @@ class AuctionSpec extends BaseSpec {
         given: "Default basic BidRequest with generic bidder"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             tmax = null
-            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber.toString())
+            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber)
         }
 
         and: "Default stored request with timeout"
@@ -59,13 +61,12 @@ class AuctionSpec extends BaseSpec {
         assert bidderRequest.tmax == timeout as Long
     }
 
-    @Unroll
     def "PBS should prefer timeout from the auction request"() {
         given: "Default basic BidRequest with generic bidder"
         def timeout = getRandomTimeout()
         def bidRequest = BidRequest.defaultBidRequest.tap {
             tmax = timeout
-            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber.toString())
+            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber)
         }
 
         and: "Default stored request"
@@ -88,12 +89,11 @@ class AuctionSpec extends BaseSpec {
         tmaxStoredRequest << [null, getRandomTimeout()]
     }
 
-    @Unroll
     def "PBS should honor max timeout from the settings for auction request"() {
         given: "Default basic BidRequest with generic bidder"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             tmax = autcionRequestTimeout
-            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber.toString())
+            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber)
         }
 
         and: "Default stored request"
@@ -123,7 +123,7 @@ class AuctionSpec extends BaseSpec {
         given: "Default basic BidRequest without timeout"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             tmax = null
-            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber.toString())
+            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber)
         }
 
         and: "Default stored request without timeout"
@@ -141,5 +141,50 @@ class AuctionSpec extends BaseSpec {
         then: "Bidder request timeout should correspond to the maximum from the settings"
         def bidderRequest = bidder.getBidderRequest(bidRequest.id)
         assert bidderRequest.tmax == DEFAULT_TIMEOUT as Long
+    }
+
+    def "PBS should take data by priority when request, stored request, default request are defined"() {
+        given: "Default request with timeout"
+        def defaultRequestModel = new BidRequest(tmax: defaultRequestTmax)
+        def defaultRequest = PBSUtils.createJsonFile(defaultRequestModel)
+
+        and: "Pbs config with default request"
+        def pbsContainer = new PrebidServerContainer(["default-request.file.path": APP_WORKDIR +
+                defaultRequest.fileName]).tap {
+            withCopyFileToContainer(MountableFile.forHostPath(defaultRequest), APP_WORKDIR)
+        }
+        pbsContainer.start()
+        def pbsService = new PrebidServerService(pbsContainer, mapper)
+
+        and: "Default basic BidRequest with timeout"
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            tmax = requestTmax
+            ext.prebid.storedRequest = new PrebidStoredRequest(id: PBSUtils.randomNumber)
+        }
+
+        and: "Default stored request with timeout"
+        def storedRequestModel = BidRequest.defaultStoredRequest.tap {
+            tmax = storedRequestTmax as Long
+        }
+
+        and: "Save storedRequest into DB"
+        def storedRequest = StoredRequest.getDbStoredRequest(bidRequest, storedRequestModel)
+        storedRequestDao.save(storedRequest)
+
+        when: "PBS processes auction request"
+        pbsService.sendAuctionRequest(bidRequest)
+
+        then: "Bidder request should contain correct tmax"
+        def bidderRequest = bidder.getBidderRequest(bidRequest.id)
+        assert bidderRequest.tmax == DEFAULT_TIMEOUT as Long
+
+        cleanup: "Stop container with default request"
+        pbsContainer.stop()
+
+        where:
+        requestTmax     | storedRequestTmax  | defaultRequestTmax
+        DEFAULT_TIMEOUT | getRandomTimeout() | getRandomTimeout()
+        null            | DEFAULT_TIMEOUT    | getRandomTimeout()
+        null            | null               | DEFAULT_TIMEOUT
     }
 }
