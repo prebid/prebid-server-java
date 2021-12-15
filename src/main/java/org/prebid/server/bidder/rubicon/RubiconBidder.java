0b6cd40ad0a646b33561b0b5c3c5499e2b2d501e
Dubyk Danylo <pointbay2001@gmail.com>
2021-12-15 19:03:25 +0200

    Replace immutable collection creation with collection factory call.

--- src/main/java/org/prebid/server/bidder/rubicon/RubiconBidder.java
+++ src/main/java/org/prebid/server/bidder/rubicon/RubiconBidder.java
@@ -148,10 +148,8 @@ public class RubiconBidder implements Bidder<BidRequest> {
     private static final String DMP_STYPE = "dmp";
     private static final String XAPI_CURRENCY = "USD";
 
-    private static final Set<Integer> USER_SEGTAXES = Collections.unmodifiableSet(
-            new HashSet<>(Collections.singletonList(4)));
-    private static final Set<Integer> SITE_SEGTAXES = Collections.unmodifiableSet(
-            new HashSet<>(Arrays.asList(1, 2, 5, 6)));
+    private static final Set<Integer> USER_SEGTAXES = Set.of(4);
+    private static final Set<Integer> SITE_SEGTAXES = Set.of(1, 2, 5, 6);
 
     private static final Set<String> STYPE_TO_REMOVE = new HashSet<>(Arrays.asList(PPUID_STYPE, SHA256EMAIL_STYPE,
             DMP_STYPE));
