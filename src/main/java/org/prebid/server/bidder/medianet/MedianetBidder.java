49995113c547a15e1b991c3b624718c6eb86b4cb
markiian <mmykush@magnite.com>
2021-12-22 13:11:04 +0200

    Refactor Medianet: Extend from Bidder interface

--- src/main/java/org/prebid/server/bidder/medianet/MedianetBidder.java
+++ src/main/java/org/prebid/server/bidder/medianet/MedianetBidder.java
@@ -1,14 +1,90 @@
 package org.prebid.server.bidder.medianet;
 
-import org.prebid.server.bidder.OpenrtbBidder;
+import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
+import io.vertx.core.http.HttpMethod;
+import org.apache.commons.collections4.CollectionUtils;
+import org.prebid.server.bidder.Bidder;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpCall;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
+import org.prebid.server.exception.PreBidException;
+import org.prebid.server.json.DecodeException;
 import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.util.HttpUtil;
 
-public class MedianetBidder extends OpenrtbBidder<Void> {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class MedianetBidder implements Bidder<BidRequest> {
+
+    private final String endpointUrl;
+    private final JacksonMapper mapper;
 
     public MedianetBidder(String endpointUrl, JacksonMapper mapper) {
-        super(endpointUrl,
-                RequestCreationStrategy.SINGLE_REQUEST,
-                Void.class,
-                mapper);
+        this.endpointUrl = HttpUtil.validateUrl(Objects.requireNonNull(endpointUrl));
+        this.mapper = Objects.requireNonNull(mapper);
+    }
+
+    @Override
+    public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest request) {
+        return Result.withValues(Collections.singletonList(
+                HttpRequest.<BidRequest>builder()
+                .method(HttpMethod.POST)
+                .uri(endpointUrl)
+                .headers(HttpUtil.headers())
+                .payload(request)
+                .body(mapper.encodeToBytes(request))
+                .build()));
+    }
+
+    @Override
+    public Result<List<BidderBid>> makeBids(HttpCall<BidRequest> httpCall, BidRequest bidRequest) {
+        try {
+            final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
+            return Result.withValues(extractBids(bidResponse, bidRequest.getImp()));
+        } catch (DecodeException | PreBidException e) {
+            return Result.withError(BidderError.badServerResponse(e.getMessage()));
+        }
+    }
+
+    private List<BidderBid> extractBids(BidResponse bidResponse, List<Imp> imps) {
+        if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
+            return Collections.emptyList();
+        }
+        return bidsFromResponse(bidResponse, imps);
+    }
+
+    private static List<BidderBid> bidsFromResponse(BidResponse bidResponse, List<Imp> imps) {
+        return bidResponse.getSeatbid().stream()
+                .filter(Objects::nonNull)
+                .map(SeatBid::getBid)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .map(bid -> BidderBid.of(bid, getBidType(imps), bidResponse.getCur()))
+                .collect(Collectors.toList());
+    }
+
+    private static BidType getBidType(List<Imp> imps) {
+        for (Imp imp : imps) {
+            if (imp.getBanner() != null) {
+                return BidType.banner;
+            }
+            if (imp.getVideo() != null) {
+                return BidType.video;
+            }
+            if (imp.getXNative() != null) {
+                return BidType.xNative;
+            }
+        }
+        throw new PreBidException(String.format("Failed to find impression %s", imps));
     }
 }
--- src/test/java/org/prebid/server/bidder/medianet/MedianetBidderTest.java
+++ src/test/java/org/prebid/server/bidder/medianet/MedianetBidderTest.java
@@ -1,8 +1,12 @@
 package org.prebid.server.bidder.medianet;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.iab.openrtb.request.Audio;
+import com.iab.openrtb.request.Banner;
 import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.request.Native;
+import com.iab.openrtb.request.Video;
 import com.iab.openrtb.response.Bid;
 import com.iab.openrtb.response.BidResponse;
 import com.iab.openrtb.response.SeatBid;
@@ -10,7 +14,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.prebid.server.VertxTest;
 import org.prebid.server.bidder.model.BidderBid;
-import org.prebid.server.bidder.model.BidderError;
 import org.prebid.server.bidder.model.HttpCall;
 import org.prebid.server.bidder.model.HttpRequest;
 import org.prebid.server.bidder.model.HttpResponse;
@@ -19,11 +22,15 @@ import org.prebid.server.proto.openrtb.ext.ExtPrebid;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.prebid.server.bidder.model.BidderError.Type.bad_server_response;
+import static org.prebid.server.proto.openrtb.ext.response.BidType.xNative;
 import static org.prebid.server.proto.openrtb.ext.response.BidType.banner;
+import static org.prebid.server.proto.openrtb.ext.response.BidType.video;
 
 public class MedianetBidderTest extends VertxTest {
 
@@ -38,8 +45,108 @@ public class MedianetBidderTest extends VertxTest {
 
     @Test
     public void creationShouldFailOnInvalidEndpointUrl() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> new MedianetBidder("invalid_url", jacksonMapper));
+        assertThatIllegalArgumentException().isThrownBy(() -> new MedianetBidder("invalid_url", jacksonMapper));
+    }
+
+    @Test
+    public void makeBidsShouldReturnBannerBidIfBannerIsPresentInRequestImp() throws JsonProcessingException {
+        // given
+        final BidRequest bidRequest = givenBidRequest(impBuilder -> impBuilder
+                .id("123")
+                .banner(Banner.builder().build()));
+        final HttpCall<BidRequest> httpCall = sampleHttpCall(
+                bidRequest,
+                mapper.writeValueAsString(
+                        givenBidResponse(bidBuilder -> bidBuilder.impid("123"))));
+
+        // when
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue())
+                .containsExactly(BidderBid.of(Bid.builder().impid("123").build(), banner, "USD"));
+    }
+
+    @Test
+    public void makeBidsShouldReturnVideoBidIfVideoIsPresentInRequestImp() throws JsonProcessingException {
+        // given
+        final BidRequest bidRequest = givenBidRequest(impBuilder -> impBuilder
+                .id("123")
+                .video(Video.builder().build()));
+        final HttpCall<BidRequest> httpCall = sampleHttpCall(
+                bidRequest,
+                mapper.writeValueAsString(givenBidResponse(bidBuilder -> bidBuilder.impid("123"))));
+
+        // when
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue())
+                .containsExactly(BidderBid.of(Bid.builder().impid("123").build(), video, "USD"));
+    }
+
+    @Test
+    public void makeBidsShouldReturnxNativeBidIfxNativeIsPresentInRequestImp() throws JsonProcessingException {
+        // given
+        final BidRequest bidRequest = givenBidRequest(impBuilder -> impBuilder
+                .id("123")
+                .xNative(Native.builder().build()));
+        final HttpCall<BidRequest> httpCall = sampleHttpCall(
+                bidRequest,
+                mapper.writeValueAsString(givenBidResponse(bidBuilder -> bidBuilder.impid("123"))));
+
+        // when
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue())
+                .containsExactly(BidderBid.of(Bid.builder().impid("123").build(), xNative, "USD"));
+    }
+
+    @Test
+    public void makeBidsShouldReturnErrorPreBidExceptionIfNotFoundAnyImpressions() throws JsonProcessingException {
+        // given
+        final BidRequest bidRequest = givenBidRequest(impBuilder -> impBuilder
+                .id("123")
+                .audio(Audio.builder().build()));
+        final HttpCall<BidRequest> httpCall = sampleHttpCall(
+                bidRequest,
+                mapper.writeValueAsString(givenBidResponse(bidBuilder -> bidBuilder.impid("123"))));
+
+        // when
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, bidRequest);
+
+        // then
+        assertThat(result.getValue()).isEmpty();
+        assertThat(result.getErrors()).hasSize(1)
+                .allSatisfy(error -> {
+                    assertThat(error.getType()).isEqualTo(bad_server_response);
+                    assertThat(error.getMessage()).startsWith("Failed to find impression");
+                });
+    }
+
+    @Test
+    public void makeBitsShouldReturnEmptyValueIfSeatBitNotPresent() throws JsonProcessingException {
+        // given
+        final BidRequest bidRequest = givenBidRequest(impBuilder -> impBuilder
+                .id("123")
+                .video(Video.builder().build()));
+        final HttpCall<BidRequest> httpCall = sampleHttpCall(
+                bidRequest,
+                mapper.writeValueAsString(BidResponse.builder()
+                        .cur("USD")
+                        .seatbid(singletonList(null))
+                        .build()));
+
+        // when
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, bidRequest);
+
+        // then
+        assertThat(result.getValue()).isEmpty();
+        assertThat(result.getErrors()).isEmpty();
     }
 
     @Test
@@ -48,38 +155,41 @@ public class MedianetBidderTest extends VertxTest {
         final BidRequest bidRequest = givenBidRequest();
 
         // when
-        final Result<List<HttpRequest<BidRequest>>> result;
-        result = medianetBidder.makeHttpRequests(bidRequest);
+        final Result<List<HttpRequest<BidRequest>>> result = medianetBidder.makeHttpRequests(bidRequest);
 
         // then
         assertThat(result.getErrors()).isEmpty();
-        assertThat(result.getValue()).hasSize(1)
-            .extracting(httpRequest -> mapper.readValue(httpRequest.getBody(), BidRequest.class))
-            .containsExactly(bidRequest);
+        assertThat(result.getValue())
+                .extracting(httpRequest -> mapper.readValue(httpRequest.getBody(), BidRequest.class))
+                .containsExactly(bidRequest);
     }
 
     @Test
     public void makeBidsShouldReturnErrorIfResponseBodyCouldNotBeParsed() {
         // given
-        final HttpCall<BidRequest> httpCall = sampleHttpCall(givenBidRequest(), "invalid response");
+        final HttpCall<BidRequest> httpCall = sampleHttpCall(
+                givenBidRequest(), "invalid response");
 
         // when
-        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, null);
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, givenBidRequest());
 
         // then
+        assertThat(result.getValue()).isEmpty();
         assertThat(result.getErrors()).hasSize(1)
-                .allMatch(error -> error.getType() == BidderError.Type.bad_server_response
-                    && error.getMessage().startsWith("Failed to decode: Unrecognized token"));
+                .allSatisfy(error -> {
+                    assertThat(error.getType()).isEqualTo(bad_server_response);
+                    assertThat(error.getMessage()).startsWith("Failed to decode: Unrecognized token");
+                });
     }
 
     @Test
     public void makeBidsShouldReturnEmptyListIfBidResponseIsNull() throws JsonProcessingException {
         // given
-        final HttpCall<BidRequest> httpCall;
-        httpCall = sampleHttpCall(givenBidRequest(), mapper.writeValueAsString(null));
+        final HttpCall<BidRequest> httpCall = sampleHttpCall(
+                givenBidRequest(), mapper.writeValueAsString(null));
 
         // when
-        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, null);
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, givenBidRequest());
 
         // then
         assertThat(result.getErrors()).isEmpty();
@@ -89,57 +199,45 @@ public class MedianetBidderTest extends VertxTest {
     @Test
     public void makeBidsShouldReturnEmptyListIfBidResponseSeatBidIsNull() throws JsonProcessingException {
         // given
-        final HttpCall<BidRequest> httpCall;
-        httpCall = sampleHttpCall(null, mapper.writeValueAsString(BidResponse.builder().build()));
-
-        // when
-        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, null);
-
-        // then
-        assertThat(result.getErrors()).isEmpty();
-        assertThat(result.getValue()).isEmpty();
-    }
-
-    @Test
-    public void makeBidsShouldReturnBannerBidIfBannerIsPresent() throws JsonProcessingException {
-        // given
         final HttpCall<BidRequest> httpCall = sampleHttpCall(
                 givenBidRequest(),
-                mapper.writeValueAsString(sampleBidResponse(bidBuilder -> bidBuilder.impid("123"))));
-
+                mapper.writeValueAsString(BidRequest.builder().build()));
         // when
-        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, null);
+        final Result<List<BidderBid>> result = medianetBidder.makeBids(httpCall, givenBidRequest());
 
         // then
+        assertThat(result.getValue()).isEmpty();
         assertThat(result.getErrors()).isEmpty();
-        assertThat(result.getValue())
-                .containsExactly(BidderBid.of(Bid.builder().impid("123").build(), banner, "USD"));
     }
 
-    private static BidResponse sampleBidResponse(Function<Bid.BidBuilder,
-            Bid.BidBuilder> bidCustomizer) {
+    private BidResponse givenBidResponse(Function<Bid.BidBuilder, Bid.BidBuilder> bidCustomize) {
         return BidResponse.builder()
-            .cur("USD")
-            .seatbid(singletonList(SeatBid.builder()
-                .bid(singletonList(bidCustomizer.apply(Bid.builder()).build()))
-                .build()))
-            .build();
+                .cur("USD")
+                .seatbid(singletonList(SeatBid.builder().bid(singletonList(bidCustomize.apply(Bid.builder()).build()))
+                        .build()))
+                .build();
     }
 
     private static HttpCall<BidRequest> sampleHttpCall(BidRequest bidRequest, String body) {
         return HttpCall.success(
-            HttpRequest.<BidRequest>builder().payload(bidRequest).build(),
-            HttpResponse.of(200, null, body),
-            null);
+                HttpRequest.<BidRequest>builder().payload(bidRequest).build(),
+                HttpResponse.of(200, null, body),
+                null);
     }
 
     private static BidRequest givenBidRequest() {
         return BidRequest.builder()
                 .id("request_id")
                 .imp(singletonList(Imp.builder()
-                    .id("imp_id")
-                    .ext(mapper.valueToTree(ExtPrebid.of(null, mapper.createObjectNode())))
-                    .build()))
+                        .id("imp_id")
+                        .ext(mapper.valueToTree(ExtPrebid.of(null, mapper.createObjectNode())))
+                        .build()))
+                .build();
+    }
+
+    private static BidRequest givenBidRequest(UnaryOperator<Imp.ImpBuilder> bidImpCustomize) {
+        return BidRequest.builder()
+                .imp(singletonList(bidImpCustomize.apply(Imp.builder()).build()))
                 .build();
     }
 }
