2b9b402419c75c84dd5f58c36e620aaff24a76d1
Yevhenii Viktorov <eugeniy.viktorov@gmail.com>
2022-01-21 18:18:39 +0200

    change another error code in CcpaSpec

--- src/test/groovy/org/prebid/server/functional/tests/privacy/CcpaSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/privacy/CcpaSpec.groovy
@@ -241,7 +241,7 @@ class CcpaSpec extends PrivacyBaseSpec {
         def response = defaultPbsService.sendAmpRequest(ampRequest)
 
         then: "Response should contain error"
-        assert response.ext?.errors[ErrorType.PREBID]*.code == [10012]
+        assert response.ext?.errors[ErrorType.PREBID]*.code == [10014]
         assert response.ext?.errors[ErrorType.PREBID]*.message == ["Invalid consent_type param passed"]
     }
 
