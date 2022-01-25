4d5b9f43c5b758e81a8b64d3611915775c05e937
markiian <mmykush@magnite.com>
2022-01-25 20:42:27 +0200

    fix check style

--- src/main/java/org/prebid/server/bidder/apacdex/ApacdexBidder.java
+++ src/main/java/org/prebid/server/bidder/apacdex/ApacdexBidder.java
@@ -2,7 +2,6 @@ package org.prebid.server.bidder.apacdex;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Imp;
 import com.iab.openrtb.response.Bid;
