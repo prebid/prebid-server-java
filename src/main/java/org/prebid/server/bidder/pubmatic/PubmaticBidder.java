043dba799dc0d5266172517685c569886de8ad29
Alex Maltsev <and1sscsgo@gmail.com>
2022-01-25 19:36:51 +0200

    Removed unused imports

--- src/main/java/org/prebid/server/bidder/pubmatic/PubmaticBidder.java
+++ src/main/java/org/prebid/server/bidder/pubmatic/PubmaticBidder.java
@@ -1,7 +1,6 @@
 package org.prebid.server.bidder.pubmatic;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.iab.openrtb.request.App;
@@ -45,7 +44,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
