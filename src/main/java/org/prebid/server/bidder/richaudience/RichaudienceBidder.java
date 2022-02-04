0962ec3f49299953e9cd010d3a640ebea8728fb8
markiian <mmykush@magnite.com>
2022-02-04 18:27:47 +0200

    Richaudience: Add Video and Multibid

--- src/main/java/org/prebid/server/bidder/richaudience/RichaudienceBidder.java
+++ src/main/java/org/prebid/server/bidder/richaudience/RichaudienceBidder.java
@@ -8,6 +8,7 @@ import com.iab.openrtb.request.Imp;
 import com.iab.openrtb.request.Site;
 import com.iab.openrtb.response.BidResponse;
 import com.iab.openrtb.response.SeatBid;
+import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.BooleanUtils;
@@ -39,6 +40,9 @@ import java.util.stream.Collectors;
 
 public class RichaudienceBidder implements Bidder<BidRequest> {
 
+    private static final int BID_TEST_REQUEST = 1;
+    private static final String OPENRTB_VERSION = "2.5";
+    private static final String DEVICE_IP = "11.222.33.44";
     private static final String DEFAULT_CURRENCY = "USD";
     private static final TypeReference<ExtPrebid<?, ExtImpRichaudience>> RICHAUDIENCE_EXT_TYPE_REFERENCE =
             new TypeReference<>() {
@@ -64,7 +68,7 @@ public class RichaudienceBidder implements Bidder<BidRequest> {
             for (Imp imp : request.getImp()) {
                 validateImp(imp);
 
-                final ExtImpRichaudience extImpRichaudience = parseImpExt(imp);
+                ExtImpRichaudience extImpRichaudience = parseImpExt(imp);
                 modifiedImps.add(modifyImp(imp, extImpRichaudience, isSecure));
 
                 if (!isTest && BooleanUtils.isTrue(extImpRichaudience.getTest())) {
@@ -76,6 +80,7 @@ public class RichaudienceBidder implements Bidder<BidRequest> {
         }
 
         final BidRequest modifiedRequest = modifyBidRequest(request, url, modifiedImps, isTest);
+
         return Result.withValue(createHttpRequest(modifiedRequest));
     }
 
@@ -128,40 +133,47 @@ public class RichaudienceBidder implements Bidder<BidRequest> {
                 .build();
     }
 
-    private static BidRequest modifyBidRequest(BidRequest bidRequest, URL url, List<Imp> imps, boolean isTest) {
-        final BidRequest.BidRequestBuilder builder = bidRequest.toBuilder()
-                .imp(imps)
-                .test(BooleanUtils.toInteger(isTest));
+    private static BidRequest modifyBidRequest(BidRequest bidRequest, URL url, List<Imp> imps,
+                                               boolean isTest) {
+        final BidRequest.BidRequestBuilder requestBuilder = bidRequest.toBuilder().imp(imps);
+
+        if (isTest) {
+            requestBuilder.test(BID_TEST_REQUEST).device(Device.builder().ip(DEVICE_IP).build());
+        }
 
         final Site site = bidRequest.getSite();
         if (url != null && StringUtils.isBlank(site.getDomain())) {
-            builder.site(site.toBuilder().domain(url.getHost()).build());
+            requestBuilder.site(site.toBuilder().domain(url.getHost()).build());
         }
 
-        return builder.build();
+        return requestBuilder.build();
     }
 
     private HttpRequest<BidRequest> createHttpRequest(BidRequest bidRequest) {
         return HttpRequest.<BidRequest>builder()
                 .method(HttpMethod.POST)
-                .headers(HttpUtil.headers())
+                .headers(resolveHeaders())
                 .uri(endpointUrl)
                 .body(mapper.encodeToBytes(bidRequest))
                 .payload(bidRequest)
                 .build();
     }
 
+    private MultiMap resolveHeaders() {
+        return HttpUtil.headers().add(HttpUtil.X_OPENRTB_VERSION_HEADER, OPENRTB_VERSION);
+    }
+
     @Override
     public Result<List<BidderBid>> makeBids(HttpCall<BidRequest> httpCall, BidRequest bidRequest) {
         try {
             final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
-            return Result.withValues(extractBids(bidResponse));
+            return Result.withValues(extractBids(bidResponse, bidRequest));
         } catch (DecodeException e) {
             return Result.withError(BidderError.badServerResponse(e.getMessage()));
         }
     }
 
-    private List<BidderBid> extractBids(BidResponse bidResponse) {
+    private List<BidderBid> extractBids(BidResponse bidResponse, BidRequest bidRequest) {
         if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
             return Collections.emptyList();
         }
@@ -171,7 +183,20 @@ public class RichaudienceBidder implements Bidder<BidRequest> {
                 .map(SeatBid::getBid)
                 .filter(Objects::nonNull)
                 .flatMap(Collection::stream)
-                .map(bid -> BidderBid.of(bid, BidType.banner, bidResponse.getCur()))
+                .map(bid -> BidderBid.of(bid, resolvedBidType(bid.getImpid(), bidRequest.getImp()),
+                        bidResponse.getCur()))
                 .collect(Collectors.toList());
+    }
+
+    private BidType resolvedBidType(String impid, List<Imp> imps) {
+        for (Imp imp : imps) {
+            if (impid.equals(imp.getId())) {
+                if (imp.getVideo() != null) {
+                    return BidType.video;
+                }
+                return BidType.banner;
+            }
+        }
+        return BidType.banner;
     }
 }
