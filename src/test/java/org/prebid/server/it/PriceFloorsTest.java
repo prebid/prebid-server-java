54c54a740c2872469308e00bbb5d0f6291423701
Yevhenii Viktorov <eugeniy.viktorov@gmail.com>
2022-03-30 17:03:20 +0300

    add different currencies to different stages of flow, change how wiremock change stub return value

--- src/test/java/org/prebid/server/it/PriceFloorsTest.java
+++ src/test/java/org/prebid/server/it/PriceFloorsTest.java
@@ -9,13 +9,13 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.util.UUID;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static java.util.Collections.singletonList;
 
 @RunWith(SpringRunner.class)
@@ -32,29 +32,35 @@ public class PriceFloorsTest extends IntegrationTest {
         // first run
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/generic-exchange"))
-                .withId(UUID.fromString("de0755a6-9878-4c61-a841-b725a02099bb"))
+                .inScenario("Price Floors Test")
+                .whenScenarioStateIs(STARTED)
                 .withRequestBody(equalToJson(jsonFrom("openrtb2/floors/floors-test-bid-request-1.json")))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/floors/floors-test-bid-response.json"))));
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/floors/floors-test-bid-response.json")))
+                .willSetStateTo("Run 1 passed"));
 
         // when
         final Response firstResponse = responseFor("openrtb2/floors/floors-test-auction-request-1.json",
                 Endpoint.openrtb2_auction);
 
         // then
-        assertJsonEquals("openrtb2/floors/floors-test-auction-response.json", firstResponse, singletonList("generic"));
+        assertJsonEquals(
+                "openrtb2/floors/floors-test-auction-response.json", firstResponse, singletonList("generic"));
 
         // second run
         // given
-        WIRE_MOCK_RULE.editStub(post(urlPathEqualTo("/generic-exchange"))
-                .withId(UUID.fromString("de0755a6-9878-4c61-a841-b725a02099bb"))
+        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/generic-exchange"))
+                .inScenario("Price Floors Test")
+                .whenScenarioStateIs("Run 1 passed")
                 .withRequestBody(equalToJson(jsonFrom("openrtb2/floors/floors-test-bid-request-2.json")))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/floors/floors-test-bid-response.json"))));
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/floors/floors-test-bid-response.json")))
+                .willSetStateTo("Run 2 passed"));
 
         // when
         final Response secondResponse = responseFor("openrtb2/floors/floors-test-auction-request-2.json",
                 Endpoint.openrtb2_auction);
 
         // then
-        assertJsonEquals("openrtb2/floors/floors-test-auction-response.json", secondResponse, singletonList("generic"));
+        assertJsonEquals(
+                "openrtb2/floors/floors-test-auction-response.json", secondResponse, singletonList("generic"));
     }
 }
