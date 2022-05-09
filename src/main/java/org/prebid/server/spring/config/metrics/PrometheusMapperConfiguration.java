773c539d73f1623ebb81588dc9b0a83ba69c891c
Alex Maltsev <and1sscsgo@gmail.com>
2022-05-09 15:16:19 +0300

    Removed unused imports

--- src/main/java/org/prebid/server/spring/config/metrics/PrometheusMapperConfiguration.java
+++ src/main/java/org/prebid/server/spring/config/metrics/PrometheusMapperConfiguration.java
@@ -12,11 +12,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Configuration
 @ConditionalOnProperty("metrics.prometheus.custom-labels-enabled")
