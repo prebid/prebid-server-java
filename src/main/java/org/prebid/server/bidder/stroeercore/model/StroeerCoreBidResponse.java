2445b3bdd7b09213155d0768aa701849acd4962e
Philip Watson <philip.watson@adscale.co.nz>
2022-05-20 14:54:58 +1200

    use better lombok annotation for StroeerCoreBidResponse

--- src/main/java/org/prebid/server/bidder/stroeercore/model/StroeerCoreBidResponse.java
+++ src/main/java/org/prebid/server/bidder/stroeercore/model/StroeerCoreBidResponse.java
@@ -1,12 +1,10 @@
 package org.prebid.server.bidder.stroeercore.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
 import java.util.List;
 
-@AllArgsConstructor(staticName = "of")
-@Value
+@Value(staticConstructor = "of")
 public class StroeerCoreBidResponse {
 
     List<StroeerCoreBid> bids;
