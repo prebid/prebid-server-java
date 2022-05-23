484665da489efad8ef808c1d4d60138b2071d016
Dubyk Danylo <pointbay2001@gmail.com>
2022-05-23 16:23:47 +0300

    Added new fields to `Publisher`.

--- src/main/java/com/iab/openrtb/request/Publisher.java
+++ src/main/java/com/iab/openrtb/request/Publisher.java
@@ -14,18 +14,34 @@ import java.util.List;
 @Value
 public class Publisher {
 
-    /** Exchange-specific publisher ID. */
+    /**
+     * Exchange-specific publisher ID.
+     */
     String id;
 
-    /** Publisher name (may be aliased at the publisher’s request). */
+    /**
+     * Publisher name (may be aliased at the publisher’s request).
+     */
     String name;
 
-    /** Array of IAB content categories that describe the publisher. Refer to List 5.1. */
+    /**
+     * The taxonomy in use. Refer to the AdCOM list <a href="https://github.com/InteractiveAdvertisingBureau/AdCOM/blob/master/AdCOM%20v1.0%20FINAL.md#list_categorytaxonomies">List: Category
+     * Taxonomies</a> for values.
+     */
+    Integer cattax;
+
+    /**
+     * Array of IAB content categories that describe the publisher. Refer to List 5.1.
+     */
     List<String> cat;
 
-    /** Highest level domain of the publisher (e.g., “publisher.com”). */
+    /**
+     * Highest level domain of the publisher (e.g., “publisher.com”).
+     */
     String domain;
 
-    /** Placeholder for exchange-specific extensions to OpenRTB. */
+    /**
+     * Placeholder for exchange-specific extensions to OpenRTB.
+     */
     ExtPublisher ext;
 }
