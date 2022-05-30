664d60410c976d6defd10f275d99e313d75e3dd5
Alex Maltsev <and1sscsgo@gmail.com>
2022-05-30 14:22:15 +0300

    Fixed functional tests

--- src/test/groovy/org/prebid/server/functional/tests/prometheus/PrometheusSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/prometheus/PrometheusSpec.groovy
@@ -10,12 +10,12 @@ import static org.prebid.server.functional.testcontainers.container.PrebidServer
 
 class PrometheusSpec extends BaseSpec {
 
-    private static final String PROMETHEUS_METRIC_NAME_REGEX = "[a-zA-Z_:]?[a-zA-Z0-9_:]*"
+    private static final String PROMETHEUS_METRIC_NAME_REGEX = "[a-zA-Z_]?[a-zA-Z0-9_]*"
 
     def "PBS should add namespace and subsystem parts to Prometheus metric names when those are config provided"() {
         given: "PBS config with set up Prometheus"
-        def namespace = "namespace_01:Rubicon"
-        def subsystem = "subsystem_01:Rubicon"
+        def namespace = "namespace_01_Rubicon"
+        def subsystem = "subsystem_01_Rubicon"
         def config = ["metrics.prometheus.enabled"  : "true",
                       "metrics.prometheus.port"     : PROMETHEUS_PORT as String,
                       "metrics.prometheus.namespace": namespace,
@@ -41,8 +41,8 @@ class PrometheusSpec extends BaseSpec {
 
     def "PBS service fails to start when invalid symbols by namespace and subsystem in Prometheus config are present"() {
         given: "PBS config with invalid symbols in namespace, subsystem Prometheus config"
-        def namespace = "namespace_01:Rubi%con"
-        def subsystem = "sub@system_01:Rubicon"
+        def namespace = "namespace_01_Rubi%con"
+        def subsystem = "sub@system_01_Rubicon"
         def config = ["metrics.prometheus.enabled"  : "true",
                       "metrics.prometheus.port"     : PROMETHEUS_PORT as String,
                       "metrics.prometheus.namespace": namespace,
