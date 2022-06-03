34e3e2b2479bbaeac0312a1d6014c19c61f2bd1e
Alex Maltsev <and1sscsgo@gmail.com>
2022-06-03 14:20:51 +0300

    Fixed functional tests

--- src/test/groovy/org/prebid/server/functional/model/response/auction/BidderCall.groovy
+++ src/test/groovy/org/prebid/server/functional/model/response/auction/BidderCall.groovy
@@ -3,10 +3,11 @@ package org.prebid.server.functional.model.response.auction
 import groovy.transform.ToString
 
 @ToString(includeNames = true, ignoreNulls = true)
-class HttpCall {
+class BidderCall {
 
     String uri
     String requestbody
+    BidderCallType calltype
     Map<String, List<String>> requestheaders
     String responsebody
     Integer status
