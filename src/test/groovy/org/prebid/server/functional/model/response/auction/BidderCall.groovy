fbb524341fba0d12d5b973914f709e5689aa939d
Oleksandr Zhevedenko <net.burst.mail@gmail.com>
2022-06-10 09:14:35 -0400

    Fix model field names

--- src/test/groovy/org/prebid/server/functional/model/response/auction/BidderCall.groovy
+++ src/test/groovy/org/prebid/server/functional/model/response/auction/BidderCall.groovy
@@ -1,16 +1,17 @@
 package org.prebid.server.functional.model.response.auction
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
 
 @ToString(includeNames = true, ignoreNulls = true)
+@JsonNaming(PropertyNamingStrategies.LowerCaseStrategy)
 class BidderCall {
 
     String uri
-    String requestbody
-    @JsonProperty("calltype")
+    String requestBody
     BidderCallType callType
-    Map<String, List<String>> requestheaders
-    String responsebody
+    Map<String, List<String>> requestHeaders
+    String responseBody
     Integer status
 }
