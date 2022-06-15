2830bd55a1dd105422cc4b36454ee233ddb8803a
markiian <mmykush@magnite.com>
2022-06-15 18:04:19 +0300

    Fix remove unused import

--- src/test/groovy/org/prebid/server/functional/model/request/auction/ImpExtPrebid.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/ImpExtPrebid.groovy
@@ -1,6 +1,5 @@
 package org.prebid.server.functional.model.request.auction
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
