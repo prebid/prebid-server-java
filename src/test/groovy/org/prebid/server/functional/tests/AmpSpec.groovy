8ad78aeb3aa96847cbe5cd06f10ba40e960f4b9e
markiian <mmykush@magnite.com>
2022-06-23 13:51:49 +0300

    Added @Retry

--- src/test/groovy/org/prebid/server/functional/tests/AmpSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/AmpSpec.groovy
@@ -1,5 +1,7 @@
 package org.prebid.server.functional.tests
 
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
 import org.prebid.server.functional.model.db.StoredRequest
 import org.prebid.server.functional.model.db.StoredResponse
 import org.prebid.server.functional.model.request.amp.AmpRequest
@@ -9,6 +11,7 @@ import org.prebid.server.functional.model.request.auction.StoredAuctionResponse
 import org.prebid.server.functional.model.response.auction.SeatBid
 import org.prebid.server.functional.service.PrebidServerService
 import org.prebid.server.functional.util.PBSUtils
+import spock.lang.Retry
 import spock.lang.Shared
 
 import static org.prebid.server.functional.util.SystemProperties.PBS_VERSION
@@ -17,6 +20,7 @@ class AmpSpec extends BaseSpec {
 
     private static final int DEFAULT_TIMEOUT = getRandomTimeout()
     private static final String PBS_VERSION_HEADER = "pbs-java/$PBS_VERSION"
+    private static final Logger logger = LoggerFactory.getLogger(AmpSpec.class)
 
     @Shared
     PrebidServerService prebidServerService = pbsServiceFactory.getService(["auction.max-timeout-ms"    : MAX_TIMEOUT as String,
@@ -267,6 +271,7 @@ class AmpSpec extends BaseSpec {
         assert bidderRequest.regs?.ext?.gdpr == ampStoredRequest.regs.ext.gdpr
     }
 
+    @Retry(exceptions = [IllegalStateException.class])
     def "PBS should generate UUID for BidRequest id and merge StoredRequest when generate-storedrequest-bidrequest-id = #generateBidRequestId"() {
         given: "PBS config with settings.generate-storedrequest-bidrequest-id and default-account-config"
         def pbsService = pbsServiceFactory.getService(["settings.generate-storedrequest-bidrequest-id": (generateBidRequestId)])
@@ -285,8 +290,6 @@ class AmpSpec extends BaseSpec {
 
         when: "PBS processes amp request"
         def ampResponse = pbsService.sendAmpRequest(ampRequest)
-
-        PBSUtils.waitUntil { ampResponse != null }
 
         then: "Actual bid request ID should be different from incoming bid request id"
         def requestId = ampResponse.ext?.debug?.resolvedRequest?.id
