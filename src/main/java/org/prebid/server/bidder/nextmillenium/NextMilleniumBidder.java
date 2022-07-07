4d5c8eca238a6c568f0096c8c274895cee6aae1c
Dubyk Danylo <pointbay2001@gmail.com>
2022-07-07 12:30:21 +0300

    Update `NextMillenium` bidder.

--- src/main/java/org/prebid/server/bidder/nextmillenium/NextMilleniumBidder.java
+++ src/main/java/org/prebid/server/bidder/nextmillenium/NextMilleniumBidder.java
@@ -1,17 +1,23 @@
 package org.prebid.server.bidder.nextmillenium;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.iab.openrtb.request.App;
+import com.iab.openrtb.request.Banner;
 import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Format;
 import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.request.Site;
 import com.iab.openrtb.response.BidResponse;
 import com.iab.openrtb.response.SeatBid;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.prebid.server.bidder.Bidder;
 import org.prebid.server.bidder.model.BidderBid;
-import org.prebid.server.bidder.model.BidderError;
 import org.prebid.server.bidder.model.BidderCall;
+import org.prebid.server.bidder.model.BidderError;
 import org.prebid.server.bidder.model.HttpRequest;
 import org.prebid.server.bidder.model.Result;
 import org.prebid.server.json.DecodeException;
@@ -23,6 +29,7 @@ import org.prebid.server.proto.openrtb.ext.request.ExtStoredRequest;
 import org.prebid.server.proto.openrtb.ext.request.nextmillenium.ExtImpNextMillenium;
 import org.prebid.server.proto.openrtb.ext.response.BidType;
 import org.prebid.server.util.HttpUtil;
+import org.prebid.server.util.ObjectUtil;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -79,11 +86,43 @@ public class NextMilleniumBidder implements Bidder<BidRequest> {
         return BidRequest.builder()
                 .id(bidRequest.getId())
                 .test(bidRequest.getTest())
-                .ext(ExtRequest.of(
-                        ExtRequestPrebid.builder()
-                                .storedrequest(ExtStoredRequest.of(ext.getPlacementId()))
-                                .build()))
+                .ext(ExtRequest.of(ExtRequestPrebid.builder()
+                        .storedrequest(ExtStoredRequest.of(resolveStoredRequestId(bidRequest, ext)))
+                        .build()))
                 .build();
+    }
+
+    private static String resolveStoredRequestId(BidRequest bidRequest, ExtImpNextMillenium extImpNextMillenium) {
+        final String groupId = extImpNextMillenium.getGroupId();
+        if (StringUtils.isEmpty(groupId)) {
+            return extImpNextMillenium.getPlacementId();
+        }
+
+        final String domain = ObjectUtils.firstNonNull(
+                ObjectUtil.getIfNotNull(bidRequest.getSite(), Site::getDomain),
+                ObjectUtil.getIfNotNull(bidRequest.getApp(), App::getDomain),
+                StringUtils.EMPTY);
+
+        final Banner banner = bidRequest.getImp().get(0).getBanner();
+        final List<Format> formats = banner.getFormat();
+        final Format firstFormat = CollectionUtils.isNotEmpty(formats) ? formats.get(0) : null;
+
+        final String size = ObjectUtils.firstNonNull(
+                formatSize(
+                        ObjectUtil.getIfNotNull(firstFormat, Format::getW),
+                        ObjectUtil.getIfNotNull(firstFormat, Format::getH)),
+                formatSize(banner.getW(), banner.getH()),
+                StringUtils.EMPTY);
+
+        return "g%s;%s;%s".formatted(groupId, size, domain);
+    }
+
+    private static String formatSize(Integer w, Integer h) {
+        if (w == null || h == null) {
+            return null;
+        }
+
+        return "%dx%d".formatted(w, h);
     }
 
     private HttpRequest<BidRequest> makeHttpRequest(BidRequest bidRequest) {
