3ba7bb556fb26e6b4e322969893ffaad0dfc936c
markiian <mmykush@magnite.com>
2022-07-07 11:43:28 +0300

    Simplified kargoBidderTest

--- src/test/java/org/prebid/server/bidder/kargo/KargoBidderTest.java
+++ src/test/java/org/prebid/server/bidder/kargo/KargoBidderTest.java
@@ -2,11 +2,8 @@ package org.prebid.server.bidder.kargo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.iab.openrtb.request.Banner;
 import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Imp;
-import com.iab.openrtb.request.Native;
-import com.iab.openrtb.request.Video;
 import com.iab.openrtb.response.Bid;
 import com.iab.openrtb.response.BidResponse;
 import com.iab.openrtb.response.SeatBid;
@@ -29,7 +26,6 @@ import static java.util.Collections.singletonList;
 import static java.util.function.UnaryOperator.identity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.prebid.server.proto.openrtb.ext.response.BidType.audio;
 import static org.prebid.server.proto.openrtb.ext.response.BidType.banner;
 import static org.prebid.server.proto.openrtb.ext.response.BidType.video;
 import static org.prebid.server.proto.openrtb.ext.response.BidType.xNative;
@@ -114,10 +110,8 @@ public class KargoBidderTest extends VertxTest {
     public void makeBidsShouldReturnBannerBidIfBannerIsPresentInResponseBidExt() throws JsonProcessingException {
         // given
         final BidderCall<BidRequest> httpCall = givenHttpCall(
-                BidRequest.builder()
-                        .imp(singletonList(Imp.builder().id("123").banner(Banner.builder().build()).build()))
-                        .build(),
-                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder.impid("123"))));
+                givenBidRequest(identity()),
+                mapper.writeValueAsString(givenBidResponse(identity())));
 
         // when
         final Result<List<BidderBid>> result = kargoBidder.makeBids(httpCall, null);
@@ -125,20 +119,16 @@ public class KargoBidderTest extends VertxTest {
         // then
         assertThat(result.getErrors()).isEmpty();
         assertThat(result.getValue())
-                .containsOnly(BidderBid.of(Bid.builder().impid("123").build(), banner, null));
+                .containsOnly(BidderBid.of(givenBid(null), banner, null));
     }
 
     @Test
     public void makeBidsShouldReturnVideoBidIfVideoIsPresentInInResponseBidExt() throws JsonProcessingException {
         // given
         final ObjectNode mediaType = mapper.createObjectNode().put("mediaType", "video");
-        final String impid = "123";
-        final BidderCall<BidRequest> httpCall = givenHttpCall(BidRequest.builder()
-                        .imp(singletonList(Imp.builder().id("123").video(Video.builder().build()).build()))
-                        .build(),
-                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder
-                        .impid(impid)
-                        .ext(mediaType))));
+        final BidderCall<BidRequest> httpCall = givenHttpCall(
+                givenBidRequest(identity()),
+                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder.ext(mediaType))));
 
         // when
         final Result<List<BidderBid>> result = kargoBidder.makeBids(httpCall, null);
@@ -146,26 +136,16 @@ public class KargoBidderTest extends VertxTest {
         // then
         assertThat(result.getErrors()).isEmpty();
         assertThat(result.getValue())
-                .allSatisfy(bidderBid -> {
-                    assertThat(bidderBid.getBid().getImpid()).isEqualTo(impid);
-                    assertThat(bidderBid.getType()).isEqualTo(video);
-                    assertThat(bidderBid.getBid().getExt()).isEqualTo(mediaType);
-                    assertThat(bidderBid.getBidCurrency()).isNull();
-                });
+                .containsExactly(BidderBid.of(givenBid(mediaType), video, null));
     }
 
     @Test
     public void makeBidsShouldReturnNativeBidIfNativeIsPresentInResponseBidExt() throws JsonProcessingException {
         // given
-        final ObjectNode mediaType = mapper.createObjectNode().put("mediaType", xNative.getName());
-        final String impid = "123";
+        final ObjectNode mediaType = mapper.createObjectNode().put("mediaType", "native");
         final BidderCall<BidRequest> httpCall = givenHttpCall(
-                BidRequest.builder()
-                        .imp(singletonList(Imp.builder().id("123").xNative(Native.builder().build()).build()))
-                        .build(),
-                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder
-                        .impid(impid)
-                        .ext(mediaType))));
+                givenBidRequest(identity()),
+                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder.ext(mediaType))));
 
         // when
         final Result<List<BidderBid>> result = kargoBidder.makeBids(httpCall, null);
@@ -173,26 +153,16 @@ public class KargoBidderTest extends VertxTest {
         // then
         assertThat(result.getErrors()).isEmpty();
         assertThat(result.getValue())
-                .allSatisfy(bidderBid -> {
-                    assertThat(bidderBid.getBid().getImpid()).isEqualTo(impid);
-                    assertThat(bidderBid.getType()).isEqualTo(xNative);
-                    assertThat(bidderBid.getBid().getExt()).isEqualTo(mediaType);
-                    assertThat(bidderBid.getBidCurrency()).isNull();
-                });
+                .containsExactly(BidderBid.of(givenBid(mediaType), xNative, null));
     }
 
     @Test
     public void makeBidsShouldReturnDefaultBannerBidIfBidUnsupportedType() throws JsonProcessingException {
         // given
-        final ObjectNode mediaType = mapper.createObjectNode().put("mediaType", audio.getName());
-        final String impid = "123";
+        final ObjectNode mediaType = mapper.createObjectNode().put("mediaType", "audio");
         final BidderCall<BidRequest> httpCall = givenHttpCall(
-                BidRequest.builder()
-                        .imp(singletonList(Imp.builder().id("123").xNative(Native.builder().build()).build()))
-                        .build(),
-                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder
-                        .impid(impid)
-                        .ext(mediaType))));
+                givenBidRequest(identity()),
+                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder.ext(mediaType))));
 
         // when
         final Result<List<BidderBid>> result = kargoBidder.makeBids(httpCall, null);
@@ -200,26 +170,16 @@ public class KargoBidderTest extends VertxTest {
         // then
         assertThat(result.getErrors()).isEmpty();
         assertThat(result.getValue())
-                .allSatisfy(bidderBid -> {
-                    assertThat(bidderBid.getBid().getImpid()).isEqualTo(impid);
-                    assertThat(bidderBid.getType()).isEqualTo(banner);
-                    assertThat(bidderBid.getBid().getExt()).isEqualTo(mediaType);
-                    assertThat(bidderBid.getBidCurrency()).isNull();
-                });
+                .containsExactly(BidderBid.of(givenBid(mediaType), banner, null));
     }
 
     @Test
     public void makeBidsShouldReturnDefaultBannerBidIfBidExtCannotBeParsed() throws JsonProcessingException {
         // given
-        final String impid = "123";
         final ObjectNode mediaType = mapper.createObjectNode().set("mediaType", mapper.createArrayNode());
         final BidderCall<BidRequest> httpCall = givenHttpCall(
-                BidRequest.builder()
-                        .imp(singletonList(Imp.builder().id("123").xNative(Native.builder().build()).build()))
-                        .build(),
-                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder
-                        .impid(impid)
-                        .ext(mediaType))));
+                givenBidRequest(identity()),
+                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder.ext(mediaType))));
 
         // when
         final Result<List<BidderBid>> result = kargoBidder.makeBids(httpCall, null);
@@ -227,26 +187,16 @@ public class KargoBidderTest extends VertxTest {
         // then
         assertThat(result.getErrors()).isEmpty();
         assertThat(result.getValue())
-                .allSatisfy(bidderBid -> {
-                    assertThat(bidderBid.getBid().getImpid()).isEqualTo(impid);
-                    assertThat(bidderBid.getType()).isEqualTo(banner);
-                    assertThat(bidderBid.getBid().getExt()).isNotEmpty();
-                    assertThat(bidderBid.getBidCurrency()).isNull();
-                });
+                .containsExactly(BidderBid.of(givenBid(mediaType), banner, null));
     }
 
     @Test
     public void makeBidsShouldReturnDefaultBannerBidIfBidExtMediaTypeIsNull() throws JsonProcessingException {
         // given
-        final String impid = "123";
         final ObjectNode mediaType = mapper.createObjectNode().set("mediaType", null);
         final BidderCall<BidRequest> httpCall = givenHttpCall(
-                BidRequest.builder()
-                        .imp(singletonList(Imp.builder().id("123").xNative(Native.builder().build()).build()))
-                        .build(),
-                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder
-                        .impid(impid)
-                        .ext(mediaType))));
+                givenBidRequest(identity()),
+                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder.ext(mediaType))));
 
         // when
         final Result<List<BidderBid>> result = kargoBidder.makeBids(httpCall, null);
@@ -254,31 +204,11 @@ public class KargoBidderTest extends VertxTest {
         // then
         assertThat(result.getErrors()).isEmpty();
         assertThat(result.getValue())
-                .allSatisfy(bidderBid -> {
-                    assertThat(bidderBid.getBid().getImpid()).isEqualTo(impid);
-                    assertThat(bidderBid.getType()).isEqualTo(banner);
-                    assertThat(bidderBid.getBid().getExt()).isNotEmpty();
-                    assertThat(bidderBid.getBidCurrency()).isNull();
-                });
+                .containsExactly(BidderBid.of(givenBid(mediaType), banner, null));
     }
 
-    @Test
-    public void makeBidsShouldReturnBidderBidIfBannerAndVideoAndNativeIsAbsentInRequestImp()
-            throws JsonProcessingException {
-        // given
-        final BidderCall<BidRequest> httpCall = givenHttpCall(
-                BidRequest.builder()
-                        .imp(singletonList(Imp.builder().id("123").build()))
-                        .build(),
-                mapper.writeValueAsString(givenBidResponse(impBuilder -> impBuilder.impid("123"))));
-
-        // when
-        final Result<List<BidderBid>> result = kargoBidder.makeBids(httpCall, null);
-
-        // then
-        assertThat(result.getErrors()).isEmpty();
-        assertThat(result.getValue())
-                .containsOnly(BidderBid.of(Bid.builder().impid("123").build(), banner, null));
+    private Bid givenBid(ObjectNode objectNode) {
+        return Bid.builder().impid("123").ext(objectNode).build();
     }
 
     private static BidRequest givenBidRequest(UnaryOperator<Imp.ImpBuilder> impCustomizers) {
@@ -295,14 +225,14 @@ public class KargoBidderTest extends VertxTest {
     private static Imp givenImp(UnaryOperator<Imp.ImpBuilder> impCustomizer) {
         return impCustomizer.apply(Imp.builder()
                         .id("123")
-                        .banner(Banner.builder().w(23).h(25).build())
                         .ext(mapper.valueToTree(ExtPrebid.of(null, ExtImpKargo.of("adSlotId")))))
                 .build();
     }
 
     private static BidResponse givenBidResponse(UnaryOperator<Bid.BidBuilder> bidCustomizer) {
         return BidResponse.builder()
-                .seatbid(singletonList(SeatBid.builder().bid(singletonList(bidCustomizer.apply(Bid.builder()).build()))
+                .seatbid(singletonList(SeatBid.builder().bid(singletonList(bidCustomizer.apply(Bid.builder()
+                                .impid("123")).build()))
                         .build()))
                 .build();
     }
