44a50efc64f602c9011d6e4e815b468e392a7b08
markiian <mmykush@magnite.com>
2022-07-19 11:51:23 +0300

    Minor fix

--- src/test/groovy/org/prebid/server/functional/tests/pg/AlertSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/pg/AlertSpec.groovy
@@ -33,7 +33,7 @@ class AlertSpec extends BasePgSpec {
     private static final String PBS_DELIVERY_CLIENT_ERROR = "pbs-delivery-stats-client-error"
     private static final Integer DEFAULT_ALERT_PERIOD = 15
 
-    @Retry(exceptions = { IllegalStateException.class })
+    @Retry(exceptions = [IllegalStateException.class])
     def "PBS should send alert request when the threshold is reached"() {
         given: "Changed Planner Register endpoint response to return bad status code"
         generalPlanner.initRegisterResponse(NOT_FOUND_404)
