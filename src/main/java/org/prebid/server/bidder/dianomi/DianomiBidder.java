79cb89a16d0515bbfd2f04a6cb7013271249ddab
Serhii Nahornyi <sergiy3344@gmail.com>
2022-09-13 16:38:29 +0300

    Dianomi: Add adapter

---
+++ src/main/java/org/prebid/server/bidder/dianomi/DianomiBidder.java
@@ -0,0 +1,164 @@
+package org.prebid.server.bidder.dianomi;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
+import io.vertx.core.http.HttpMethod;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.prebid.server.bidder.Bidder;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.BidderCall;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
+import org.prebid.server.exception.PreBidException;
+import org.prebid.server.json.DecodeException;
+import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.proto.openrtb.ext.ExtPrebid;
+import org.prebid.server.proto.openrtb.ext.request.ExtRequest;
+import org.prebid.server.proto.openrtb.ext.request.dianomi.ExtImpDianomi;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.util.HttpUtil;
+import org.prebid.server.util.ObjectUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class DianomiBidder implements Bidder<BidRequest> {
+
+    private static final String REQUEST_EXT_PT = "pt";
+    private static final TypeReference<ExtPrebid<?, ExtImpDianomi>> DIANOMI_EXT_TYPE_REFERENCE =
+            new TypeReference<>() {
+            };
+
+    private final String endpointUrl;
+    private final JacksonMapper mapper;
+
+    public DianomiBidder(String endpointUrl, JacksonMapper mapper) {
+        this.endpointUrl = HttpUtil.validateUrl(Objects.requireNonNull(endpointUrl));
+        this.mapper = Objects.requireNonNull(mapper);
+    }
+
+    @Override
+    public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest bidRequest) {
+        final List<Imp> modifiedImps = new ArrayList<>();
+        final List<BidderError> errors = new ArrayList<>();
+        String priceType = null;
+
+        for (Imp imp : bidRequest.getImp()) {
+            final ExtImpDianomi extImpDianomi;
+            try {
+                extImpDianomi = parseImpExt(imp);
+            } catch (PreBidException e) {
+                errors.add(BidderError.badInput(e.getMessage()));
+                continue;
+            }
+
+            modifiedImps.add(imp.toBuilder().tagid(extImpDianomi.getSmartAdId()).build());
+            priceType = StringUtils.defaultIfEmpty(priceType, extImpDianomi.getPriceType());
+        }
+
+        if (modifiedImps.isEmpty()) {
+            return Result.withErrors(errors);
+        }
+
+        final BidRequest modifiedBidRequest = modifyBidRequest(bidRequest, modifiedImps, priceType);
+        return Result.of(Collections.singletonList(makeRequest(modifiedBidRequest)), errors);
+    }
+
+    private ExtImpDianomi parseImpExt(Imp imp) {
+        try {
+            return mapper.mapper().convertValue(imp.getExt(), DIANOMI_EXT_TYPE_REFERENCE).getBidder();
+        } catch (IllegalArgumentException e) {
+            throw new PreBidException(e.getMessage());
+        }
+    }
+
+    private BidRequest modifyBidRequest(BidRequest bidRequest, List<Imp> imps, String priceType) {
+        final ExtRequest extRequest = ObjectUtils.defaultIfNull(bidRequest.getExt(), ExtRequest.empty());
+        final ExtRequest modifiedExtRequest = StringUtils.isNotEmpty(priceType)
+                ? modifyExtRequest(extRequest, priceType)
+                : extRequest;
+
+        return bidRequest.toBuilder()
+                .imp(imps)
+                .ext(modifiedExtRequest)
+                .build();
+    }
+
+    private ExtRequest modifyExtRequest(ExtRequest extRequest, String priceType) {
+        final ObjectNode adfNode = mapper.mapper().createObjectNode().put(REQUEST_EXT_PT, priceType);
+        return mapper.fillExtension(extRequest, adfNode);
+    }
+
+    private HttpRequest<BidRequest> makeRequest(BidRequest bidRequest) {
+        return HttpRequest.<BidRequest>builder()
+                .method(HttpMethod.POST)
+                .uri(endpointUrl)
+                .headers(HttpUtil.headers())
+                .body(mapper.encodeToBytes(bidRequest))
+                .payload(bidRequest)
+                .build();
+    }
+
+    @Override
+    public Result<List<BidderBid>> makeBids(BidderCall<BidRequest> httpCall, BidRequest bidRequest) {
+        try {
+            final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
+            final List<BidderError> errors = new ArrayList<>();
+            final List<BidderBid> bidderBids = extractBids(bidResponse, errors);
+            return Result.of(bidderBids, errors);
+        } catch (DecodeException e) {
+            return Result.withError(BidderError.badServerResponse(e.getMessage()));
+        }
+    }
+
+    private List<BidderBid> extractBids(BidResponse bidResponse, List<BidderError> errors) {
+        if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
+            return Collections.emptyList();
+        }
+
+        return bidResponse.getSeatbid().stream()
+                .filter(Objects::nonNull)
+                .map(SeatBid::getBid)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .map(bid -> makeBidderBid(bid, bidResponse.getCur(), errors))
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+    private BidderBid makeBidderBid(Bid bid, String bidCurrency, List<BidderError> errors) {
+        final JsonNode prebidNode = ObjectUtil.getIfNotNull(bid.getExt(), node -> node.get("prebid"));
+        final JsonNode typeNode = ObjectUtil.getIfNotNull(prebidNode, node -> node.get("type"));
+        final BidType bidType;
+        try {
+            bidType = mapper.mapper().convertValue(typeNode, BidType.class);
+        } catch (IllegalArgumentException e) {
+            addMediaTypeParseError(errors, bid.getImpid());
+            return null;
+        }
+
+        if (bidType == null) {
+            addMediaTypeParseError(errors, bid.getImpid());
+            return null;
+        }
+
+        return BidderBid.of(bid, bidType, bidCurrency);
+    }
+
+    private void addMediaTypeParseError(List<BidderError> errors, String impId) {
+        errors.add(BidderError.badServerResponse("Failed to parse impression %s mediatype".formatted(impId)));
+    }
+}
