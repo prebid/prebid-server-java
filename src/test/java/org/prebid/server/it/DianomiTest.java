adaa23cbbab2ec531a60608af8375e737f0ed39c
Serhii Nahornyi <sergiy3344@gmail.com>
2022-09-13 16:52:02 +0300

    Name fixes

--- src/test/java/org/prebid/server/it/DianomiTest.java
+++ src/test/java/org/prebid/server/it/DianomiTest.java
@@ -19,18 +19,18 @@ import static java.util.Collections.singletonList;
 public class DianomiTest extends IntegrationTest {
 
     @Test
-    public void openrtb2AuctionShouldRespondWithBidsFromAdf() throws IOException, JSONException {
+    public void openrtb2AuctionShouldRespondWithBidsFromDianomi() throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/dianomi-exchange"))
-                .withRequestBody(equalToJson(jsonFrom("openrtb2/dianomi/test-adf-bid-request.json")))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/dianomi/test-adf-bid-response.json"))));
+                .withRequestBody(equalToJson(jsonFrom("openrtb2/dianomi/test-dianomi-bid-request.json")))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/dianomi/test-dianomi-bid-response.json"))));
 
         // when
-        final Response response = responseFor("openrtb2/dianomi/test-auction-adf-request.json",
+        final Response response = responseFor("openrtb2/dianomi/test-auction-dianomi-request.json",
                 Endpoint.openrtb2_auction);
 
         // then
-        assertJsonEquals("openrtb2/dianomi/test-auction-adf-response.json", response,
+        assertJsonEquals("openrtb2/dianomi/test-auction-dianomi-response.json", response,
                 singletonList("dianomi"));
     }
 }
