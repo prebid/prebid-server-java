ae75aa6beb2cc3f9c471a6650c80bb8c9550c457
Alex Maltsev <and1sscsgo@gmail.com>
2022-09-20 13:19:46 +0300

    Added data object mapper

---
+++ src/main/java/org/prebid/server/protobuf/mappers/ProtobufDataObjectMapper.java
@@ -0,0 +1,34 @@
+package org.prebid.server.protobuf.mappers;
+
+import com.iab.openrtb.request.DataObject;
+import com.iabtechlab.openrtb.v2.OpenRtb;
+import org.prebid.server.protobuf.JsonProtobufExtensionMapper;
+import org.prebid.server.protobuf.ProtobufMapper;
+
+import static org.prebid.server.protobuf.MapperUtils.setNotNull;
+
+public class ProtobufDataObjectMapper<ProtobufExtensionType>
+        implements ProtobufMapper<DataObject, OpenRtb.NativeRequest.Asset.Data> {
+
+    private final JsonProtobufExtensionMapper<OpenRtb.NativeRequest.Asset.Data, ProtobufExtensionType> extensionMapper;
+
+    public ProtobufDataObjectMapper(
+            JsonProtobufExtensionMapper<OpenRtb.NativeRequest.Asset.Data, ProtobufExtensionType> extensionMapper) {
+
+        this.extensionMapper = extensionMapper;
+    }
+
+    @Override
+    public OpenRtb.NativeRequest.Asset.Data map(DataObject dataObject) {
+        final OpenRtb.NativeRequest.Asset.Data.Builder resultBuilder = OpenRtb.NativeRequest.Asset.Data.newBuilder();
+
+        setNotNull(dataObject.getType(), resultBuilder::setType);
+        setNotNull(dataObject.getLen(), resultBuilder::setLen);
+
+        if (extensionMapper != null) {
+            final ProtobufExtensionType ext = extensionMapper.map(dataObject.getExt());
+            resultBuilder.setExtension(extensionMapper.extensionType(), ext);
+        }
+        return resultBuilder.build();
+    }
+}
