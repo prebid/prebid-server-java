eac5a9635121829071d7392906a75b5bb8abf968
Alex Maltsev <and1sscsgo@gmail.com>
2022-09-20 15:44:16 +0300

    Added java docs

--- src/main/java/org/prebid/server/protobuf/ProtobufExtensionMapper.java
+++ src/main/java/org/prebid/server/protobuf/ProtobufExtensionMapper.java
@@ -3,6 +3,9 @@ package org.prebid.server.protobuf;
 import com.google.protobuf.Extension;
 import com.google.protobuf.Message;
 
+/**
+ * Generic interface for mapping protobuf extensions.
+ */
 public interface ProtobufExtensionMapper<ContainingType extends Message, FromType, ToType> {
 
     ToType map(FromType fromType);
--- src/main/java/org/prebid/server/protobuf/ProtobufMapper.java
+++ src/main/java/org/prebid/server/protobuf/ProtobufMapper.java
@@ -2,6 +2,13 @@ package org.prebid.server.protobuf;
 
 import com.google.protobuf.Message;
 
+/**
+ * Generic interface for mapping internal models to protobuf models.
+ * MapStruct is not used for implementations, because models have extensions,
+ * for which different extension mappers are provided via constructor injection.
+ * Sadly, for now MapStruct doesn't support this feature.
+ * See <a href="https://github.com/mapstruct/mapstruct/issues/2257">corresponding issue</a>
+ */
 @FunctionalInterface
 public interface ProtobufMapper<FromType, ToType extends Message> {
 
