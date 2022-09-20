d8accdbc9a21d6443666a6c073076d432cadc143
Alex Maltsev <and1sscsgo@gmail.com>
2022-09-20 13:29:59 +0300

    Added event tracker mapper

---
+++ src/main/java/org/prebid/server/protobuf/mappers/ProtobufEventTrackerMapper.java
@@ -0,0 +1,38 @@
+package org.prebid.server.protobuf.mappers;
+
+import com.iab.openrtb.request.EventTracker;
+import com.iabtechlab.openrtb.v2.OpenRtb;
+import org.prebid.server.protobuf.JsonProtobufExtensionMapper;
+import org.prebid.server.protobuf.ProtobufMapper;
+
+import static org.prebid.server.protobuf.MapperUtils.setNotNull;
+
+public class ProtobufEventTrackerMapper<ProtobufExtensionType>
+        implements ProtobufMapper<EventTracker, OpenRtb.NativeRequest.EventTrackers> {
+
+    private final JsonProtobufExtensionMapper<
+            OpenRtb.NativeRequest.EventTrackers,
+            ProtobufExtensionType
+            > extensionMapper;
+
+    public ProtobufEventTrackerMapper(
+            JsonProtobufExtensionMapper<OpenRtb.NativeRequest.EventTrackers, ProtobufExtensionType> extensionMapper) {
+
+        this.extensionMapper = extensionMapper;
+    }
+
+    @Override
+    public OpenRtb.NativeRequest.EventTrackers map(EventTracker eventTracker) {
+        final OpenRtb.NativeRequest.EventTrackers.Builder resultBuilder =
+                OpenRtb.NativeRequest.EventTrackers.newBuilder();
+
+        setNotNull(eventTracker.getEvent(), resultBuilder::setEvent);
+        setNotNull(eventTracker.getMethods(), resultBuilder::addAllMethods);
+
+        if (extensionMapper != null) {
+            final ProtobufExtensionType ext = extensionMapper.map(eventTracker.getExt());
+            resultBuilder.setExtension(extensionMapper.extensionType(), ext);
+        }
+        return resultBuilder.build();
+    }
+}
