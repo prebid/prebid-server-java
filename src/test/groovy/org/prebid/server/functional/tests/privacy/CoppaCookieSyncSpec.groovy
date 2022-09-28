d714edb5c2ab096df7e5efbd9ad436aaf8e6e274
markiian <mmykush@magnite.com>
2022-09-28 13:09:24 +0300

    Minor enhancement

--- src/test/groovy/org/prebid/server/functional/tests/privacy/CoppaCookieSyncSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/privacy/CoppaCookieSyncSpec.groovy
@@ -1,6 +1,7 @@
 package org.prebid.server.functional.tests.privacy
 
 import org.prebid.server.functional.model.request.cookiesync.CookieSyncRequest
+import org.prebid.server.functional.util.PBSUtils
 
 import static org.prebid.server.functional.model.bidder.BidderName.UNKNOWN
 
@@ -9,8 +10,8 @@ class CoppaCookieSyncSpec extends PrivacyBaseSpec {
     def "PBS should return error when unsupported bidder in ccpa context"() {
         given: "Default CookieSyncRequest with UNKNOWN bidder"
         def cookieSyncRequest = CookieSyncRequest.defaultCookieSyncRequest.tap {
-            bidders.add(UNKNOWN)
-            usPrivacy = "1YYY"
+            bidders = [UNKNOWN]
+            usPrivacy = PBSUtils.randomString
         }
 
         when: "PBS processes cookie sync request"
