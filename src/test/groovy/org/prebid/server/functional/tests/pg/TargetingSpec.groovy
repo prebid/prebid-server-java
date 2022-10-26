23b5b6696660167a58f4bbe28dce8af9704f0b5e
markiian <mmykush@magnite.com>
2022-10-26 12:18:25 +0300

    Update test due to comments

--- src/test/groovy/org/prebid/server/functional/tests/pg/TargetingSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/pg/TargetingSpec.groovy
@@ -532,20 +532,19 @@ class TargetingSpec extends BasePgSpec {
 
     def "PBS should be able to match video size targeting taken from imp[].video sources by INTERSECTS matching function"() {
         given: "Default video bid request"
-        def defaultLineItemSize = LineItemSize.defaultLineItemSize
+        def lineItemSize = LineItemSize.defaultLineItemSize
         def bidRequest = BidRequest.defaultVideoRequest.tap {
-            imp[0].video.w = defaultLineItemSize.w
-            imp[0].video.h = defaultLineItemSize.h
+            imp[0].video.w = lineItemSize.w
+            imp[0].video.h = lineItemSize.h
         }
 
         and: "Planner response"
         def plansResponse = PlansResponse.getDefaultPlansResponse(bidRequest.site.publisher.id).tap {
-            lineItems[0].targeting = new Targeting.Builder().addTargeting(AD_UNIT_SIZE, INTERSECTS, [defaultLineItemSize])
+            lineItems[0].targeting = new Targeting.Builder().addTargeting(AD_UNIT_SIZE, INTERSECTS, [lineItemSize])
                                                             .addTargeting(AD_UNIT_MEDIA_TYPE, INTERSECTS, [VIDEO])
                                                             .build()
         }
         generalPlanner.initPlansResponse(plansResponse)
-        def lineItemSize = plansResponse.lineItems.size()
 
         and: "Line items are fetched by PBS"
         updateLineItemsAndWait()
@@ -554,6 +553,6 @@ class TargetingSpec extends BasePgSpec {
         def auctionResponse = pgPbsService.sendAuctionRequest(bidRequest)
 
         then: "PBS had PG auction"
-        assert auctionResponse.ext?.debug?.pgmetrics?.matchedWholeTargeting?.size() == lineItemSize
+        assert auctionResponse.ext?.debug?.pgmetrics?.matchedWholeTargeting?.size() == plansResponse.lineItems.size()
     }
 }
