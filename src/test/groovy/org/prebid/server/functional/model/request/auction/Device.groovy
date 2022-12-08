ebd2ac39cd4fd268fe5a18bf25fe082ab5077625
Oleksandr Zhevedenko <net.burst.mail@gmail.com>
2022-12-08 00:19:20 -0500

    Add tests for LMT override

--- src/test/groovy/org/prebid/server/functional/model/request/auction/Device.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/Device.groovy
@@ -1,7 +1,6 @@
 package org.prebid.server.functional.model.request.auction
 
 import groovy.transform.ToString
-import org.prebid.server.functional.util.PBSUtils
 
 @ToString(includeNames = true, ignoreNulls = true)
 class Device {
@@ -38,4 +37,5 @@ class Device {
     String dpidmd5
     String macsha1
     String macmd5
+    DeviceExt ext
 }
