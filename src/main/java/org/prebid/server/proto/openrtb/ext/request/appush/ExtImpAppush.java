eb5f13beda8dc622256ac6d916b3733e09fa3fd8
vyniciuspontes <vynicius.moraispontes@freestar.io>
2023-01-31 13:25:55 -0300

    S2S-712 - Add Appush bidder class and ext proto

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/appush/ExtImpAppush.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/appush/ExtImpAppush.java
@@ -1,7 +1,11 @@
 package org.prebid.server.proto.openrtb.ext.request.appush;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Value;
 
+@AllArgsConstructor(staticName = "of")
+@Value
 public class ExtImpAppush {
 
     @JsonProperty("placementId")
