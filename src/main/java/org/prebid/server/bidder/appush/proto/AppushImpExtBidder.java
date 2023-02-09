35f428ee62434238ff21138e74b5978975f3225d
vyniciuspontes <vynicius.moraispontes@freestar.io>
2023-02-09 09:10:10 -0300

    S2S-712 - Remove JsonInclude NonEmpty

--- src/main/java/org/prebid/server/bidder/appush/proto/AppushImpExtBidder.java
+++ src/main/java/org/prebid/server/bidder/appush/proto/AppushImpExtBidder.java
@@ -1,6 +1,5 @@
 package org.prebid.server.bidder.appush.proto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
@@ -12,10 +11,8 @@ public class AppushImpExtBidder {
     String type;
 
     @JsonProperty(value = "placementId")
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     String placementId;
 
     @JsonProperty(value = "endpointId")
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     String endpointId;
 }
