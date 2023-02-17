07b0acea6a30af44db3be0cf2f854dca8b871d9a
Dubyk Danylo <pointbay2001@gmail.com>
2023-02-17 12:37:48 +0200

    Fix functional tests.

--- src/main/java/org/prebid/server/cookie/CookieSyncService.java
+++ src/main/java/org/prebid/server/cookie/CookieSyncService.java
@@ -2,6 +2,7 @@ package org.prebid.server.cookie;
 
 import io.vertx.core.Future;
 import io.vertx.ext.web.RoutingContext;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.collections4.SetUtils;
 import org.apache.commons.lang3.ObjectUtils;
@@ -330,11 +331,12 @@ public class CookieSyncService {
                 validStatuses(biddersToSync, cookieSyncContext),
                 debugStatuses(biddersToSync, cookieSyncContext));
 
-        final List<String> warnings = cookieSyncContext.isDebug()
-                        ? cookieSyncContext.getWarnings()
+        final List<String> warnings = cookieSyncContext.getWarnings();
+        final List<String> resolvedWarnings = cookieSyncContext.isDebug() && CollectionUtils.isNotEmpty(warnings)
+                        ? warnings
                         : null;
 
-        return CookieSyncResponse.of(cookieSyncStatus, Collections.unmodifiableList(statuses), warnings);
+        return CookieSyncResponse.of(cookieSyncStatus, Collections.unmodifiableList(statuses), resolvedWarnings);
     }
 
     private Set<String> biddersToSync(CookieSyncContext cookieSyncContext) {
