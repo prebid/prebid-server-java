1f5ca5f08f5645a958acd33572086fcc4d05d3fe
SerhiiNahornyi <sergiy3344@gmail.com>
2023-02-17 14:01:50 +0200

    VideoHeroes: Add adapter (#2177)

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/visx/ExtImpVisx.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/visx/ExtImpVisx.java
@@ -1,17 +1,10 @@
 package org.prebid.server.proto.openrtb.ext.request.visx;
 
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
 import java.util.List;
 
-/**
- * Defines the contract for bidRequest.imp[i].ext.visx
- */
-@AllArgsConstructor(
-        staticName = "of"
-)
-@Value
+@Value(staticConstructor = "of")
 public class ExtImpVisx {
 
     Integer uid;
