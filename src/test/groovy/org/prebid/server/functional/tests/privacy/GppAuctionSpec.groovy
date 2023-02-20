0c11cd820babe29ceafdcdff658084e67ce1f76e
markiian <mmykush@magnite.com>
2023-02-20 17:09:56 +0200

    Fix of half of the comments

--- src/test/groovy/org/prebid/server/functional/tests/privacy/GppAuctionSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/privacy/GppAuctionSpec.groovy
@@ -1,6 +1,8 @@
 package org.prebid.server.functional.tests.privacy
 
 import com.iab.gpp.encoder.section.TcfCaV1
+import com.iab.gpp.encoder.section.TcfEuV2
+import com.iab.gpp.encoder.section.UspV1
 import org.prebid.server.functional.model.request.auction.BidRequest
 import org.prebid.server.functional.model.request.auction.User
 import org.prebid.server.functional.model.response.auction.ErrorType
@@ -9,6 +11,8 @@ import org.prebid.server.functional.util.PBSUtils
 import org.prebid.server.functional.util.privacy.CcpaConsent
 import org.prebid.server.functional.util.privacy.GppConsent
 
+import static org.prebid.server.functional.model.request.GppSectionId.TCF_EU_V2
+import static org.prebid.server.functional.model.request.GppSectionId.US_PV_V1
 import static org.prebid.server.functional.util.privacy.CcpaConsent.Signal.ENFORCED
 
 class GppAuctionSpec extends BaseSpec {
@@ -17,7 +21,7 @@ class GppAuctionSpec extends BaseSpec {
         given: "Default bid request with gppSid and without gdpr"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             regs.gdpr = null
-            regs.gppSid = [2]
+            regs.gppSid = [TCF_EU_V2.valueAsInt]
         }
 
         when: "PBS processes auction request"
@@ -32,7 +36,7 @@ class GppAuctionSpec extends BaseSpec {
         given: "Default bid request with gppSid and without gdpr"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             regs.gdpr = null
-            regs.gppSid = [PBSUtils.getRandomNumberWithExcept(2)]
+            regs.gppSid = [PBSUtils.getRandomNumber(TCF_EU_V2.valueAsInt)]
         }
 
         when: "PBS processes auction request"
@@ -47,7 +51,7 @@ class GppAuctionSpec extends BaseSpec {
         given: "Default bid request with gppSid and gdpr"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             regs.gdpr = 0
-            regs.gppSid = [2]
+            regs.gppSid = [TCF_EU_V2.valueAsInt]
         }
 
         when: "PBS processes auction request"
@@ -63,7 +67,7 @@ class GppAuctionSpec extends BaseSpec {
         given: "Default bid request with gppSid and gdpr"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             regs.gdpr = 1
-            regs.gppSid = [PBSUtils.getRandomNumberWithExcept(2)]
+            regs.gppSid = [PBSUtils.getRandomNumberWithExcept(TCF_EU_V2.valueAsInt)]
         }
 
         when: "PBS processes auction request"
@@ -75,7 +79,7 @@ class GppAuctionSpec extends BaseSpec {
                 ["GPP scope does not match TCF2 scope"]
     }
 
-    def "PBS should emit warning when GPP string invalid"() {
+    def "PBS should emit warning when GPP string is invalid"() {
         given: "Default bid request with invalid gpp"
         def bidRequest = BidRequest.defaultBidRequest.tap {
             regs.gpp = "Invalid_GPP_Consent_String"
@@ -92,9 +96,9 @@ class GppAuctionSpec extends BaseSpec {
 
     def "PBS should copy regs.gpp to user.consent when gppSid contains 2, gpp is TCF2-EU and user.consent isn't specified"() {
         given: "Default bid request with gpp and gppSid"
-        def gppConsent = new GppConsent().setFieldValue()
+        def gppConsent = new GppConsent().setFieldValue(TcfEuV2.NAME)
         def bidRequest = BidRequest.defaultBidRequest.tap {
-            regs.gppSid = [2]
+            regs.gppSid = [TCF_EU_V2.valueAsInt]
             regs.gpp = gppConsent
             user = new User().tap {
                 consent = null
@@ -113,8 +117,8 @@ class GppAuctionSpec extends BaseSpec {
     def "PBS should emit warning when gppSid contains 2, gpp is TCF2-EU and regs.gpp and user.consent are different"() {
         given: "Default bid request with gpp and gppSid"
         def bidRequest = BidRequest.defaultBidRequest.tap {
-            regs.gppSid = [2]
-            regs.gpp = new GppConsent().setFieldValue()
+            regs.gppSid = [TCF_EU_V2.valueAsInt]
+            regs.gpp = new GppConsent().setFieldValue(TcfEuV2.NAME)
             user = new User(consent: new GppConsent().setFieldValue(TcfCaV1.NAME))
         }
 
@@ -127,11 +131,11 @@ class GppAuctionSpec extends BaseSpec {
                 ["GPP TCF2 string does not match user.consent"]
     }
 
-    def "PBS should copy regs.gpp to regs.usPrivacy when gppSid contains 6, gpp is TCF2-EU and regs.us_privacy isn't specified"() {
+    def "PBS should copy regs.gpp to regs.usPrivacy when gppSid contains 6, gpp is USP_V1 and regs.us_privacy isn't specified"() {
         given: "Default bid request with gpp and gppSid, without us_privacy"
-        def gppConsent = new GppConsent().setFieldValue()
+        def gppConsent = new GppConsent().setFieldValue(UspV1.NAME)
         def bidRequest = BidRequest.defaultBidRequest.tap {
-            regs.gppSid = [6]
+            regs.gppSid = [US_PV_V1.valueAsInt]
             regs.gpp = gppConsent
             regs.usPrivacy = null
         }
@@ -145,11 +149,11 @@ class GppAuctionSpec extends BaseSpec {
         assert bidderRequest.regs.gpp == gppConsent as String
     }
 
-    def "PBS shouldn't copy regs.gpp to regs.usPrivacy when gppSid doesn't contain 6, gpp is TCF2-EU and regs.us_privacy isn't specified"() {
+    def "PBS shouldn't copy regs.gpp to regs.usPrivacy when gppSid doesn't contain 6, gpp is USP_V1 and regs.us_privacy isn't specified"() {
         given: "Default bid request with gpp and gppSid, without us_privacy"
         def bidRequest = BidRequest.defaultBidRequest.tap {
-            regs.gppSid = [PBSUtils.getRandomNumberWithExcept(6)]
-            regs.gpp = new GppConsent().setFieldValue()
+            regs.gppSid = [PBSUtils.getRandomNumberWithExcept(US_PV_V1.valueAsInt)]
+            regs.gpp = new GppConsent().setFieldValue(UspV1.NAME)
             regs.usPrivacy = null
         }
 
@@ -161,11 +165,11 @@ class GppAuctionSpec extends BaseSpec {
         assert !bidderRequest.regs.usPrivacy
     }
 
-    def "PBS should emit warning when gppSid contains 6, gpp is TCF2-EU and regs.gpp and regs.usPrivacy are different"() {
+    def "PBS should emit warning when gppSid contains 6, gpp is USP_V1 and regs.gpp and regs.usPrivacy are different"() {
         given: "Default bid request with gpp, gppSid and usPrivacy"
         def bidRequest = BidRequest.defaultBidRequest.tap {
-            regs.gppSid = [6]
-            regs.gpp = new GppConsent().setFieldValue()
+            regs.gppSid = [US_PV_V1.valueAsInt]
+            regs.gpp = new GppConsent().setFieldValue(UspV1.NAME)
             regs.usPrivacy = new CcpaConsent(explicitNotice: ENFORCED, optOutSale: ENFORCED)
         }
 
