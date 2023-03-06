c0804dc69eecfb1d4ad316d2c5446294ca44a693
tamarm <tamarm@perion.com>
2023-03-06 13:16:02 +0200

    UndertoneRequestExt - @Value(staticConstructor = " of\)

--- src/main/java/org/prebid/server/bidder/undertone/proto/UndertoneRequestExt.java
+++ src/main/java/org/prebid/server/bidder/undertone/proto/UndertoneRequestExt.java
@@ -1,11 +1,9 @@
 package org.prebid.server.bidder.undertone.proto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
-@AllArgsConstructor(staticName = "of")
-@Value
+@Value(staticConstructor = "of")
 public class UndertoneRequestExt {
 
     @JsonProperty("id")
