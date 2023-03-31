2868203ec0e020097c4992ddcdac6e49b6eff243
Wesley Hall <99892134+wesley-freestar@users.noreply.github.com>
2023-03-31 11:06:02 +0100

    Freewheelssp: Add adapter (#2251)

---
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/freewheelssp/ExtImpFreewheelSSP.java
@@ -0,0 +1,9 @@
+package org.prebid.server.proto.openrtb.ext.request.freewheelssp;
+
+import lombok.Value;
+
+@Value(staticConstructor = "of")
+public class ExtImpFreewheelSSP {
+
+    Integer zoneId;
+}
