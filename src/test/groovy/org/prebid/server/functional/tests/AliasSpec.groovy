852d47f8fafcd1cc355a11fa13f837d20af39541
markiian <mmykush@magnite.com>
2023-04-12 18:39:36 +0300

    Update after review

--- src/test/groovy/org/prebid/server/functional/tests/AliasSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/AliasSpec.groovy
@@ -1,15 +1,16 @@
 package org.prebid.server.functional.tests
 
 import org.prebid.server.functional.model.bidder.Generic
-import org.prebid.server.functional.model.bidderspecific.BidderRequest
 import org.prebid.server.functional.model.request.auction.BidRequest
 import org.prebid.server.functional.service.PrebidServerException
 import org.prebid.server.functional.util.PBSUtils
 
+import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST
 import static org.prebid.server.functional.model.bidder.BidderName.ALIAS
 import static org.prebid.server.functional.model.bidder.BidderName.BOGUS
 import static org.prebid.server.functional.model.bidder.BidderName.GENERIC
 import static org.prebid.server.functional.model.bidder.CompressionType.GZIP
+import static org.prebid.server.functional.testcontainers.Dependencies.getNetworkServiceContainer
 import static org.prebid.server.functional.util.HttpUtil.CONTENT_ENCODING_HEADER
 
 class AliasSpec extends BaseSpec {
@@ -108,5 +109,41 @@ class AliasSpec extends BaseSpec {
 
         where:
         invalidId << [PBSUtils.randomNegativeNumber, 0]
+    }
+
+    def "PBS should emit error when alias didn't participate in request"() {
+        given: "Default bid request with alias"
+        def randomString = PBSUtils.randomString
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            ext.prebid.aliases = [(randomString): BOGUS]
+        }
+
+        when: "PBS processes auction request"
+        defaultPbsService.sendAuctionRequest(bidRequest)
+
+        then: "Request should fail with an error"
+        def exception = thrown(PrebidServerException)
+        assert exception.statusCode == BAD_REQUEST.code()
+        assert exception.responseBody == "Invalid request format: request.ext.prebid.aliases.$randomString " +
+                "refers to unknown bidder: $BOGUS.value"
+    }
+
+    def "PBS aliased bidder config should be independently from parent"() {
+        given: "Pbs config"
+        def prebidServerService = pbsServiceFactory.getService(
+                ["adapters.generic.aliases.alias.enabled" : "true",
+                 "adapters.generic.aliases.alias.endpoint": "$networkServiceContainer.rootUri/auction".toString()])
+
+        and: "Default bid request with alias"
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            imp[0].ext.prebid.bidder.alias = new Generic()
+        }
+
+        when: "PBS processes auction request"
+        prebidServerService.sendAuctionRequest(bidRequest)
+
+        then: "Bidder request should contain request per-alies"
+        def bidderRequests = bidder.getBidderRequests(bidRequest.id)
+        assert bidderRequests.size() == 2
     }
 }
