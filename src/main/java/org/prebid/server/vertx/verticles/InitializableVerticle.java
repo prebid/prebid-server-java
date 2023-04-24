3d284185f1cb75a2b034618554e8d1c1423b1bd2
Alex Maltsev <and1sscsgo@gmail.com>
2023-04-24 14:57:29 +0300

    Added docs, made init method abstract to force child classes to implement it.

--- src/main/java/org/prebid/server/vertx/verticles/InitializableVerticle.java
+++ src/main/java/org/prebid/server/vertx/verticles/InitializableVerticle.java
@@ -1,12 +1,16 @@
 package org.prebid.server.vertx.verticles;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
 /**
  * Base class for server Verticles, exists for making asynchronous verticles initialization synchronous,
- * so that server bootstrap will crash if verticle can't init.
+ * so that server bootstrap will crash if verticle can't init. Every child class should call either
+ * {@link #signalInitializationFailure(Throwable)} or {@link #signalInitializationFailure(Throwable)} at
+ * the end of initialization, which should be done in {@link #init(Vertx, Context)} method
  */
 public abstract class InitializableVerticle extends AbstractVerticle {
 
@@ -23,4 +27,7 @@ public abstract class InitializableVerticle extends AbstractVerticle {
     public Future<Void> getInitializationMarker() {
         return initializationPromise.future();
     }
+
+    @Override
+    public abstract void init(Vertx vertx, Context context);
 }
