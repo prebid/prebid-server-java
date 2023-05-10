4af8d63ec4c2b842b5b1034be59b9a84e155a464
Serhii Nahornyi <sergiy3344@gmail.com>
2023-05-10 16:37:49 +0300

    Axis: Add adapter

---
+++ src/main/java/org/prebid/server/spring/config/bidder/AxisConfiguration.java
@@ -0,0 +1,42 @@
+package org.prebid.server.spring.config.bidder;
+
+import org.prebid.server.bidder.BidderDeps;
+import org.prebid.server.bidder.axis.AxisBidder;
+import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.spring.config.bidder.model.BidderConfigurationProperties;
+import org.prebid.server.spring.config.bidder.util.BidderDepsAssembler;
+import org.prebid.server.spring.config.bidder.util.UsersyncerCreator;
+import org.prebid.server.spring.env.YamlPropertySourceFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.validation.constraints.NotBlank;
+
+@Configuration
+@PropertySource(value = "classpath:/bidder-config/axis.yaml", factory = YamlPropertySourceFactory.class)
+public class AxisConfiguration {
+
+    private static final String BIDDER_NAME = "axis";
+
+    @Bean("axisConfigurationProperties")
+    @ConfigurationProperties("adapters.axis")
+    BidderConfigurationProperties configurationProperties() {
+        return new BidderConfigurationProperties();
+    }
+
+    @Bean
+    BidderDeps axisBidderDeps(BidderConfigurationProperties axisConfigurationProperties,
+                              @NotBlank @Value("${external-url}") String externalUrl,
+                              JacksonMapper mapper) {
+
+        return BidderDepsAssembler.forBidder(BIDDER_NAME)
+                .withConfig(axisConfigurationProperties)
+                .usersyncerCreator(UsersyncerCreator.create(externalUrl))
+                .bidderCreator(config -> new AxisBidder(config.getEndpoint(), mapper))
+                .assemble();
+    }
+}
+
