e0f3a067a6c73ec6ab8af8cb47ab08e5a1b5c342
markiian <mmykush@magnite.com>
2023-05-31 11:29:01 +0300

    Add "gppSid" field to Condition obj

--- src/test/groovy/org/prebid/server/functional/model/request/auction/Condition.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/Condition.groovy
@@ -2,15 +2,21 @@ package org.prebid.server.functional.model.request.auction
 
 import groovy.transform.ToString
 import org.prebid.server.functional.model.bidder.BidderName
+import org.prebid.server.functional.model.request.GppSectionId
 
 @ToString(includeNames = true, ignoreNulls = true)
 class Condition {
 
+    List<Integer> gppSid
     List<ConditionType> componentType
     List<String> componentName
 
     static Condition getBaseCondition(String componentName = BidderName.GENERIC.value) {
-        new Condition(componentName: [componentName], componentType: [ConditionType.BIDDER])
+        new Condition().tap {
+            it.gppSid = [GppSectionId.TCF_EU_V2.getIntValue()]
+            it.componentType = [ConditionType.BIDDER]
+            it.componentName = [componentName]
+        }
     }
 
     enum ConditionType {
