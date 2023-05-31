45d4a9e7cf0e598a384944d48300dd8c3e1a34ef
Danylo <dubyk.dv@gmail.com>
2023-05-31 19:35:29 +0300

    Add unit tests for `ActivityRuleFactory`.

--- src/main/java/org/prebid/server/activity/infrastructure/creator/rule/AbstractRuleCreator.java
+++ src/main/java/org/prebid/server/activity/infrastructure/creator/rule/AbstractRuleCreator.java
@@ -20,9 +20,11 @@ public abstract class AbstractRuleCreator<T> implements RuleCreator<T> {
 
     @Override
     public Rule from(Object ruleConfiguration, GppContext gppContext) {
-        return relatedConfigurationClass.isInstance(ruleConfiguration)
-                ? fromConfiguration(relatedConfigurationClass.cast(ruleConfiguration), gppContext)
-                : null;
+        if (!relatedConfigurationClass.isInstance(ruleConfiguration)) {
+            throw new AssertionError();
+        }
+
+        return fromConfiguration(relatedConfigurationClass.cast(ruleConfiguration), gppContext);
     }
 
     protected abstract Rule fromConfiguration(T ruleConfiguration, GppContext gppContext);
