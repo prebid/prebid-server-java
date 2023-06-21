af091ab26a940a32d8c16277b15c63c0db0dcb63
markiian <mmykush@magnite.com>
2023-06-21 17:00:17 +0300

    Minor update price floors base spec

--- src/test/groovy/org/prebid/server/functional/tests/pricefloors/PriceFloorsBaseSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/pricefloors/PriceFloorsBaseSpec.groovy
@@ -6,6 +6,7 @@ import org.prebid.server.functional.model.config.AccountConfig
 import org.prebid.server.functional.model.config.AccountPriceFloorsConfig
 import org.prebid.server.functional.model.config.PriceFloorsFetch
 import org.prebid.server.functional.model.db.Account
+import org.prebid.server.functional.model.mock.services.currencyconversion.CurrencyConversionRatesResponse
 import org.prebid.server.functional.model.pricefloors.Country
 import org.prebid.server.functional.model.pricefloors.MediaType
 import org.prebid.server.functional.model.pricefloors.Rule
@@ -18,12 +19,15 @@ import org.prebid.server.functional.model.request.auction.Prebid
 import org.prebid.server.functional.model.request.auction.Video
 import org.prebid.server.functional.model.response.currencyrates.CurrencyRatesResponse
 import org.prebid.server.functional.service.PrebidServerService
+import org.prebid.server.functional.testcontainers.scaffolding.CurrencyConversion
 import org.prebid.server.functional.testcontainers.scaffolding.FloorsProvider
 import org.prebid.server.functional.tests.BaseSpec
 import org.prebid.server.functional.util.PBSUtils
 
 import java.math.RoundingMode
 
+import static org.prebid.server.functional.model.Currency.EUR
+import static org.prebid.server.functional.model.Currency.GBP
 import static org.prebid.server.functional.model.Currency.USD
 import static org.prebid.server.functional.model.request.auction.DistributionChannel.SITE
 import static org.prebid.server.functional.model.request.auction.FetchStatus.INPROGRESS
@@ -33,8 +37,16 @@ abstract class PriceFloorsBaseSpec extends BaseSpec {
 
     public static final BigDecimal FLOOR_MIN = 0.5
     public static final BigDecimal FLOOR_MAX = 2
-    public static final Map<String, String> FLOORS_CONFIG = ["price-floors.enabled"           : "true",
-                                                             "settings.default-account-config": encode(defaultAccountConfigSettings)]
+    public static final Map<String, String> floorsConfig = ["price-floors.enabled"           : "true",
+                                                            "settings.default-account-config": encode(defaultAccountConfigSettings)]
+
+    private static final Map<String, String> getExternalCurrencyConverterConfig() {
+        ["auction.ad-server-currency"                          : USD.toString(),
+         "currency-converter.external-rates.enabled"           : "true",
+         "currency-converter.external-rates.url"               : "$networkServiceContainer.rootUri/currency".toString(),
+         "currency-converter.external-rates.default-timeout-ms": "4000",
+         "currency-converter.external-rates.refresh-period-ms" : "900000"]
+    }
 
     protected static final String basicFetchUrl = networkServiceContainer.rootUri + FloorsProvider.FLOORS_ENDPOINT
     protected static final FloorsProvider floorsProvider = new FloorsProvider(networkServiceContainer)
@@ -43,13 +55,16 @@ abstract class PriceFloorsBaseSpec extends BaseSpec {
     private static final int DEFAULT_MODEL_WEIGHT = 1
     private static final int CURRENCY_CONVERSION_PRECISION = 3
     private static final int FLOOR_VALUE_PRECISION = 4
-    private static final Map<String, String> CURRENCY_CONVERTER_CONFIG = ["auction.ad-server-currency"                          : USD as String,
-                                                                          "currency-converter.external-rates.enabled"           : "true",
-                                                                          "currency-converter.external-rates.url"               : "$networkServiceContainer.rootUri/currency".toString(),
-                                                                          "currency-converter.external-rates.default-timeout-ms": "4000",
-                                                                          "currency-converter.external-rates.refresh-period-ms" : "900000"]
 
-    protected final PrebidServerService floorsPbsService = pbsServiceFactory.getService(FLOORS_CONFIG + CURRENCY_CONVERTER_CONFIG)
+    private static final Map<Currency, Map<Currency, BigDecimal>> DEFAULT_CURRENCY_RATES = [(USD): [(EUR): 0.9124920156948626,
+                                                                                                    (GBP): 0.793776804452961],
+                                                                                            (GBP): [(USD): 1.2597999770088517,
+                                                                                                    (EUR): 1.1495574203931487],
+                                                                                            (EUR): [(USD): 1.3429368029739777]]
+    private static final CurrencyConversion currencyConversion = new CurrencyConversion(networkServiceContainer).tap {
+        setCurrencyConversionRatesResponse(CurrencyConversionRatesResponse.getDefaultCurrencyConversionRatesResponse(DEFAULT_CURRENCY_RATES))
+    }
+    protected static final PrebidServerService floorsPbsService = pbsServiceFactory.getService(floorsConfig + externalCurrencyConverterConfig)
 
     def setupSpec() {
         floorsProvider.setResponse()
