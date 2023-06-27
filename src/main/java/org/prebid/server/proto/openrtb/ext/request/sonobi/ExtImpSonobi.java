4b0aa71570c40493ec8909fe9a9256de60b37eb4
SerhiiNahornyi <sergiy3344@gmail.com>
2023-06-27 16:43:03 +0300

    Rise: Add bidder (#2455)

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/sonobi/ExtImpSonobi.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/sonobi/ExtImpSonobi.java
@@ -1,14 +1,9 @@
 package org.prebid.server.proto.openrtb.ext.request.sonobi;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
-/**
- * Defines the contract for bidRequest.imp[i].ext.sonobi
- */
-@AllArgsConstructor(staticName = "of")
-@Value
+@Value(staticConstructor = "of")
 public class ExtImpSonobi {
 
     @JsonProperty("TagID")
