194f9509a215d0f408d0caa4bc4edb100627346b
Alex Maltsev <and1sscsgo@gmail.com>
2023-07-14 15:36:26 +0300

    Fixed styling

--- src/main/java/org/prebid/server/bidder/rubicon/proto/request/RubiconImpExtRp.java
+++ src/main/java/org/prebid/server/bidder/rubicon/proto/request/RubiconImpExtRp.java
@@ -1,7 +1,6 @@
 package org.prebid.server.bidder.rubicon.proto.request;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import lombok.AllArgsConstructor;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
