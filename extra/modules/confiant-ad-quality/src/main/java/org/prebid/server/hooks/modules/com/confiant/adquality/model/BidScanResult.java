324865e193f6e0e9829632c39a5aa53d2844ef87
IvanTsymbal <ivan@confiant.com>
2023-08-01 14:40:46 +0300

    feature: Confiant Module - Remove unused Redis result model properties

--- extra/modules/confiant-ad-quality/src/main/java/org/prebid/server/hooks/modules/com/confiant/adquality/model/BidScanResult.java
+++ extra/modules/confiant-ad-quality/src/main/java/org/prebid/server/hooks/modules/com/confiant/adquality/model/BidScanResult.java
@@ -1,11 +1,13 @@
 package org.prebid.server.hooks.modules.com.confiant.adquality.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BidScanResult {
 
     /**
@@ -21,36 +23,6 @@ public class BidScanResult {
     @JsonProperty("imp_id")
     String impId;
 
-    /** True if creative has already been scanned, false otherwise. */
-    @JsonProperty("known_creative")
-    boolean knownCreative;
-
-    /**
-     * True if creative has not been previously recorded in Redis (or has expired).
-     * The bid response must be re-submitted to the corresponding master Redis instance. Otherwise, false.
-     */
-    @JsonProperty("ro_skipped")
-    boolean roSkipped;
-
     /** An array of issue objects {@link Issue}. Only available if known_creative is true. */
     List<Issue> issues;
-
-    /** An attributes object {@link CreativeAttributes}. Only available if known_creative is true. */
-    CreativeAttributes attributes;
-
-    /** A metrics object {@link Metrics}. Only available if known_creative is true. */
-    Metrics metrics;
-
-    /**
-     * Adinstance ID of the most recent scan with unique results, populated even when no issue is found.
-     * Only available if known_creative is true.
-     */
-    String adinstance;
-
-    /** Screenshot of the creative */
-    String screenshot;
-
-    /** Identifier of the tag */
-    @JsonProperty("tag_id")
-    String tagId;
 }
