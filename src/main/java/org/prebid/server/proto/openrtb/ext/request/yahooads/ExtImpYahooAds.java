01cc1393849484d9aa6c4c717f837ea985383a8b
oath-jac <hb-fe-tech@oath.com>
2023-08-09 18:26:12 +0300

    Review comments - updates

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/yahooads/ExtImpYahooAds.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/yahooads/ExtImpYahooAds.java
@@ -2,6 +2,7 @@ package org.prebid.server.proto.openrtb.ext.request.yahooads;
 
 import lombok.AllArgsConstructor;
 import lombok.Value;
+
 @AllArgsConstructor(staticName = "of")
 @Value
 public class ExtImpYahooAds {
