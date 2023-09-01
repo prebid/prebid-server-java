69142355eb126ae8f889445034d332b34ff1884b
Irakli Gotsiridze <ikagotso@gmail.com>
2023-09-01 15:40:38 +0300

    Sovrn: Adapter to accept ad unit codes (#2593)

--- src/test/java/org/prebid/server/validation/BidderParamValidatorTest.java
+++ src/test/java/org/prebid/server/validation/BidderParamValidatorTest.java
@@ -179,7 +179,7 @@ public class BidderParamValidatorTest extends VertxTest {
     @Test
     public void validateShouldNotReturnValidationMessagesWhenSovrnImpExtIsOk() {
         // given
-        final ExtImpSovrn ext = ExtImpSovrn.of("tag", null, null);
+        final ExtImpSovrn ext = ExtImpSovrn.of("tag", null, null, null);
 
         final JsonNode node = mapper.convertValue(ext, JsonNode.class);
 
