afe61fcdcdd735f3d3aefc3a901cca3254b34fb8
Alex Maltsev <and1sscsgo@gmail.com>
2023-09-11 13:56:36 +0300

    Minor refactoring

--- src/main/java/org/prebid/server/bidder/ix/IxBidder.java
+++ src/main/java/org/prebid/server/bidder/ix/IxBidder.java
@@ -143,7 +143,7 @@ public class IxBidder implements Bidder<BidRequest> {
         final Integer w = banner.getW();
         final Integer h = banner.getH();
 
-        if (formats == null || formats.isEmpty() && h != null && w != null) {
+        if (CollectionUtils.isEmpty(formats) && h != null && w != null) {
             final List<Format> newFormats = Collections.singletonList(Format.builder().w(w).h(h).build());
             final Banner modifiedBanner = banner.toBuilder().format(newFormats).build();
             return UpdateResult.updated(modifiedBanner);
