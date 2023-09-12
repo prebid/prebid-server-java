c21c43272e912de8696ca0e78a41aba535825ff2
markiian <mmykush@magnite.com>
2023-09-12 13:27:16 +0300

    Move field to camelCase

--- src/test/groovy/org/prebid/server/functional/model/bidderspecific/Target.groovy
+++ src/test/groovy/org/prebid/server/functional/model/bidderspecific/Target.groovy
@@ -1,17 +1,20 @@
 package org.prebid.server.functional.model.bidderspecific
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
 
 @ToString(includeNames = true, ignoreNulls = true)
+@JsonNaming(PropertyNamingStrategies.LowerCaseStrategy)
 class Target {
 
     List<String> page
     List<String> keywords
-    List<String> buyeruid
-    List<String> buyeruids
+    List<String> buyerUid
+    List<String> buyerUids
     List<String> iab
-    List<String> sectioncat
-    List<String> pagecat
+    List<String> sectionCat
+    List<String> pageCat
     List<String> ref
     List<String> search
     List<String> domain
