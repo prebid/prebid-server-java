7cc6a4782d5fa2a191b64fa52e0aaf08862a3654
Alex Maltsev <and1sscsgo@gmail.com>
2023-09-18 18:17:02 +0300

    Removed test changes

--- src/main/java/org/prebid/server/Application.java
+++ src/main/java/org/prebid/server/Application.java
@@ -1,6 +1,5 @@
 package org.prebid.server;
 
-import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,17 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    static volatile int test = 0;
-
-    @SneakyThrows
     public static void main(String[] args) {
-        for (int i = 0; i < 60; i++) {
-            new Thread(() -> {
-                while (true) {
-                    test++;
-                }
-            }).start();
-        }
         SpringApplication.run(Application.class, args);
     }
 }
