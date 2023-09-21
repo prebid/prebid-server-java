bbce4aa731c87f6aacc2bbc9e65fc96f76916f85
markiian <mmykush@magnite.com>
2023-09-21 14:44:05 +0300

    Remove unused import

--- src/test/groovy/org/prebid/server/functional/tests/AmpFpdSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/AmpFpdSpec.groovy
@@ -19,7 +19,6 @@ import org.prebid.server.functional.model.request.auction.User
 import org.prebid.server.functional.service.PrebidServerException
 import org.prebid.server.functional.util.HttpUtil
 import org.prebid.server.functional.util.PBSUtils
-import spock.lang.RepeatUntilFailure
 
 import static io.netty.handler.codec.http.HttpResponseStatus.BAD_REQUEST
 import static org.prebid.server.functional.model.bidder.BidderName.ALIAS
