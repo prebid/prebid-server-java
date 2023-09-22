fc00a2196dc8d3d136499e64eb823f9df8a03e40
Serhii Kolomiiets <skolomiiets@magnite.com>
2023-09-22 14:50:04 +0300

    Finished transition period for alias resolving

--- src/main/java/org/prebid/server/auction/BidResponseCreator.java
+++ src/main/java/org/prebid/server/auction/BidResponseCreator.java
@@ -35,7 +35,6 @@ import org.prebid.server.auction.model.CategoryMappingResult;
 import org.prebid.server.auction.model.MultiBidConfig;
 import org.prebid.server.auction.model.TargetingInfo;
 import org.prebid.server.auction.model.debug.DebugContext;
-import org.prebid.server.auction.requestfactory.Ortb2ImplicitParametersResolver;
 import org.prebid.server.bidder.BidderCatalog;
 import org.prebid.server.bidder.model.BidderBid;
 import org.prebid.server.bidder.model.BidderError;
@@ -1515,16 +1514,7 @@ public class BidResponseCreator {
         final ExtRequestPrebid prebid = ext != null ? ext.getPrebid() : null;
         final ExtRequestPrebidChannel channel = prebid != null ? prebid.getChannel() : null;
 
-        return channel != null ? recogniseChannelName(channel.getName()) : null;
-    }
-
-    // TODO: remove alias resolving after transition period
-    private static String recogniseChannelName(String channelName) {
-        if (StringUtils.equalsIgnoreCase("pbjs", channelName)) {
-            return Ortb2ImplicitParametersResolver.WEB_CHANNEL;
-        }
-
-        return channelName;
+        return channel != null ? channel.getName() : null;
     }
 
     private static boolean eventsAllowedByRequest(AuctionContext auctionContext) {
