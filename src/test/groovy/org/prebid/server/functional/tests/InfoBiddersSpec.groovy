2dd3ca2ce83c1321b179cc95fa88823d8bce3ec6
Markiyan Mykush <95693607+marki1an@users.noreply.github.com>
2023-09-26 16:00:20 +0300

    Tests: Add functional tests for bidder info endpoint: `/info/bidders?baseAdaptersOnly` (#2645)

--- src/test/groovy/org/prebid/server/functional/tests/InfoBiddersSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/InfoBiddersSpec.groovy
@@ -5,26 +5,28 @@ import org.prebid.server.functional.util.PBSUtils
 
 class InfoBiddersSpec extends BaseSpec {
 
+    private static final String baseAdaptersOnly = "baseAdaptersOnly"
+
     def "PBS should get info about active bidders when enabledOnly = #enabledOnly"() {
         when: "PBS processes bidders info request"
-        def response = defaultPbsService.sendInfoBiddersRequest(enabledOnly)
+        def response = defaultPbsService.sendInfoBiddersRequest(["enabledonly": enabledOnly])
 
         then: "Response should contain only generic bidder"
         assert response == ["generic"]
 
         where:
-        enabledOnly << ["true", "True", "truE"]
+        enabledOnly << (1..3).collect { PBSUtils.getRandomCase("true") }
     }
 
     def "PBS should get info about all bidders when enabledOnly = #enabledOnly"() {
         when: "PBS processes bidders info request"
-        def response = defaultPbsService.sendInfoBiddersRequest(enabledOnly)
+        def response = defaultPbsService.sendInfoBiddersRequest(["enabledonly": enabledOnly])
 
         then: "Response should contain info about all bidders"
         assert response.size() > 1
 
         where:
-        enabledOnly << ["false", "False", "falsE"]
+        enabledOnly << (1..3).collect { PBSUtils.getRandomCase("false") }
     }
 
     def "PBS should get info about all bidders when enabledonly isn't passed"() {
@@ -37,7 +39,7 @@ class InfoBiddersSpec extends BaseSpec {
 
     def "PBS should return error when enabledOnly is incorrect"() {
         when: "PBS processes bidders info request"
-        defaultPbsService.sendInfoBiddersRequest(enabledOnly)
+        defaultPbsService.sendInfoBiddersRequest(["enabledonly": enabledOnly])
 
         then: "Request should fail with error"
         def exception = thrown(PrebidServerException)
@@ -46,5 +48,46 @@ class InfoBiddersSpec extends BaseSpec {
 
         where:
         enabledOnly << [PBSUtils.randomString, ""]
+    }
+
+    def "PBS should get info only about base bidders when baseAdaptersOnly = #baseAdaptersOnly"() {
+        when: "PBS processes bidders info request"
+        def response = defaultPbsService.sendInfoBiddersRequest([baseAdaptersOnly: baseAdaptersOnly])
+
+        then: "Response should contain info about base bidders only"
+        assert response.size() > 1
+
+        and: "Response shouldn't contain generic alias"
+        assert !response.contains("genericAlias")
+
+        where:
+        baseAdaptersOnly << (1..3).collect { PBSUtils.getRandomCase("true") }
+    }
+
+    def "PBS should get info about all bidders when baseAdaptersOnly = #baseAdaptersOnly"() {
+        when: "PBS processes bidders info request"
+        def response = defaultPbsService.sendInfoBiddersRequest([baseAdaptersOnly: baseAdaptersOnly])
+
+        then: "Response should contain info about all bidders"
+        assert response.size() > 1
+
+        and: "Response should contain generic alias"
+        assert response.contains("genericAlias")
+
+        where:
+        baseAdaptersOnly << (1..3).collect { PBSUtils.getRandomCase("false") }
+    }
+
+    def "PBS should return error when baseAdaptersOnly is incorrect"() {
+        when: "PBS processes bidders info request"
+        defaultPbsService.sendInfoBiddersRequest([baseAdaptersOnly: baseAdaptersOnly])
+
+        then: "Request should fail with error"
+        def exception = thrown(PrebidServerException)
+        assert exception.statusCode == 400
+        assert exception.responseBody == "Invalid value for 'baseadaptersonly' query param, must be of boolean type"
+
+        where:
+        baseAdaptersOnly << [PBSUtils.randomString, "", PBSUtils.randomNumber as String]
     }
 }
