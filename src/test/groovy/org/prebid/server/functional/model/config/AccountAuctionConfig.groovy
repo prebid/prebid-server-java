01a3893fbbf51b160ad1f726280b4e3dd94b864c
osulzhenko <osulzhenko@magnite.com>
2023-10-31 13:13:55 +0200

    Add functional tests for  targeting account level config

--- src/test/groovy/org/prebid/server/functional/model/config/AccountAuctionConfig.groovy
+++ src/test/groovy/org/prebid/server/functional/model/config/AccountAuctionConfig.groovy
@@ -3,6 +3,7 @@ package org.prebid.server.functional.model.config
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
+import org.prebid.server.functional.model.request.auction.Targeting
 
 @ToString(includeNames = true, ignoreNulls = true)
 @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy)
@@ -17,4 +18,5 @@ class AccountAuctionConfig {
     AccountEventsConfig events
     Boolean debugAllow
     AccountPriceFloorsConfig priceFloors
+    Targeting targeting
 }
