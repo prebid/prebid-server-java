6f5e5e02e1a69a61f8304d523b408f81d1ce6246
Danylo <dubyk.dv@gmail.com>
2023-11-28 14:53:59 +0200

    Fix functional tests.

--- src/test/java/org/prebid/server/it/AppnexusVideoTest.java
+++ src/test/java/org/prebid/server/it/AppnexusVideoTest.java
@@ -27,30 +27,30 @@ public class AppnexusVideoTest extends IntegrationTest {
                 .withHeader("Content-Type", equalToIgnoreCase("application/json;charset=UTF-8"))
                 .withHeader("Accept", equalTo("application/json"))
                 .withRequestBody(equalToJson(
-                        jsonFrom("openrtb2/video/test-video-appnexus-bid-request-1.json"), true, true))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/video/test-video-appnexus-bid-response-1.json"))));
+                        jsonFrom("openrtb2/appnexus/test-video-appnexus-bid-request-1.json"), true, true))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/appnexus/test-video-appnexus-bid-response-1.json"))));
 
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/appnexus-exchange"))
                 .withHeader("Content-Type", equalToIgnoreCase("application/json;charset=UTF-8"))
                 .withHeader("Accept", equalTo("application/json"))
                 .withRequestBody(equalToJson(
-                        jsonFrom("openrtb2/video/test-video-appnexus-bid-request-2.json"), true, true))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/video/test-video-appnexus-bid-response-2.json"))));
+                        jsonFrom("openrtb2/appnexus/test-video-appnexus-bid-request-2.json"), true, true))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/appnexus/test-video-appnexus-bid-response-2.json"))));
 
         // pre-bid cache
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/cache"))
                 .withRequestBody(equalToBidCacheRequest(
-                        jsonFrom("openrtb2/video/test-video-cache-request.json")))
+                        jsonFrom("openrtb2/appnexus/test-video-cache-request.json")))
                 .willReturn(aResponse()
                         .withTransformers("cache-response-transformer")
                         .withTransformerParameter("matcherName",
-                                "openrtb2/video/test-video-cache-response-matcher.json")));
+                                "openrtb2/appnexus/test-video-cache-response-matcher.json")));
 
         // when
-        final Response response = responseFor("openrtb2/video/test-video-appnexus-request.json",
+        final Response response = responseFor("openrtb2/appnexus/test-video-appnexus-request.json",
                 Endpoint.openrtb2_video);
 
         // then
-        assertJsonEquals("openrtb2/video/test-video-appnexus-response.json", response, emptyList());
+        assertJsonEquals("openrtb2/appnexus/test-video-appnexus-response.json", response, emptyList());
     }
 }
