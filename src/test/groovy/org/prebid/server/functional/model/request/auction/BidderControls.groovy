7a061eaeddbdd1638c46a0d6cc21cfe289714d00
markiian <mmykush@magnite.com>
2024-01-03 14:21:32 +0200

    Update path for preferredMediaType feature

--- src/test/groovy/org/prebid/server/functional/model/request/auction/BidderControls.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/BidderControls.groovy
@@ -3,8 +3,7 @@ package org.prebid.server.functional.model.request.auction
 import groovy.transform.ToString
 
 @ToString(includeNames = true, ignoreNulls = true)
-class PreferredBidders {
+class BidderControls {
 
     GenericPreferredBidder generic
-    GenericPreferredBidder bidder
 }
