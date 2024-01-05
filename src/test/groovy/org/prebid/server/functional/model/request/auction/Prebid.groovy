8d83fe8037ad7639acbade1de1b8fc1e6b673e9a
osulzhenko <osulzhenko@magnite.com>
2024-01-05 20:21:14 +0200

    code refactoring

--- src/test/groovy/org/prebid/server/functional/model/request/auction/Prebid.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/Prebid.groovy
@@ -35,7 +35,6 @@ class Prebid {
     Boolean createTids
     Sdk sdk
     List<AdServerTargeting> adServerTargeting
-    PreferredBidders bidders
     BidderControls bidderControls
     PrebidModulesConfig modules
 
--- src/test/groovy/org/prebid/server/functional/tests/module/richmedia/RichMediaFilterSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/module/richmedia/RichMediaFilterSpec.groovy
@@ -111,7 +111,7 @@ class RichMediaFilterSpec extends ModuleBaseSpec {
 
         and: "Stored bid response in DB"
         def storedBidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
-            it.seatbid[0].bid[0].adm = amdValue
+            it.seatbid[0].bid[0].adm = amdValue as String
         }
         def storedResponse = new StoredResponse(responseId: storedResponseId, storedBidResponse: storedBidResponse)
         storedResponseDao.save(storedResponse)
@@ -146,18 +146,18 @@ class RichMediaFilterSpec extends ModuleBaseSpec {
             it.imp.first().ext.prebid.storedBidResponse = [new StoredBidResponse(id: storedResponseId, bidder: GENERIC)]
         }
 
-        and: "Stored bid response in DB"
-        def storedBidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
-            it.seatbid[0].bid[0].adm = amdValue
-        }
-        def storedResponse = new StoredResponse(responseId: storedResponseId, storedBidResponse: storedBidResponse)
-        storedResponseDao.save(storedResponse)
-
         and: "Account with enabled richMedia config in the DB"
-        def richMediaFilterConfig = new PrebidModulesConfig(pbRichmediaFilter: new RichmediaFilter(filterMraid: true , mraidScriptPattern: PATTERN_NAME))
+        def richMediaFilterConfig = new PrebidModulesConfig(pbRichmediaFilter: new RichmediaFilter(filterMraid: true, mraidScriptPattern: PATTERN_NAME))
         def accountConfig = new AccountConfig(hooks: new AccountHooksConfiguration(modules: richMediaFilterConfig))
         def account = new Account(uuid: bidRequest.getAccountId(), config: accountConfig)
         accountDao.save(account)
+
+        and: "Stored bid response in DB"
+        def storedBidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
+            it.seatbid[0].bid[0].adm = amdValue as String
+        }
+        def storedResponse = new StoredResponse(responseId: storedResponseId, storedBidResponse: storedBidResponse)
+        storedResponseDao.save(storedResponse)
 
         when: "PBS processes auction request"
         def response = pbsServiceWithDisabledMediaFilter.sendAuctionRequest(bidRequest)
@@ -189,36 +189,30 @@ class RichMediaFilterSpec extends ModuleBaseSpec {
             it.imp.first().ext.prebid.storedBidResponse = [new StoredBidResponse(id: storedResponseId, bidder: GENERIC)]
         }
 
-        and: "Stored bid response in DB"
-        def storedBidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
-            it.seatbid[0].bid[0].adm = amdValue
-        }
-        def storedResponse = new StoredResponse(responseId: storedResponseId, storedBidResponse: storedBidResponse)
-        storedResponseDao.save(storedResponse)
-
-        and: "Account with enabled richMedia config in the DB"
+        and: "Account with disabled richMedia config in the DB"
         def richMediaFilterConfig = new PrebidModulesConfig(pbRichmediaFilter: new RichmediaFilter(filterMraid: false, mraidScriptPattern: PATTERN_NAME))
         def accountConfig = new AccountConfig(hooks: new AccountHooksConfiguration(modules: richMediaFilterConfig))
         def account = new Account(uuid: bidRequest.getAccountId(), config: accountConfig)
         accountDao.save(account)
 
+        and: "Stored bid response in DB"
+        def storedBidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
+            it.seatbid[0].bid[0].adm = amdValue as String
+        }
+        def storedResponse = new StoredResponse(responseId: storedResponseId, storedBidResponse: storedBidResponse)
+        storedResponseDao.save(storedResponse)
+
         when: "PBS processes auction request"
         def response = pbsServiceWithEnabledMediaFilter.sendAuctionRequest(bidRequest)
 
-        then: "Response header shouldn't contain any seatbid"
-        assert !response.seatbid
+        then: "Response header should contain seatbid"
+        assert response.seatbid.size() == 1
 
-        and: "Response should contain error of invalid creation for imp with code 350"
-        def responseErrors = response.ext.errors
-        assert responseErrors[ErrorType.GENERIC]*.message == ['Invalid creatives']
-        assert responseErrors[ErrorType.GENERIC]*.code == [350]
-        assert responseErrors[ErrorType.GENERIC].collectMany { it.impIds } == bidRequest.imp.id
+        and: "Response shouldn't contain errors of invalid creation"
+        assert !response.ext.errors
 
-        and: "Add an entry to the analytics tag for this rejected bid response"
-        def analyticsTags = getAnalyticResults(response)
-        assert analyticsTags.size() == 1
-        def analyticResult = analyticsTags.first()
-        assert analyticResult == AnalyticResult.buildFromImp(bidRequest.imp.first())
+        and: "Response shouldn't contain analytics"
+        assert !getAnalyticResults(response)
 
         where:
         amdValue << [PATTERN_NAME, "${PATTERN_NAME}.js", "${PBSUtils.randomString}-${PATTERN_NAME}.js", "${PATTERN_NAME}-${PBSUtils.randomString}.js"]
@@ -236,13 +230,13 @@ class RichMediaFilterSpec extends ModuleBaseSpec {
 
         and: "Stored bid response in DB"
         def storedBidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
-            it.seatbid[0].bid[0].adm = amdValue
+            it.seatbid[0].bid[0].adm = amdValue as String
         }
         def storedResponse = new StoredResponse(responseId: storedResponseId, storedBidResponse: storedBidResponse)
         storedResponseDao.save(storedResponse)
 
         when: "PBS processes auction request"
-        def response = pbsServiceWithEnabledMediaFilter.sendAuctionRequest(bidRequest)
+        def response = pbsServiceWithDisabledMediaFilter.sendAuctionRequest(bidRequest)
 
         then: "Response header shouldn't contain any seatbid"
         assert !response.seatbid
@@ -275,7 +269,7 @@ class RichMediaFilterSpec extends ModuleBaseSpec {
 
         and: "Stored bid response in DB"
         def storedBidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
-            it.seatbid[0].bid[0].adm = amdValue
+            it.seatbid[0].bid[0].adm = amdValue as String
         }
         def storedResponse = new StoredResponse(responseId: storedResponseId, storedBidResponse: storedBidResponse)
         storedResponseDao.save(storedResponse)
