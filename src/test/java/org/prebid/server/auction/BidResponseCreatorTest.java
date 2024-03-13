0b3e4992f09500a9ba380025f1892a460d496b6c
Alex Maltsev <and1sscsgo@gmail.com>
2024-03-13 15:41:44 +0200

    Fixed unit tests

--- src/test/java/org/prebid/server/auction/BidResponseCreatorTest.java
+++ src/test/java/org/prebid/server/auction/BidResponseCreatorTest.java
@@ -2652,7 +2652,8 @@ public class BidResponseCreatorTest extends VertxTest {
                         ExtBidderError.of(3, "Failed to decode: Cannot deserialize value"
                                 + " of type `com.iab.openrtb.response.Response` from Array value "
                                 + "(token `JsonToken.START_ARRAY`)\n"
-                                + " at [Source: (String)\"[]\"; line: 1, column: 1]"))),
+                                + " at [Source: REDACTED (`StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION`"
+                                + " disabled); line: 1, column: 1]"))),
                 entry("cache", singletonList(ExtBidderError.of(999, "cacheError"))));
 
         assertThat(responseExt.getResponsetimemillis()).hasSize(2)
@@ -3150,7 +3151,7 @@ public class BidResponseCreatorTest extends VertxTest {
                 .extracting(error -> error.get(bidder1))
                 .extracting(extBidderErrors -> extBidderErrors.get(0))
                 .isEqualTo(ExtBidderError.of(3, "No content to map due to end-of-input\n"
-                        + " at [Source: (String)\"\"; line: 1, column: 0]"));
+                        + " at [Source: REDACTED (`StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION` disabled); line: 1]"));
     }
 
     @Test
