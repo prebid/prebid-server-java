5470ffafe69e6116b109400c658c8ad0bfbd8df2
Oleksandr Zhevedenko <net.burst.mail@gmail.com>
2024-04-02 11:28:12 -0400

    Feature: Cross-currency conversion capability (#3084)
    * Added cross-currency conversion capability and updated precision determination
    
    * Small update to test

--- src/test/groovy/org/prebid/server/functional/tests/CurrencySpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/CurrencySpec.groovy
@@ -9,6 +9,8 @@ import org.prebid.server.functional.testcontainers.scaffolding.CurrencyConversio
 
 import java.math.RoundingMode
 
+import static org.prebid.server.functional.model.Currency.CAD
+import static org.prebid.server.functional.model.Currency.CHF
 import static org.prebid.server.functional.model.Currency.EUR
 import static org.prebid.server.functional.model.Currency.JPY
 import static org.prebid.server.functional.model.Currency.USD
@@ -18,8 +20,11 @@ class CurrencySpec extends BaseSpec {
 
     private static final Currency DEFAULT_CURRENCY = USD
     private static final int PRICE_PRECISION = 3
-    private static final Map<Currency, Map<Currency, BigDecimal>> DEFAULT_CURRENCY_RATES = [(USD): [(EUR): 0.8872327211427558,
-                                                                                                    (JPY): 114.12],
+    private static final Map<Currency, Map<Currency, BigDecimal>> DEFAULT_CURRENCY_RATES = [(USD): [(USD): 1,
+                                                                                                    (EUR): 0.9249838127832763,
+                                                                                                    (CHF): 0.9033391915641477,
+                                                                                                    (JPY): 151.1886041994265,
+                                                                                                    (CAD): 1.357136250115623],
                                                                                             (EUR): [(USD): 1.3429368029739777]]
     private static final CurrencyConversion currencyConversion = new CurrencyConversion(networkServiceContainer).tap {
         setCurrencyConversionRatesResponse(CurrencyConversionRatesResponse.getDefaultCurrencyConversionRatesResponse(DEFAULT_CURRENCY_RATES))
@@ -113,6 +118,34 @@ class CurrencySpec extends BaseSpec {
         JPY             || USD
     }
 
+    def "PBS should use cross currency conversion when direct, reverse and intermediate conversion is not available"() {
+        given: "Default BidRequest with #requestCurrency currency"
+        def bidRequest = BidRequest.defaultBidRequest.tap { cur = [requestCurrency] }
+
+        and: "Default Bid with a #bidCurrency currency"
+        def bidderResponse = BidResponse.getDefaultBidResponse(bidRequest).tap { cur = bidCurrency }
+        bidder.setResponse(bidRequest.id, bidderResponse)
+
+        when: "PBS processes auction request"
+        def bidResponse = pbsService.sendAuctionRequest(bidRequest)
+
+        then: "Auction response should contain bid in #requestCurrency currency"
+        assert bidResponse.cur == requestCurrency
+        def bidPrice = bidResponse.seatbid[0].bid[0].price
+        assert bidPrice == convertCurrency(bidderResponse.seatbid[0].bid[0].price, bidCurrency, requestCurrency)
+        assert bidResponse.seatbid[0].bid[0].ext.origbidcpm == bidderResponse.seatbid[0].bid[0].price
+        assert bidResponse.seatbid[0].bid[0].ext.origbidcur == bidCurrency
+
+        where:
+        requestCurrency || bidCurrency
+        CHF             || JPY
+        JPY             || CHF
+        CAD             || JPY
+        JPY             || CAD
+        EUR             || CHF
+        CHF             || EUR
+    }
+
     private static Map<String, String> getExternalCurrencyConverterConfig() {
         ["auction.ad-server-currency"                          : DEFAULT_CURRENCY as String,
          "currency-converter.external-rates.enabled"           : "true",
@@ -129,11 +162,26 @@ class CurrencySpec extends BaseSpec {
         def conversionRate
         if (fromCurrency == toCurrency) {
             conversionRate = 1
-        } else if (fromCurrency in DEFAULT_CURRENCY_RATES) {
+        } else if (toCurrency in DEFAULT_CURRENCY_RATES?[fromCurrency]) {
             conversionRate = DEFAULT_CURRENCY_RATES[fromCurrency][toCurrency]
-        } else {
+        } else if (fromCurrency in DEFAULT_CURRENCY_RATES?[toCurrency]) {
             conversionRate = 1 / DEFAULT_CURRENCY_RATES[toCurrency][fromCurrency]
+        } else {
+            conversionRate = getCrossConversionRate(fromCurrency, toCurrency)
         }
         conversionRate
+    }
+
+    private static BigDecimal getCrossConversionRate(Currency fromCurrency, Currency toCurrency) {
+        for (Map<Currency, BigDecimal> rates : DEFAULT_CURRENCY_RATES.values()) {
+            def fromRate = rates?[fromCurrency]
+            def toRate = rates?[toCurrency]
+
+            if (fromRate && toRate) {
+                return toRate / fromRate
+            }
+        }
+
+        null
     }
 }
