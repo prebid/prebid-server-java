d21205be09e66277589999fd10c01ffd304af033
markiian <mmykush@magnite.com>
2024-04-04 16:08:14 +0300

    Add new bwx bidder

---
+++ src/main/java/org/prebid/server/bidder/bwx/BwxBidder.java
@@ -0,0 +1,132 @@
+package org.prebid.server.bidder.bwx;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
+import io.vertx.core.http.HttpMethod;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.prebid.server.bidder.Bidder;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.BidderCall;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
+import org.prebid.server.exception.PreBidException;
+import org.prebid.server.json.DecodeException;
+import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.proto.openrtb.ext.ExtPrebid;
+import org.prebid.server.proto.openrtb.ext.request.bwx.ExtImpBwx;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.util.HttpUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class BwxBidder implements Bidder<BidRequest> {
+
+    private static final TypeReference<ExtPrebid<?, ExtImpBwx>> BWX_EXT_TYPE_REFERENCE =
+            new TypeReference<>() {
+            };
+    private static final String URL_HOST_MACRO = "{{Host}}";
+    private static final String PUBLISHER_ID_MACRO = "{{SourceId}}";
+    private final String endpointUrl;
+    private final JacksonMapper mapper;
+
+    public BwxBidder(String endpointUrl, JacksonMapper mapper) {
+        this.endpointUrl = HttpUtil.validateUrl(Objects.requireNonNull(endpointUrl));
+        this.mapper = Objects.requireNonNull(mapper);
+    }
+
+    @Override
+    public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest request) {
+        final List<HttpRequest<BidRequest>> httpRequests = new ArrayList<>();
+        final List<BidderError> errors = new ArrayList<>();
+
+        for (Imp imp : request.getImp()) {
+            final ExtImpBwx extImpBwx;
+            try {
+                extImpBwx = parseImpExt(imp);
+            } catch (PreBidException e) {
+                errors.add(BidderError.badInput(e.getMessage()));
+                continue;
+            }
+            httpRequests.add(createHttpRequest(request, extImpBwx));
+        }
+
+        return Result.of(httpRequests, errors);
+    }
+
+    private ExtImpBwx parseImpExt(Imp imp) {
+        try {
+            return mapper.mapper().convertValue(imp.getExt(), BWX_EXT_TYPE_REFERENCE).getBidder();
+        } catch (IllegalArgumentException e) {
+            throw new PreBidException("Missing bidder ext in impression with id: " + imp.getId());
+        }
+    }
+
+    private HttpRequest<BidRequest> createHttpRequest(BidRequest request, ExtImpBwx extImpBwx) {
+        return HttpRequest.<BidRequest>builder()
+                .method(HttpMethod.POST)
+                .uri(resolveEndpoint(extImpBwx))
+                .body(mapper.encodeToBytes(request))
+                .payload(request)
+                .headers(HttpUtil.headers())
+                .build();
+    }
+
+    private String resolveEndpoint(ExtImpBwx extImpBwx) {
+        return endpointUrl
+                .replace(URL_HOST_MACRO, StringUtils.defaultString(extImpBwx.getEnv()))
+                .replace(PUBLISHER_ID_MACRO, StringUtils.defaultString(extImpBwx.getPid()));
+    }
+
+    @Override
+    public Result<List<BidderBid>> makeBids(BidderCall<BidRequest> httpCall, BidRequest bidRequest) {
+        try {
+            final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
+            return Result.withValues(extractBids(bidResponse));
+        } catch (PreBidException | DecodeException e) {
+            return Result.withError(BidderError.badServerResponse(e.getMessage()));
+        }
+    }
+
+    private List<BidderBid> extractBids(BidResponse bidResponse) {
+        if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
+            return Collections.emptyList();
+        }
+
+        return bidsFromResponse(bidResponse);
+    }
+
+    private List<BidderBid> bidsFromResponse(BidResponse bidResponse) {
+        return bidResponse.getSeatbid().stream()
+                .filter(Objects::nonNull)
+                .map(SeatBid::getBid)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .map(bid -> BidderBid.of(bid, getBidType(bid), bidResponse.getCur()))
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+    private static BidType getBidType(Bid bid) {
+        final Integer mType = ObjectUtils.defaultIfNull(bid.getMtype(), 0);
+        return switch (mType) {
+            case 1 -> BidType.banner;
+            case 2 -> BidType.video;
+            case 4 -> BidType.xNative;
+            default -> throw new PreBidException(
+                    "Failed to parse bid mtype: %s for impression id %s".formatted(bid.getMtype(), bid.getImpid())
+            );
+        };
+    }
+}
