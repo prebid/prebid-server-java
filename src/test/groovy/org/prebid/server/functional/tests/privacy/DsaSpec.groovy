e7c0959ed0b795f7dd8f0e09ba799a44983cf65c
markiian <mmykush@magnite.com>
2024-04-24 11:04:52 +0000

    Update tests for DSA validation

--- src/test/groovy/org/prebid/server/functional/tests/privacy/DsaSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/privacy/DsaSpec.groovy
@@ -19,7 +19,7 @@ import static org.prebid.server.functional.model.request.auction.DsaRequired.NOT
 import static org.prebid.server.functional.model.request.auction.DsaRequired.REQUIRED
 import static org.prebid.server.functional.model.request.auction.DsaRequired.REQUIRED_PUBLISHER_IS_ONLINE_PLATFORM
 import static org.prebid.server.functional.model.request.auction.DsaRequired.SUPPORTED
-import static org.prebid.server.functional.model.response.auction.BidRejectionReason.GENERAL
+import static org.prebid.server.functional.model.response.auction.BidRejectionReason.REJECTED_DUE_TO_DSA
 import static org.prebid.server.functional.model.response.auction.DsaAdRender.ADVERTISER_WILL_RENDER
 import static org.prebid.server.functional.model.response.auction.DsaAdRender.ADVERTISER_WONT_RENDER
 import static org.prebid.server.functional.model.response.auction.ErrorType.GENERIC
@@ -133,8 +133,7 @@ class DsaSpec extends PrivacyBaseSpec {
         assert !response.ext.errors
 
         where:
-        dsaRequired << [NOT_REQUIRED,
-                        SUPPORTED]
+        dsaRequired << [NOT_REQUIRED, SUPPORTED]
     }
 
     def "AMP request should reject bids without DSA when dsarequired is #dsaRequired"() {
@@ -169,11 +168,10 @@ class DsaSpec extends PrivacyBaseSpec {
         and: "Response should contain an error"
         def bidId = bidResponse.seatbid[0].bid[0].id
         assert response.ext?.warnings[GENERIC]*.code == [5]
-        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\" missing DSA"]
+        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\": DSA object missing when required"]
 
         where:
-        dsaRequired << [REQUIRED,
-                        REQUIRED_PUBLISHER_IS_ONLINE_PLATFORM]
+        dsaRequired << [REQUIRED, REQUIRED_PUBLISHER_IS_ONLINE_PLATFORM]
     }
 
     def "Auction request should always forward DSA to bidders"() {
@@ -259,8 +257,7 @@ class DsaSpec extends PrivacyBaseSpec {
         assert !response.ext.errors
 
         where:
-        dsaRequired << [NOT_REQUIRED,
-                        SUPPORTED]
+        dsaRequired << [NOT_REQUIRED, SUPPORTED]
     }
 
     def "Auction request should reject bids without DSA when dsarequired is #dsaRequired"() {
@@ -286,11 +283,10 @@ class DsaSpec extends PrivacyBaseSpec {
         and: "Response should contain an error"
         def bidId = bidResponse.seatbid[0].bid[0].id
         assert response.ext?.warnings[GENERIC]*.code == [5]
-        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\" missing DSA"]
+        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\": DSA object missing when required"]
 
         where:
-        dsaRequired << [REQUIRED,
-                        REQUIRED_PUBLISHER_IS_ONLINE_PLATFORM]
+        dsaRequired << [REQUIRED, REQUIRED_PUBLISHER_IS_ONLINE_PLATFORM]
     }
 
     def "Auction request should reject bids without DSA and populate seatNonBid when dsarequired is #dsaRequired"() {
@@ -320,97 +316,15 @@ class DsaSpec extends PrivacyBaseSpec {
         def seatNonBid = response.ext.seatnonbid[0]
         assert seatNonBid.seat == GENERIC.value
         assert seatNonBid.nonBid[0].impId == bidRequest.imp[0].id
-        assert seatNonBid.nonBid[0].statusCode == GENERAL
+        assert seatNonBid.nonBid[0].statusCode == REJECTED_DUE_TO_DSA
 
         and: "Response should contain an error"
         def bidId = bidResponse.seatbid[0].bid[0].id
         assert response.ext?.warnings[GENERIC]*.code == [5]
-        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\" missing DSA"]
+        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\": DSA object missing when required"]
 
         where:
-        dsaRequired << [REQUIRED,
-                        REQUIRED_PUBLISHER_IS_ONLINE_PLATFORM]
-    }
-
-    def "Auction request should reject bids with DSA when pubRender is #pubRender and adRender is #adRender"() {
-        given: "Default bid request with DSA pubRender"
-        def bidRequest = BidRequest.defaultBidRequest.tap {
-            ext.prebid.returnAllBidStatus = true
-            regs.ext.dsa = new RequestDsa(pubRender: pubRender)
-        }
-
-        and: "Default bidder response with incorrect DSA adRender"
-        def bidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
-            seatbid[0].bid[0].ext = new BidExt(dsa: new DsaResponse(adRender: adRender))
-        }
-
-        and: "Set bidder response"
-        bidder.setResponse(bidRequest.id, bidResponse)
-
-        when: "PBS processes auction request"
-        def response = privacyPbsService.sendAuctionRequest(bidRequest)
-
-        then: "PBS should reject bid"
-        assert !response.seatbid
-
-        and: "PBS response should contain seatNonBid for rejected bids"
-        assert response.ext.seatnonbid.size() == 1
-
-        def seatNonBid = response.ext.seatnonbid[0]
-        assert seatNonBid.seat == GENERIC.value
-        assert seatNonBid.nonBid[0].impId == bidRequest.imp[0].id
-        assert seatNonBid.nonBid[0].statusCode == GENERAL
-
-        and: "Response should contain an error"
-        def bidId = bidResponse.seatbid[0].bid[0].id
-        assert response.ext?.warnings[GENERIC]*.code == [305]
-//  TODO      Add error message
-        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\" missing DSA"]
-
-        where:
-        pubRender       | adRender
-        PUB_WILL_RENDER | ADVERTISER_WILL_RENDER
-        PUB_CANT_RENDER | ADVERTISER_WONT_RENDER
-    }
-
-    def "Auction request should reject bids with DSA when dsa response have paid or behalf fields longer then 100 characters"() {
-        given: "Default bid request with DSA pubRender"
-        def bidRequest = BidRequest.defaultBidRequest.tap {
-            ext.prebid.returnAllBidStatus = true
-            regs.ext.dsa = RequestDsa.defaultDsa
-        }
-
-        and: "Default bidder response with incorrect DSA"
-        def bidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
-            seatbid[0].bid[0].ext = new BidExt(dsa: invalidDsaResponse)
-        }
-
-        and: "Set bidder response"
-        bidder.setResponse(bidRequest.id, bidResponse)
-
-        when: "PBS processes auction request"
-        def response = privacyPbsService.sendAuctionRequest(bidRequest)
-
-        then: "PBS should reject bid"
-        assert !response.seatbid
-
-        and: "PBS response should contain seatNonBid for rejected bids"
-        assert response.ext.seatnonbid.size() == 1
-
-        def seatNonBid = response.ext.seatnonbid[0]
-        assert seatNonBid.seat == GENERIC.value
-        assert seatNonBid.nonBid[0].impId == bidRequest.imp[0].id
-        assert seatNonBid.nonBid[0].statusCode == GENERAL
-
-        and: "Response should contain an error"
-        def bidId = bidResponse.seatbid[0].bid[0].id
-        assert response.ext?.warnings[GENERIC]*.code == [305]
-        //  TODO Add error message
-        assert response.ext?.warnings[GENERIC]*.message == ["Error message"]
-
-        where:
-        invalidDsaResponse << [new DsaResponse(paid: PBSUtils.getRandomString(101)),
-                               new DsaResponse(behalf: PBSUtils.getRandomString(101))]
+        dsaRequired << [REQUIRED, REQUIRED_PUBLISHER_IS_ONLINE_PLATFORM]
     }
 
     def "Auction request should set account DSA when BidRequest DSA is null"() {
@@ -549,5 +463,87 @@ class DsaSpec extends PrivacyBaseSpec {
 
         then: "Bidder request shouldn't contain DSA"
         assert !bidder.getBidderRequest(bidRequest.id)?.regs?.ext?.dsa
+    }
+
+    def "Auction request should reject bids with DSA when pubRender is #pubRender and adRender is #adRender"() {
+        given: "Default bid request with DSA pubRender"
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            ext.prebid.returnAllBidStatus = true
+            regs.ext.dsa = RequestDsa.getDefaultDsa(REQUIRED).tap {
+                it.pubRender = pubRender
+            }
+        }
+
+        and: "Default bidder response with incorrect DSA adRender"
+        def bidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
+            seatbid[0].bid[0].ext = new BidExt(dsa: new DsaResponse(adRender: adRender))
+        }
+
+        and: "Set bidder response"
+        bidder.setResponse(bidRequest.id, bidResponse)
+
+        when: "PBS processes auction request"
+        def response = privacyPbsService.sendAuctionRequest(bidRequest)
+
+        then: "PBS should reject bid"
+        assert !response.seatbid
+
+        and: "PBS response should contain seatNonBid for rejected bids"
+        assert response.ext.seatnonbid.size() == 1
+
+        def seatNonBid = response.ext.seatnonbid[0]
+        assert seatNonBid.seat == GENERIC.value
+        assert seatNonBid.nonBid[0].impId == bidRequest.imp[0].id
+        assert seatNonBid.nonBid[0].statusCode == REJECTED_DUE_TO_DSA
+
+        and: "Response should contain an error"
+        def bidId = bidResponse.seatbid[0].bid[0].id
+        assert response.ext?.warnings[GENERIC]*.code == [5]
+        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\": ${warningMessage}"]
+
+        where:
+        warningMessage                                        | pubRender       | adRender
+        "DSA publisher and buyer both signal will render"     | PUB_WILL_RENDER | ADVERTISER_WILL_RENDER
+        "DSA publisher and buyer both signal will not render" | PUB_CANT_RENDER | ADVERTISER_WONT_RENDER
+    }
+
+    def "Auction request should reject bids with DSA when dsa response have paid or behalf fields longer then 100 characters"() {
+        given: "Default bid request with DSA pubRender"
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            ext.prebid.returnAllBidStatus = true
+            regs.ext.dsa = RequestDsa.getDefaultDsa(REQUIRED)
+        }
+
+        and: "Default bidder response with incorrect DSA"
+        def bidResponse = BidResponse.getDefaultBidResponse(bidRequest).tap {
+            seatbid[0].bid[0].ext = new BidExt(dsa: invalidDsaResponse)
+        }
+
+        and: "Set bidder response"
+        bidder.setResponse(bidRequest.id, bidResponse)
+
+        when: "PBS processes auction request"
+        def response = privacyPbsService.sendAuctionRequest(bidRequest)
+
+        then: "PBS should reject bid"
+        assert !response.seatbid
+
+        and: "PBS response should contain seatNonBid for rejected bids"
+        assert response.ext.seatnonbid.size() == 1
+
+        def seatNonBid = response.ext.seatnonbid[0]
+        assert seatNonBid.seat == GENERIC.value
+        assert seatNonBid.nonBid[0].impId == bidRequest.imp[0].id
+        assert seatNonBid.nonBid[0].statusCode == REJECTED_DUE_TO_DSA
+
+        and: "Response should contain an error"
+        def bidId = bidResponse.seatbid[0].bid[0].id
+        assert response.ext?.warnings[GENERIC]*.code == [5]
+        assert response.ext?.warnings[GENERIC]*.message == ["Bid \"$bidId\": ${warningMessage}"]
+
+        where:
+        warningMessage                          | invalidDsaResponse
+        "DSA paid exceeds limit of 100 chars"   | new DsaResponse(paid: PBSUtils.getRandomString(101))
+        "DSA behalf exceeds limit of 100 chars" | new DsaResponse(behalf: PBSUtils.getRandomString(101))
     }
 }
