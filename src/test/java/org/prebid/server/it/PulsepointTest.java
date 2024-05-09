bcaeced7fdd432f82710a5d752ff551a7a8a79bb
anand-venkatraman <avenkatraman@pulsepoint.com>
2024-05-09 23:09:17 +0530

    PulsePoint Adapter: ensuring ct/cp params can be either int or string

--- src/test/java/org/prebid/server/it/PulsepointTest.java
+++ src/test/java/org/prebid/server/it/PulsepointTest.java
@@ -20,13 +20,23 @@ public class PulsepointTest extends IntegrationTest {
 
     @Test
     public void openrtb2AuctionShouldRespondWithBidsFromPulsepoint() throws IOException, JSONException {
+        // pulsepoint params as integers
+        openrtb2AuctionShouldRespondWithBidsFromPulsepoint("openrtb2/pulsepoint/test-auction-pulsepoint-request.json",
+                "openrtb2/pulsepoint/test-pulsepoint-bid-request.json");
+
+        // pulsepoint params as string
+        openrtb2AuctionShouldRespondWithBidsFromPulsepoint("openrtb2/pulsepoint/test-auction-pulsepoint-request-params-as-string.json",
+                "openrtb2/pulsepoint/test-pulsepoint-bid-request-params-as-string.json");
+    }
+
+    private void openrtb2AuctionShouldRespondWithBidsFromPulsepoint(String auctionFilePath, String pulsepointRequestPath) throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/pulsepoint-exchange"))
-                .withRequestBody(equalToJson(jsonFrom("openrtb2/pulsepoint/test-pulsepoint-bid-request.json")))
+                .withRequestBody(equalToJson(jsonFrom(pulsepointRequestPath)))
                 .willReturn(aResponse().withBody(jsonFrom("openrtb2/pulsepoint/test-pulsepoint-bid-response.json"))));
 
         // when
-        final Response response = responseFor("openrtb2/pulsepoint/test-auction-pulsepoint-request.json",
+        final Response response = responseFor(auctionFilePath,
                 Endpoint.openrtb2_auction);
 
         // then
