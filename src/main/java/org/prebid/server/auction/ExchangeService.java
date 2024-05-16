c5a4c6402c577cc395ee6ac7d2dfc9a0bfa907df
Danylo <dubyk.dv@gmail.com>
2024-05-16 16:44:07 +0300

    Add unit tests.

--- src/main/java/org/prebid/server/auction/ExchangeService.java
+++ src/main/java/org/prebid/server/auction/ExchangeService.java
@@ -110,6 +110,7 @@ import org.prebid.server.proto.openrtb.ext.request.TraceLevel;
 import org.prebid.server.proto.openrtb.ext.response.ExtAnalyticsTags;
 import org.prebid.server.proto.openrtb.ext.response.ExtBidResponse;
 import org.prebid.server.proto.openrtb.ext.response.ExtBidResponsePrebid;
+import org.prebid.server.proto.openrtb.ext.response.ExtBidderError;
 import org.prebid.server.proto.openrtb.ext.response.ExtModules;
 import org.prebid.server.proto.openrtb.ext.response.ExtModulesTrace;
 import org.prebid.server.proto.openrtb.ext.response.ExtModulesTraceAnalyticsActivity;
@@ -260,6 +261,7 @@ public class ExchangeService {
     public Future<AuctionContext> holdAuction(AuctionContext context) {
         return processAuctionRequest(context)
                 .compose(this::invokeResponseHooks)
+                .map(this::enrichWithAnalyticsTags)
                 .map(this::enrichWithHooksDebugInfo)
                 .map(this::updateHooksMetrics);
     }
@@ -1685,11 +1687,105 @@ public class ExchangeService {
         };
     }
 
-    private AuctionContext enrichWithHooksDebugInfo(AuctionContext context) {
-        final ExtModules extModules = toExtModules(context);
+    private AuctionContext enrichWithAnalyticsTags(AuctionContext context) {
+        final boolean clientDetailsEnabled = isClientDetailsEnabled(context);
+        if (!clientDetailsEnabled) {
+            return context;
+        }
+
+        final boolean allowClientDetails = Optional.ofNullable(context.getAccount())
+                .map(Account::getAnalytics)
+                .map(AccountAnalyticsConfig::isAllowClientDetails)
+                .orElse(false);
+
+        if (!allowClientDetails) {
+            return addClientDetailsWarning(context);
+        }
+
         final List<ExtAnalyticsTags> extAnalyticsTags = toExtAnalyticsTags(context);
 
-        if (extModules == null && extAnalyticsTags == null) {
+        if (extAnalyticsTags == null) {
+            return context;
+        }
+
+        final BidResponse bidResponse = context.getBidResponse();
+        final Optional<ExtBidResponse> ext = Optional.ofNullable(bidResponse.getExt());
+        final Optional<ExtBidResponsePrebid> extPrebid = ext.map(ExtBidResponse::getPrebid);
+
+        final ExtBidResponsePrebid updatedExtPrebid = extPrebid
+                .map(ExtBidResponsePrebid::toBuilder)
+                .orElse(ExtBidResponsePrebid.builder())
+                .analyticsTags(extAnalyticsTags)
+                .build();
+
+        final ExtBidResponse updatedExt = ext
+                .map(ExtBidResponse::toBuilder)
+                .orElse(ExtBidResponse.builder())
+                .prebid(updatedExtPrebid)
+                .build();
+
+        final BidResponse updatedBidResponse = bidResponse.toBuilder().ext(updatedExt).build();
+        return context.with(updatedBidResponse);
+    }
+
+    private static boolean isClientDetailsEnabled(AuctionContext context) {
+        final JsonNode analytics = Optional.ofNullable(context.getBidRequest())
+                .map(BidRequest::getExt)
+                .map(ExtRequest::getPrebid)
+                .map(ExtRequestPrebid::getAnalytics)
+                .orElse(null);
+
+        if (notObjectNode(analytics)) {
+            return false;
+        }
+
+        final JsonNode options = analytics.get("options");
+        if (notObjectNode(options)) {
+            return false;
+        }
+
+        final JsonNode enableClientDetails = options.get("enableclientdetails");
+        return enableClientDetails != null
+                && enableClientDetails.isBoolean()
+                && enableClientDetails.asBoolean();
+    }
+
+    private static boolean notObjectNode(JsonNode jsonNode) {
+        return jsonNode == null || !jsonNode.isObject();
+    }
+
+    private static AuctionContext addClientDetailsWarning(AuctionContext context) {
+        final BidResponse bidResponse = context.getBidResponse();
+        final Optional<ExtBidResponse> ext = Optional.ofNullable(bidResponse.getExt());
+
+        final Map<String, List<ExtBidderError>> warnings = ext
+                .map(ExtBidResponse::getWarnings)
+                .orElse(Collections.emptyMap());
+        final List<ExtBidderError> prebidWarnings = ObjectUtils.defaultIfNull(
+                warnings.get(BidResponseCreator.DEFAULT_DEBUG_KEY),
+                Collections.emptyList());
+
+        final List<ExtBidderError> updatedPrebidWarnings = new ArrayList<>(prebidWarnings);
+        updatedPrebidWarnings.add(ExtBidderError.of(
+                BidderError.Type.generic.getCode(),
+                "analytics.options.enableclientdetails not enabled for account"));
+        final Map<String, List<ExtBidderError>> updatedWarnings = new HashMap<>(warnings);
+        updatedWarnings.put(BidResponseCreator.DEFAULT_DEBUG_KEY, updatedPrebidWarnings);
+
+        final ExtBidResponse updatedExt = ext
+                .map(ExtBidResponse::toBuilder)
+                .orElse(ExtBidResponse.builder())
+                .warnings(updatedWarnings)
+                .build();
+
+        final BidResponse updatedBidResponse = bidResponse.toBuilder().ext(updatedExt).build();
+        return context.with(updatedBidResponse);
+    }
+
+    private AuctionContext enrichWithHooksDebugInfo(AuctionContext context) {
+        final ExtModules extModules = toExtModules(context);
+
+        if (extModules == null) {
             return context;
         }
 
@@ -1701,7 +1797,6 @@ public class ExchangeService {
                 .map(ExtBidResponsePrebid::toBuilder)
                 .orElse(ExtBidResponsePrebid.builder())
                 .modules(extModules)
-                .analyticsTags(extAnalyticsTags)
                 .build();
 
         final ExtBidResponse updatedExt = ext
@@ -1879,57 +1974,18 @@ public class ExchangeService {
     }
 
     private static List<ExtAnalyticsTags> toExtAnalyticsTags(AuctionContext context) {
-        final JsonNode analytics = Optional.ofNullable(context.getBidRequest().getExt())
-                .map(ExtRequest::getPrebid)
-                .map(ExtRequestPrebid::getAnalytics)
-                .orElse(null);
-
-        final boolean enableClientDetails = enableClientDetails(analytics);
-        if (!enableClientDetails) {
-            return null;
-        }
-
-        final boolean allowClientDetails = Optional.ofNullable(context.getAccount())
-                .map(Account::getAnalytics)
-                .map(AccountAnalyticsConfig::isAllowClientDetails)
-                .orElse(false);
-
-        if (!allowClientDetails) {
-            context.getDebugWarnings().add("analytics.options.enableclientdetails not enabled for account");
-            return null;
-        }
-
         return context.getHookExecutionContext().getStageOutcomes().entrySet().stream()
                 .flatMap(stageToExecutionOutcome -> stageToExecutionOutcome.getValue().stream()
                         .map(StageExecutionOutcome::getGroups)
                         .flatMap(Collection::stream)
                         .map(GroupExecutionOutcome::getHooks)
                         .flatMap(Collection::stream)
+                        .filter(hookExecutionOutcome -> hookExecutionOutcome.getAnalyticsTags() != null)
                         .map(hookExecutionOutcome -> ExtAnalyticsTags.of(
                                 stageToExecutionOutcome.getKey(),
                                 hookExecutionOutcome.getHookId().getModuleCode(),
                                 toTraceAnalyticsTags(hookExecutionOutcome.getAnalyticsTags()))))
                 .toList();
-    }
-
-    private static boolean enableClientDetails(JsonNode analytics) {
-        if (notObjectNode(analytics)) {
-            return false;
-        }
-
-        final JsonNode options = analytics.get("options");
-        if (notObjectNode(options)) {
-            return false;
-        }
-
-        final JsonNode enableClientDetails = options.get("enableclientdetails");
-        return enableClientDetails != null
-                && enableClientDetails.isBoolean()
-                && enableClientDetails.asBoolean();
-    }
-
-    private static boolean notObjectNode(JsonNode jsonNode) {
-        return jsonNode == null || !jsonNode.isObject();
     }
 
     private AuctionContext updateHooksMetrics(AuctionContext context) {
