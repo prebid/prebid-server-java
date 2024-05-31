39d0b8d217707a1f5d5681730219ce272d875245
Danylo <dubyk.dv@gmail.com>
2024-05-31 03:35:33 +0300

    Add support for `AccountAnalyticsConfig`.

--- src/main/java/org/prebid/server/settings/model/AccountAnalyticsConfig.java
+++ src/main/java/org/prebid/server/settings/model/AccountAnalyticsConfig.java
@@ -1,5 +1,6 @@
 package org.prebid.server.settings.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Value;
@@ -18,7 +19,7 @@ public class AccountAnalyticsConfig {
                 "app", true);
     }
 
-    @JsonProperty("auction-events")
+    @JsonAlias("auction-events")
     AccountAuctionEventConfig auctionEvents;
 
     Map<String, ObjectNode> modules;
