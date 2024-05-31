a35894ebea5d6a87037833531fa73803aede1941
Danylo <dubyk.dv@gmail.com>
2024-05-31 03:40:00 +0300

    Add support for `AccountSettings`.

--- src/main/java/org/prebid/server/settings/model/AccountSettings.java
+++ src/main/java/org/prebid/server/settings/model/AccountSettings.java
@@ -1,12 +1,12 @@
 package org.prebid.server.settings.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
 public class AccountSettings {
 
-    @JsonProperty("geo-lookup")
+    @JsonAlias("geo-lookup")
     Boolean geoLookup;
-
 }
