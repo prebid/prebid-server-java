b4dbe8269713d274f5c80ac5ebbe9c086719852b
Danylo <dubyk.dv@gmail.com>
2024-05-31 03:36:02 +0300

    Add support for `AccountMetricsConfig`.

--- src/main/java/org/prebid/server/settings/model/AccountMetricsConfig.java
+++ src/main/java/org/prebid/server/settings/model/AccountMetricsConfig.java
@@ -1,14 +1,14 @@
 package org.prebid.server.settings.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.prebid.server.metric.model.AccountMetricsVerbosityLevel;
 
-@Value
-@AllArgsConstructor(staticName = "of")
+@Value(staticConstructor = "of")
 public class AccountMetricsConfig {
 
-    @JsonProperty("verbosity-level")
+    @JsonAlias("verbosity-level")
     AccountMetricsVerbosityLevel verbosityLevel;
 }
