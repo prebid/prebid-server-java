40e40728c23a065e16b04b5bcb43bc585fc9fb73
markiian <mmykush@magnite.com>
2024-06-19 16:16:02 +0300

    Add test for global stored auction response

--- src/test/groovy/org/prebid/server/functional/model/request/auction/StoredAuctionResponse.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/StoredAuctionResponse.groovy
@@ -1,9 +1,11 @@
 package org.prebid.server.functional.model.request.auction
 
 import groovy.transform.ToString
+import org.prebid.server.functional.model.response.auction.SeatBid
 
 @ToString(includeNames = true, ignoreNulls = true)
 class StoredAuctionResponse {
 
     String id
+    List<SeatBid> seatbid
 }
