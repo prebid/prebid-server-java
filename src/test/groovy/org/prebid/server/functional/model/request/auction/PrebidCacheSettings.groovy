6dcde096344fcb150e982311b6e79ca1ac6a5913
Anton Babak <76536883+AntoxaAntoxic@users.noreply.github.com>
2024-06-19 13:14:41 +0200

    Core: Always Return Bid Exp (#3213)

--- src/test/groovy/org/prebid/server/functional/model/request/auction/PrebidCacheSettings.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/PrebidCacheSettings.groovy
@@ -1,10 +1,12 @@
 package org.prebid.server.functional.model.request.auction
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import groovy.transform.ToString
 
 @ToString(includeNames = true, ignoreNulls = true)
 class PrebidCacheSettings {
 
+    @JsonProperty("ttlseconds")
     Integer ttlSeconds
     Boolean returnCreative
 }
