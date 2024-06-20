3f92136c42680818dd284f335bdc1c26ec24bc58
Alex Maltsev <and1sscsgo@gmail.com>
2024-06-20 17:41:13 +0300

    Fixed enum deserialization.

--- src/main/java/org/prebid/server/cache/proto/request/module/ModuleCacheType.java
+++ src/main/java/org/prebid/server/cache/proto/request/module/ModuleCacheType.java
@@ -1,6 +1,17 @@
 package org.prebid.server.cache.proto.request.module;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ModuleCacheType {
 
-    JSON, XML, TEXT
+    JSON("json"),
+    XML("xml"),
+    TEXT("text");
+
+    @JsonValue
+    private final String text;
+
+    ModuleCacheType(final String text) {
+        this.text = text;
+    }
 }
