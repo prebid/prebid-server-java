3ebb8316fab2a0d1fd9221b213c3cb8c871330c6
markiian <mmykush@magnite.com>
2024-06-21 12:44:35 +0300

    Update tests

--- src/test/groovy/org/prebid/server/functional/tests/privacy/GppAmpSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/privacy/GppAmpSpec.groovy
@@ -5,6 +5,7 @@ import org.prebid.server.functional.model.request.amp.AmpRequest
 import org.prebid.server.functional.model.request.amp.ConsentType
 import org.prebid.server.functional.model.request.auction.BidRequest
 import org.prebid.server.functional.model.request.auction.Regs
+import org.prebid.server.functional.model.response.amp.AmpResponse
 import org.prebid.server.functional.util.PBSUtils
 import org.prebid.server.functional.util.privacy.gpp.TcfEuV2Consent
 import org.prebid.server.functional.util.privacy.gpp.UsV1Consent
@@ -74,12 +75,16 @@ class GppAmpSpec extends PrivacyBaseSpec {
         storedRequestDao.save(storedRequest)
 
         when: "PBS processes amp request"
-        defaultPbsService.sendAmpRequest(ampRequest)
+        def ampResponse = defaultPbsService.sendAmpRequest(ampRequest)
 
         then: "Bidder request shouldn't contain regs.gpp"
         def bidderRequests = bidder.getBidderRequest(ampStoredRequest.id)
         assert !bidderRequests.regs.gpp
         assert !bidderRequests.regs.gppSid
+
+        and: "Repose should contain warning"
+        assert ampResponse.ext?.warnings[PREBID]*.code == [999]
+        assert ampResponse.ext?.warnings[PREBID]*.message[0].startsWith("Failed to parse gppSid: \'${gppSids}\'")
     }
 
     def "PBS should emit warning when consent_string is invalid"() {
