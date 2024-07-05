cb9fb71ac890ba67f0b981e0a015110e3d94ea0d
antonbabak <ababak@magnite.com>
2024-07-05 10:04:03 +0200

    Fix comments

--- src/test/java/org/prebid/server/floors/BasicPriceFloorEnforcerTest.java
+++ src/test/java/org/prebid/server/floors/BasicPriceFloorEnforcerTest.java
@@ -99,7 +99,6 @@ public class BasicPriceFloorEnforcerTest {
         assertSame(result, auctionParticipation);
     }
 
-
     @Test
     public void shouldNotEnforceIfRequestFloorsSkipped() {
         // given
