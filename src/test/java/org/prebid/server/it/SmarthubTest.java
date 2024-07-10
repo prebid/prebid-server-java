c5f29396060db0776b5dfd0d37457d35a119c0a8
Anton Babak <76536883+AntoxaAntoxic@users.noreply.github.com>
2024-07-10 15:31:24 +0200

    SmartHub: JDPMedia Alias (#3297)

--- src/test/java/org/prebid/server/it/SmarthubTest.java
+++ src/test/java/org/prebid/server/it/SmarthubTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.IOException;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
@@ -22,6 +23,9 @@ public class SmarthubTest extends IntegrationTest {
     public void openrtb2AuctionShouldRespondWithBidsFromSmarthub() throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/smarthub-exchange"))
+                .withQueryParam("host", equalTo("someUniquePartnerName"))
+                .withQueryParam("accountId", equalTo("someSeat"))
+                .withQueryParam("sourceId", equalTo("someToken"))
                 .withRequestBody(equalToJson(jsonFrom("openrtb2/smarthub/test-smarthub-bid-request.json")))
                 .willReturn(aResponse().withBody(jsonFrom("openrtb2/smarthub/test-smarthub-bid-response.json"))));
 
