955ef80e1896016724199d0026d971ddeedd707c
Danylo <dubyk.dv@gmail.com>
2024-07-22 15:23:55 +0200

    Update bidder.

--- src/main/java/org/prebid/server/bidder/freewheelssp/FreewheelSSPBidder.java
+++ src/main/java/org/prebid/server/bidder/freewheelssp/FreewheelSSPBidder.java
@@ -1,9 +1,12 @@
 package org.prebid.server.bidder.freewheelssp;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.response.Bid;
 import com.iab.openrtb.response.BidResponse;
 import com.iab.openrtb.response.SeatBid;
-import io.vertx.core.http.HttpMethod;
+import io.vertx.core.MultiMap;
 import org.apache.commons.collections4.CollectionUtils;
 import org.prebid.server.bidder.Bidder;
 import org.prebid.server.bidder.model.BidderBid;
@@ -11,12 +14,17 @@ import org.prebid.server.bidder.model.BidderCall;
 import org.prebid.server.bidder.model.BidderError;
 import org.prebid.server.bidder.model.HttpRequest;
 import org.prebid.server.bidder.model.Result;
+import org.prebid.server.exception.PreBidException;
 import org.prebid.server.json.DecodeException;
 import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.proto.openrtb.ext.ExtPrebid;
+import org.prebid.server.proto.openrtb.ext.request.freewheelssp.ExtImpFreewheelSSP;
 import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.proto.openrtb.ext.response.ExtBidPrebidVideo;
 import org.prebid.server.util.BidderUtil;
 import org.prebid.server.util.HttpUtil;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -24,9 +32,14 @@ import java.util.Objects;
 
 public class FreewheelSSPBidder implements Bidder<BidRequest> {
 
-    private static final BidType BID_TYPE = BidType.video;
+    private static final TypeReference<ExtPrebid<?, ExtImpFreewheelSSP>> FREEWHEELSSP_EXT_TYPE_REFERENCE =
+            new TypeReference<>() {
+            };
+
     private static final String COMPONENT_ID_HEADER_NAME = "Componentid";
     private static final String COMPONENT_ID_HEADER_VALUE = "prebid-java";
+    private static final BidType BID_TYPE = BidType.video;
+
     private final String endpointUrl;
     private final JacksonMapper mapper;
 
@@ -36,16 +49,49 @@ public class FreewheelSSPBidder implements Bidder<BidRequest> {
     }
 
     @Override
-    public final Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest bidRequest) {
+    public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest bidRequest) {
+        final List<Imp> modifiedImps = new ArrayList<>();
+
+        try {
+            for (final Imp imp : bidRequest.getImp()) {
+                final ExtImpFreewheelSSP extImpFreewheelSSP = parseExtImp(imp);
+                modifiedImps.add(modifyImp(imp, extImpFreewheelSSP));
+            }
+        } catch (PreBidException e) {
+            return Result.withError(BidderError.badInput(e.getMessage()));
+        }
+
+
         return Result.withValue(
-                HttpRequest.<BidRequest>builder()
-                        .method(HttpMethod.POST)
-                        .uri(endpointUrl)
-                        .headers(HttpUtil.headers().add(COMPONENT_ID_HEADER_NAME, COMPONENT_ID_HEADER_VALUE))
-                        .body(mapper.encodeToBytes(bidRequest))
-                        .impIds(BidderUtil.impIds(bidRequest))
-                        .payload(bidRequest)
-                        .build());
+                BidderUtil.defaultRequest(
+                        modifyBidRequest(bidRequest, modifiedImps),
+                        headers(),
+                        endpointUrl,
+                        mapper));
+    }
+
+    private ExtImpFreewheelSSP parseExtImp(Imp imp) {
+        try {
+            return mapper.mapper().convertValue(imp.getExt(), FREEWHEELSSP_EXT_TYPE_REFERENCE).getBidder();
+        } catch (IllegalArgumentException e) {
+            throw new PreBidException(
+                    "Invalid imp.ext for impression id %s. Error Infomation: %s"
+                            .formatted(imp.getId(), e.getMessage()));
+        }
+    }
+
+    private Imp modifyImp(Imp imp, ExtImpFreewheelSSP extImpFreewheelSSP) {
+        return imp.toBuilder()
+                .ext(mapper.mapper().valueToTree(extImpFreewheelSSP))
+                .build();
+    }
+
+    private static BidRequest modifyBidRequest(BidRequest bidRequest, List<Imp> imps) {
+        return bidRequest.toBuilder().imp(imps).build();
+    }
+
+    private static MultiMap headers() {
+        return HttpUtil.headers().add(COMPONENT_ID_HEADER_NAME, COMPONENT_ID_HEADER_VALUE);
     }
 
     @Override
@@ -62,16 +108,31 @@ public class FreewheelSSPBidder implements Bidder<BidRequest> {
         if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
             return Collections.emptyList();
         }
-        return bidsFromResponse(bidResponse);
-    }
 
-    private static List<BidderBid> bidsFromResponse(BidResponse bidResponse) {
         return bidResponse.getSeatbid().stream()
                 .filter(Objects::nonNull)
                 .map(SeatBid::getBid)
                 .filter(Objects::nonNull)
                 .flatMap(Collection::stream)
-                .map(bid -> BidderBid.of(bid, BID_TYPE, bidResponse.getCur()))
+                .map(bid -> BidderBid.builder()
+                        .bid(bid)
+                        .type(BID_TYPE)
+                        .bidCurrency(bidResponse.getCur())
+                        .videoInfo(videoInfo(bid))
+                        .build())
                 .toList();
+    }
+
+    private static ExtBidPrebidVideo videoInfo(Bid bid) {
+        final List<String> cat = bid.getCat();
+        final Integer duration = bid.getDur();
+
+        final boolean catNotEmpty = CollectionUtils.isNotEmpty(cat);
+        final boolean durationValid = duration != null && duration > 0;
+        return catNotEmpty || durationValid
+                ? ExtBidPrebidVideo.of(
+                durationValid ? duration : null,
+                catNotEmpty ? cat.getFirst() : null)
+                : null;
     }
 }
