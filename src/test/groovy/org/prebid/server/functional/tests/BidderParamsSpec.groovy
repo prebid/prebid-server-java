8303ebc754fde502cc60df208e0c79c002c26944
Markiyan Mykush <95693607+marki1an@users.noreply.github.com>
2024-07-30 16:17:39 +0300

    Test: `imp.ext` (#3337)
    * Add functional tests for imp.ext
    
    * Update after review
    
    * Remove unused import

--- src/test/groovy/org/prebid/server/functional/tests/BidderParamsSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/BidderParamsSpec.groovy
@@ -10,6 +10,9 @@ import org.prebid.server.functional.model.request.auction.BidRequest
 import org.prebid.server.functional.model.request.auction.Device
 import org.prebid.server.functional.model.request.auction.Geo
 import org.prebid.server.functional.model.request.auction.Imp
+import org.prebid.server.functional.model.request.auction.ImpExt
+import org.prebid.server.functional.model.request.auction.ImpExtContext
+import org.prebid.server.functional.model.request.auction.ImpExtContextData
 import org.prebid.server.functional.model.request.auction.Native
 import org.prebid.server.functional.model.request.auction.PrebidStoredRequest
 import org.prebid.server.functional.model.request.auction.RegsExt
@@ -560,7 +563,7 @@ class BidderParamsSpec extends BaseSpec {
 
         then: "Bid response should contain proper warning"
         assert response.ext?.warnings[ErrorType.GENERIC]?.message ==
-                ["Imp ${bidRequest.imp[1].id} does not have a supported media type and has been removed from the request for this bidder." ]
+                ["Imp ${bidRequest.imp[1].id} does not have a supported media type and has been removed from the request for this bidder."]
 
         and: "Bid response should contain seatbid"
         assert response.seatbid
@@ -720,5 +723,82 @@ class BidderParamsSpec extends BaseSpec {
         null          | 1
         1             | 1
         0             | 0
+    }
+
+    def "PBS shouldn't emit warning and proceed auction when imp.ext.foo and imp.ext.prebid.bidder.generic in the request"() {
+        given: "Default bid request"
+        def randomString = PBSUtils.randomString
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            imp[0].ext.foo = randomString
+            imp[0].ext.prebid.bidder.generic = new Generic()
+        }
+
+        when: "PBS processes auction request"
+        def response = defaultPbsService.sendAuctionRequest(bidRequest)
+
+        then: "Bidder request should contain imp.ext.foo"
+        def bidderRequest = bidder.getBidderRequest(bidRequest.id)
+        assert bidderRequest.imp[0].ext.foo == randomString
+
+        and: "Response shouldn't contain warning"
+        assert !response?.ext?.warnings
+    }
+
+    def "PBS should emit warning and proceed auction when imp.ext.foo and imp.ext.generic in the request"() {
+        given: "Default bid request"
+        def randomString = PBSUtils.randomString
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            imp[0].ext.generic = new Generic()
+            imp[0].ext.foo = randomString
+            imp[0].ext.prebid.bidder.generic = null
+        }
+
+        when: "PBS processes auction request"
+        def response = defaultPbsService.sendAuctionRequest(bidRequest)
+
+        then: "Bidder request should contain imp.ext.foo"
+        def bidderRequest = bidder.getBidderRequest(bidRequest.id)
+        assert bidderRequest.imp[0].ext.foo == randomString
+
+        and: "PBS should emit an warning"
+        assert response?.ext?.warnings[PREBID]*.code == [999]
+        assert response?.ext?.warnings[PREBID]*.message ==
+                ["WARNING: request.imp[0].ext.foo unknown bidder."]
+    }
+
+    def "PBS shouldn't emit warning and proceed auction when imp.ext.foo and imp.ext.generic in the request"() {
+        given: "Default bid request with populated imp.ext"
+        def impExt = ImpExt.getDefaultImpExt().tap {
+            prebid.bidder.generic = null
+            generic = new Generic()
+            ae = PBSUtils.randomNumber
+            all = PBSUtils.randomNumber
+            context = new ImpExtContext(data: new ImpExtContextData())
+            data = new ImpExtContextData(pbAdSlot: PBSUtils.randomString)
+            general = PBSUtils.randomString
+            gpid = PBSUtils.randomString
+            skadn = PBSUtils.randomString
+            tid = PBSUtils.randomString
+        }
+        def bidRequest = BidRequest.defaultBidRequest.tap {
+            imp[0].ext = impExt
+        }
+
+        when: "PBS processes auction request"
+        defaultPbsService.sendAuctionRequest(bidRequest)
+
+        then: "Bidder request should contain same field as requested"
+        def bidderRequest = bidder.getBidderRequest(bidRequest.id)
+        verifyAll(bidderRequest.imp[0].ext){
+            bidder == impExt.generic
+            ae == impExt.ae
+            all == impExt.all
+            context == impExt.context
+            data == impExt.data
+            general == impExt.general
+            gpid == impExt.gpid
+            skadn == impExt.skadn
+            tid == impExt.tid
+        }
     }
 }
