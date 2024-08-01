51ba2d8aa8de9ecf179cd9bd0045223b2fe7ea64
EvgeniiMunin <evgenii.m@solta.io>
2024-08-01 18:36:48 +0200

    fetch gcs + model cache + auth partners

--- extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/AuthorizedPartner.java
+++ extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/AuthorizedPartner.java
@@ -10,42 +10,26 @@ public enum AuthorizedPartner {
             .pbuid("PBUID_FROM_GREENBIDS")
             .targetTpr(0.95)
             .explorationRate(0.001)
-            //.onnxModelRunner(PredefinedModelRunners.testModelRunner)
-            .thresholdsJsonPath("extra/modules/greenbids-real-time-data/src/main/resources/thresholds_pbuid=PBUID_FROM_GREENBIDS.json")
+            .thresholdsJsonPath("thresholds_pbuid=PBUID_FROM_GREENBIDS.json")
             .build(),
-            new ModelCache("extra/modules/greenbids-real-time-data/src/main/resources/models_pbuid=PBUID_FROM_GREENBIDS.onnx")),
+            new ModelCache("models_pbuid=PBUID_FROM_GREENBIDS.onnx")),
 
     LELP(Partner.builder()
             .pbuid("lelp-pbuid")
             .targetTpr(0.99)
             .explorationRate(0.2)
-            //.onnxModelRunner(PredefinedModelRunners.lelpModelRunner)
-            .thresholdsJsonPath("extra/modules/greenbids-real-time-data/src/main/resources/thresholds_pbuid=lelp-pbuid.json")
+            .thresholdsJsonPath("thresholds_pbuid=lelp-pbuid.json")
             .build(),
-            new ModelCache("extra/modules/greenbids-real-time-data/src/main/resources/models_pbuid=lelp-pbuid.onnx"));
+            new ModelCache("models_pbuid=lelp-pbuid.onnx"));
 
     @Getter
     private final Partner partner;
-
-    private final ModelCache modelCache;
 
     AuthorizedPartner(Partner partner, ModelCache modelCache) {
         this.partner = partner.toBuilder()
                 .onnxModelRunner(modelCache.getModelRunner())
                 .build();
-        this.modelCache = modelCache;
-
-        //partner = partner.toBuilder()
-        //        .onnxModelRunner(modelCache.getModelRunner())
-        //        .build();
-
-        //try {
-        //    partner = partner.toBuilder()
-        //            .onnxModelRunner(modelCache.getModelRunner())
-        //            .build();
-        //} catch (OrtException e) {
-        //    throw new RuntimeException("Failed to initialize ONNX Model Runner", e);
-        //}
+        System.out.println("AuthorizedPartner partner: " + this.partner);
     }
 
     public static Partner getPartnerByPbuid(String pbuid) {
