d2d5fd2f40cfa267dcfcd16b6bd72932196b5bd9
osulzhenko <125548596+osulzhenko@users.noreply.github.com>
2024-08-09 15:56:28 +0300

    Tests: Add functional tests for additional SeatNonBid codes (#3340)

--- src/test/groovy/org/prebid/server/functional/model/config/Ortb2BlockingConfig.groovy
+++ src/test/groovy/org/prebid/server/functional/model/config/Ortb2BlockingConfig.groovy
@@ -3,12 +3,9 @@ package org.prebid.server.functional.model.config
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
-import org.prebid.server.functional.model.request.auction.RichmediaFilter
 
 @ToString(includeNames = true, ignoreNulls = true)
-@JsonNaming(PropertyNamingStrategies.KebabCaseStrategy)
-class PbsModulesConfig {
+class Ortb2BlockingConfig {
 
-    RichmediaFilter pbRichmediaFilter
-    Ortb2BlockingConfig ortb2Blocking
+    Ortb2BlockingAttributes attributes
 }
