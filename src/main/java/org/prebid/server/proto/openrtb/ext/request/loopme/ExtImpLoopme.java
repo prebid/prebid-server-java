7a3724f824d88f2fc1bcd6d9942ee5bd609f8e06
Oleksandr Balashov <alexander@loopme.com>
2024-08-16 15:25:59 +0300

    Loopme: Add Bidder

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/loopme/ExtImpLoopme.java
+++ src/main/java/org/prebid/server/proto/openrtb/ext/request/loopme/ExtImpLoopme.java
@@ -1,16 +1,16 @@
 package org.prebid.server.proto.openrtb.ext.request.loopme;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 
 /**
- * Defines the contract for bidrequest.imp[i].ext.loopme
+ * Defines the contract for bidRequest.imp[i].ext.loopme
  */
 @AllArgsConstructor(staticName = "of")
 @Value
 public class ExtImpLoopme {
 
-    @JsonProperty("accountId")
-    String accountId;
+    String publisherId;
+    String bundleId;
+    String placementId;
 }
