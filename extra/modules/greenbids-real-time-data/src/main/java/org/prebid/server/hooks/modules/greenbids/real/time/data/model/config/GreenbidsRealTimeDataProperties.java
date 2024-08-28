a63a2863a5c84728c58f96c8ddc23735054f6fff
EvgeniiMunin <evgenii.m@solta.io>
2024-08-28 18:46:50 +0200

    fix review1: config + cache atomic bool flag

--- extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/model/config/GreenbidsRealTimeDataProperties.java
+++ extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/model/config/GreenbidsRealTimeDataProperties.java
@@ -1,9 +1,6 @@
 package org.prebid.server.hooks.modules.greenbids.real.time.data.model.config;
 
-import com.github.benmanes.caffeine.cache.Cache;
 import lombok.Data;
-import org.prebid.server.hooks.modules.greenbids.real.time.data.core.ThrottlingThresholds;
-import org.prebid.server.hooks.modules.greenbids.real.time.data.model.predictor.OnnxModelRunner;
 import org.prebid.server.hooks.modules.greenbids.real.time.data.v1.GreenbidsRealTimeDataModule;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -14,10 +11,6 @@ public class GreenbidsRealTimeDataProperties {
     String googleCloudGreenbidsProject;
 
     String geoLiteCountryPath;
-
-    Cache<String, OnnxModelRunner> modelCacheWithExpiration;
-
-    Cache<String, ThrottlingThresholds> thresholdsCacheWithExpiration;
 
     String gcsBucketName;
 
