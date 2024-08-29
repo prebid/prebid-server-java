89ced518febbde98937096a14e86116bf203d528
EvgeniiMunin <evgenii.m@solta.io>
2024-08-29 14:13:30 +0200

    fix review1: partner/ throttling thrs constructors

--- extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/ThrottlingThresholds.java
+++ extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/ThrottlingThresholds.java
@@ -1,14 +1,11 @@
 package org.prebid.server.hooks.modules.greenbids.real.time.data.core;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Value;
 
 import java.util.List;
 
-@Builder(toBuilder = true)
-@Value
+@Value(staticConstructor = "of")
 public class ThrottlingThresholds {
 
     @JsonProperty("thresholds")
@@ -22,16 +19,4 @@ public class ThrottlingThresholds {
 
     @JsonProperty("version")
     String version;
-
-    @JsonCreator
-    public ThrottlingThresholds(
-            @JsonProperty("thresholds") List<Double> thresholds,
-            @JsonProperty("tpr") List<Double> tpr,
-            @JsonProperty("fpr") List<Double> fpr,
-            @JsonProperty("version") String version) {
-        this.thresholds = thresholds;
-        this.tpr = tpr;
-        this.fpr = fpr;
-        this.version = version;
-    }
 }
