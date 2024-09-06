5008dfdf0be3f9281e3e3b85ad94060113816744
antonbabak <ababak@magnite.com>
2024-09-06 14:31:08 +0200

    Fix hook name

--- extra/modules/response-correction/src/main/java/org/prebid/server/hooks/modules/response/correction/v1/ResponseCorrectionAllProcessedBidResponsesHook.java
+++ extra/modules/response-correction/src/main/java/org/prebid/server/hooks/modules/response/correction/v1/ResponseCorrectionAllProcessedBidResponsesHook.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 
 public class ResponseCorrectionAllProcessedBidResponsesHook implements AllProcessedBidResponsesHook {
 
-    private static final String CODE = "pb-response-correction-all-processed-bid-responses";
+    private static final String CODE = "pb-response-correction-all-processed-bid-responses-hook";
 
     private final CorrectionsProvider correctionsProvider;
     private final ObjectMapper mapper;
