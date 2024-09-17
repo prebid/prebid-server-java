1d5400b3ad75fc375fcfdc572558ae7ba6b3495b
Anton Babak <76536883+AntoxaAntoxic@users.noreply.github.com>
2024-09-17 18:31:44 +0200

    Modules: Response Correction Module (#3409)

--- src/test/groovy/org/prebid/server/functional/model/config/Ortb2BlockingConfig.groovy
+++ src/test/groovy/org/prebid/server/functional/model/config/Ortb2BlockingConfig.groovy
@@ -1,7 +1,5 @@
 package org.prebid.server.functional.model.config
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
 
 @ToString(includeNames = true, ignoreNulls = true)
