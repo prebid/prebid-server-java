20348cb336422859452c20980d8c951416efbf12
EvgeniiMunin <munin.evgenii@gmail.com>
2024-09-17 02:42:15 -0700

    fix review5: refactos

--- extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/ThrottlingThresholds.java
+++ extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/ThrottlingThresholds.java
@@ -1,7 +1,6 @@
 package org.prebid.server.hooks.modules.greenbids.real.time.data.core;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import java.util.List;
@@ -10,9 +9,7 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ThrottlingThresholds {
 
-    @JsonProperty("thresholds")
     List<Double> thresholds;
 
-    @JsonProperty("tpr")
     List<Double> tpr;
 }
