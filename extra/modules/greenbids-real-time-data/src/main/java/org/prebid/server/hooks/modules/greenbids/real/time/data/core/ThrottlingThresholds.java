c941a5be64afd2c5c218827c8f37ddf48ac15abf
EvgeniiMunin <munin.evgenii@gmail.com>
2024-09-20 09:05:37 -0700

    fix review6: ObjectMapperProvider

--- extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/ThrottlingThresholds.java
+++ extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/core/ThrottlingThresholds.java
@@ -1,12 +1,10 @@
 package org.prebid.server.hooks.modules.greenbids.real.time.data.core;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Value;
 
 import java.util.List;
 
 @Value(staticConstructor = "of")
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class ThrottlingThresholds {
 
     List<Double> thresholds;
