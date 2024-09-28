234a8a0f1c770cfa438d8d4244b253593fe759be
serhiinahornyi <snahornyi@magnite.com>
2024-09-28 19:07:40 +0200

    Refactoring: Extract bidAdjustments code from exchangeService

--- src/main/java/org/prebid/server/util/ListUtil.java
+++ src/main/java/org/prebid/server/util/ListUtil.java
@@ -1,5 +1,6 @@
 package org.prebid.server.util;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.prebid.server.util.algorithms.ListsUnionView;
 
 import java.util.List;
@@ -11,5 +12,9 @@ public class ListUtil {
 
     public static <T> List<T> union(List<? extends T> first, List<? extends T> second) {
         return new ListsUnionView<>(first, second);
+    }
+
+    public static <T> List<T> nullIfEmpty(List<T> value) {
+        return CollectionUtils.isEmpty(value) ? null : value;
     }
 }
