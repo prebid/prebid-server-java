bae8fd6e1f9bf55707de9b94cb8ce3655ea0d6b0
Oronno Mamun <oronno.mamun@indexexchange.com>
2024-11-25 10:07:17 -0500

    IX: used same integration test for privacy sandbox case

--- src/test/java/org/prebid/server/it/IxTest.java
+++ src/test/java/org/prebid/server/it/IxTest.java
@@ -33,20 +33,4 @@ public class IxTest extends IntegrationTest {
         // then
         assertJsonEquals("openrtb2/ix/test-auction-ix-response.json", response, singletonList("ix"));
     }
-
-    @Test
-    public void openrtb2AuctionShouldRespondWithBidsFromIxForPrivacySandboxRequest() throws IOException, JSONException {
-        // given
-        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/ix-exchange"))
-                .withRequestBody(equalToJson(jsonFrom("openrtb2/ix/privacy-sandbox-test-ix-bid-request.json",
-                        prebidVersionProvider)))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/ix/privacy-sandbox-test-ix-bid-response.json"))));
-
-        // when
-        final Response response = responseFor("openrtb2/ix/privacy-sandbox-test-auction-ix-request.json",
-                Endpoint.openrtb2_auction);
-
-        // then
-        assertJsonEquals("openrtb2/ix/privacy-sandbox-test-auction-ix-response.json", response, singletonList("ix"));
-    }
 }
