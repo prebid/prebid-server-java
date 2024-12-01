44550ae70bdbbb7f33e7d9fccebeb1f111cf91c8
osulzhenko <osulzhenko@magnite.com>
2024-12-01 22:54:50 +0200

    Functional tests for module execution host config check

--- src/test/groovy/org/prebid/server/functional/model/config/AdminConfig.groovy
+++ src/test/groovy/org/prebid/server/functional/model/config/AdminConfig.groovy
@@ -1,17 +1,13 @@
 package org.prebid.server.functional.model.config
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
+import org.prebid.server.functional.model.ModuleName
 
 @ToString(includeNames = true, ignoreNulls = true)
 @JsonNaming(PropertyNamingStrategies.KebabCaseStrategy)
-class AccountHooksConfiguration {
+class AdminConfig {
 
-    ExecutionPlan executionPlan
-    @JsonProperty("execution_plan")
-    ExecutionPlan executionPlanSnakeCase
-    PbsModulesConfig modules
-    AdminConfig admin
+    Map<ModuleName, Boolean> moduleExecution
 }
