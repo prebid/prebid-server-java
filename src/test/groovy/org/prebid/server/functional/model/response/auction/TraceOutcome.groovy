1b6365fccbbf51107bab2e5b4a547db1af4db6dc
osulzhenko <125548596+osulzhenko@users.noreply.github.com>
2024-12-04 12:51:29 +0200

    Functional tests for module execution host config check (#3589)
    * Functional tests for module execution host config check

--- src/test/groovy/org/prebid/server/functional/model/response/auction/TraceOutcome.groovy
+++ src/test/groovy/org/prebid/server/functional/model/response/auction/TraceOutcome.groovy
@@ -3,13 +3,12 @@ package org.prebid.server.functional.model.response.auction
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
-import org.prebid.server.functional.model.config.Stage
 
 @ToString(includeNames = true, ignoreNulls = true)
 @JsonNaming(PropertyNamingStrategies.LowerCaseStrategy)
 class TraceOutcome {
 
-    Stage entity
+    String entity
     Long executionTimeMillis
     List<TraceGroup> groups
 }
