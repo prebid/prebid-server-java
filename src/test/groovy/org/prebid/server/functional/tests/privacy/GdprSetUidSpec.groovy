181bdde9acf7571fea6c0e70300cbd0140e5149d
markiian <mmykush@magnite.com>
2025-01-08 10:30:20 +0200

    Update after review

--- src/test/groovy/org/prebid/server/functional/tests/privacy/GdprSetUidSpec.groovy
+++ src/test/groovy/org/prebid/server/functional/tests/privacy/GdprSetUidSpec.groovy
@@ -9,11 +9,13 @@ import org.prebid.server.functional.model.config.PurposeConfig
 import org.prebid.server.functional.model.db.Account
 import org.prebid.server.functional.model.request.setuid.SetuidRequest
 import org.prebid.server.functional.model.response.cookiesync.UserSyncInfo
+import org.prebid.server.functional.model.response.setuid.SetuidResponse
 import org.prebid.server.functional.service.PrebidServerException
 import org.prebid.server.functional.service.PrebidServerService
 import org.prebid.server.functional.util.PBSUtils
 import org.prebid.server.functional.util.privacy.TcfConsent
 import org.prebid.server.util.ResourceUtil
+import spock.lang.IgnoreRest
 
 import static org.prebid.server.functional.model.AccountStatus.ACTIVE
 import static org.prebid.server.functional.model.bidder.BidderName.GENERIC
@@ -153,6 +155,49 @@ class GdprSetUidSpec extends PrivacyBaseSpec {
         and: "Metric should be increased usersync.FAMILY.tcf.blocked"
         def metric = prebidServerService.sendCollectedMetricsRequest()
         assert metric["usersync.${GENERIC.value}.tcf.blocked"] == 1
+
+        cleanup: "Stop and remove pbs container"
+        pbsServiceFactory.removeContainer(pbsConfig)
+    }
+
+    def "PBS setuid shouldn't failed with tcf when purpose access device not enforced for host and host-vendor-id empty"() {
+        given: "PBS config"
+        def pbsConfig = VENDOR_GENERIC_PBS_CONFIG + ["gdpr.purposes.p1.enforce-purpose": NO.value,
+                                                     "gdpr.host-vendor-id"             : ""]
+        def prebidServerService = pbsServiceFactory.getService(pbsConfig)
+
+        and: "Default setuid request with account"
+        def setuidRequest = SetuidRequest.defaultSetuidRequest.tap {
+            it.account = PBSUtils.randomNumber.toString()
+            it.uid = UUID.randomUUID().toString()
+            it.bidder = GENERIC
+            it.gdpr = "1"
+            it.gdprConsent = new TcfConsent.Builder()
+                    .setPurposesLITransparency(DEVICE_ACCESS)
+                    .setVendorLegitimateInterest([GENERIC_VENDOR_ID])
+                    .build()
+        }
+
+        and: "Default uids cookie with rubicon bidder"
+        def uidsCookie = UidsCookie.defaultUidsCookie.tap {
+            it.tempUIDs = [(GENERIC): defaultUidWithExpiry]
+        }
+
+        and: "Save account config with purpose into DB"
+        def accountConfig = new AccountConfig(
+                auction: new AccountAuctionConfig(debugAllow: true),
+                privacy: new AccountPrivacyConfig(gdpr: new AccountGdprConfig(purposes: [(P1): new PurposeConfig(enforcePurpose: NO)], enabled: true)))
+        def account = new Account(status: ACTIVE, uuid: setuidRequest.account, config: accountConfig)
+        accountDao.save(account)
+
+        when: "PBS processes setuid request"
+        def response = prebidServerService.sendSetUidRequest(setuidRequest, uidsCookie)
+
+        then: "Response should contain tempUids cookie and headers"
+        assert response.headers.size() == 7
+        assert response.uidsCookie.tempUIDs[GENERIC].uid == setuidRequest.uid
+        assert response.responseBody ==
+                ResourceUtil.readByteArrayFromClassPath("org/prebid/server/functional/tracking-pixel.png")
 
         cleanup: "Stop and remove pbs container"
         pbsServiceFactory.removeContainer(pbsConfig)
