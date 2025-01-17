e9417e3eb5caae984d9ef9c34e721e5a02ca787f
EvgeniiMunin <35193823+EvgeniiMunin@users.noreply.github.com>
2025-01-17 05:14:56 -0800

    Greenbids fix geolookup: fetch from official MaxMind URL + mock dbReader UT (#3626)

--- extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/config/DatabaseReaderFactory.java
+++ extra/modules/greenbids-real-time-data/src/main/java/org/prebid/server/hooks/modules/greenbids/real/time/data/config/DatabaseReaderFactory.java
@@ -1,52 +1,128 @@
 package org.prebid.server.hooks.modules.greenbids.real.time.data.config;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpClientResponse;
+import com.maxmind.db.Reader;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import com.maxmind.geoip2.DatabaseReader;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.RequestOptions;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.prebid.server.exception.PreBidException;
+import org.prebid.server.log.Logger;
+import org.prebid.server.log.LoggerFactory;
 import org.prebid.server.vertx.Initializable;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.zip.GZIPInputStream;
 
 public class DatabaseReaderFactory implements Initializable {
 
-    private final String geoLiteCountryUrl;
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseReaderFactory.class);
+
+    private final GreenbidsRealTimeDataProperties properties;
 
     private final Vertx vertx;
 
     private final AtomicReference<DatabaseReader> databaseReaderRef = new AtomicReference<>();
 
-    public DatabaseReaderFactory(String geoLitCountryUrl, Vertx vertx) {
-        this.geoLiteCountryUrl = geoLitCountryUrl;
+    private final FileSystem fileSystem;
+
+    public DatabaseReaderFactory(GreenbidsRealTimeDataProperties properties, Vertx vertx) {
+        this.properties = properties;
         this.vertx = vertx;
+        this.fileSystem = vertx.fileSystem();
     }
 
     @Override
     public void initialize(Promise<Void> initializePromise) {
+        downloadAndExtract()
+                .onSuccess(databaseReaderRef::set)
+                .<Void>mapEmpty()
+                .onComplete(initializePromise);
+    }
 
-        vertx.executeBlocking(() -> {
-            try {
-                final URL url = new URL(geoLiteCountryUrl);
-                final Path databasePath = Files.createTempFile("GeoLite2-Country", ".mmdb");
+    private Future<DatabaseReader> downloadAndExtract() {
+        final String downloadUrl = properties.getGeoLiteCountryPath();
+        final String tmpPath = properties.getTmpPath();
+        return downloadFile(downloadUrl, tmpPath)
+                .compose(ignored -> vertx.executeBlocking(() -> extractMMDB(tmpPath)))
+                .onComplete(ar -> removeFile(tmpPath));
+    }
 
-                try (InputStream inputStream = url.openStream();
-                     FileOutputStream outputStream = new FileOutputStream(databasePath.toFile())) {
-                    inputStream.transferTo(outputStream);
+    private Future<Void> downloadFile(String downloadUrl, String tmpPath) {
+        return fileSystem.open(tmpPath, new OpenOptions())
+                .compose(tmpFile -> sendHttpRequest(downloadUrl)
+                        .onFailure(ignore -> tmpFile.close())
+                        .compose(response -> response.pipeTo(tmpFile)));
+    }
+
+    private Future<HttpClientResponse> sendHttpRequest(String url) {
+        final RequestOptions options = new RequestOptions()
+                .setFollowRedirects(true)
+                .setMethod(HttpMethod.GET)
+                .setTimeout(properties.getTimeoutMs())
+                .setAbsoluteURI(url);
+
+        final HttpClientOptions httpClientOptions = new HttpClientOptions()
+                .setConnectTimeout(properties.getTimeoutMs().intValue())
+                .setMaxRedirects(properties.getMaxRedirects());
+
+        return vertx.createHttpClient(httpClientOptions).request(options)
+                .compose(HttpClientRequest::send)
+                .map(this::validateResponse);
+    }
+
+    private HttpClientResponse validateResponse(HttpClientResponse response) {
+        final int statusCode = response.statusCode();
+        if (statusCode != HttpResponseStatus.OK.code()) {
+            throw new PreBidException("Got unexpected response from server with status code %s and message %s"
+                    .formatted(statusCode, response.statusMessage()));
+        }
+        return response;
+    }
+
+    private DatabaseReader extractMMDB(String tarGzPath) {
+        try (GZIPInputStream gis = new GZIPInputStream(Files.newInputStream(Path.of(tarGzPath)));
+                TarArchiveInputStream tarInput = new TarArchiveInputStream(gis)) {
+
+            TarArchiveEntry currentEntry;
+            boolean hasDatabaseFile = false;
+            while ((currentEntry = tarInput.getNextTarEntry()) != null) {
+                if (currentEntry.getName().contains("GeoLite2-Country.mmdb")) {
+                    hasDatabaseFile = true;
+                    break;
                 }
-
-                databaseReaderRef.set(new DatabaseReader.Builder(databasePath.toFile()).build());
-            } catch (IOException e) {
-                throw new PreBidException("Failed to initialize DatabaseReader from URL", e);
             }
-            return null;
-        }).<Void>mapEmpty()
-        .onComplete(initializePromise);
+
+            if (!hasDatabaseFile) {
+                throw new PreBidException("GeoLite2-Country.mmdb not found in the archive");
+            }
+
+            return new DatabaseReader.Builder(tarInput)
+                    .fileMode(Reader.FileMode.MEMORY).build();
+        } catch (IOException e) {
+            throw new PreBidException("Failed to extract MMDB file", e);
+        }
+    }
+
+    private void removeFile(String filePath) {
+        fileSystem.exists(filePath).onSuccess(exists -> {
+            if (exists) {
+               fileSystem.delete(filePath)
+                        .onFailure(err -> logger.error("Failed to remove file {}", filePath, err));
+            }
+        });
     }
 
     public DatabaseReader getDatabaseReader() {
