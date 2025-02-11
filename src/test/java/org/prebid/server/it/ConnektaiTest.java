559082ab3a886da5a5314031244c93170744492f
pkaczmarek <pkaczmarek@prebid.org>
2025-02-11 12:56:59 +0100

    fix comments

---
+++ src/test/java/org/prebid/server/it/ConnektaiTest.java
@@ -0,0 +1,35 @@
+package org.prebid.server.it;
+
+import io.restassured.response.Response;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.prebid.server.model.Endpoint;
+
+import java.io.IOException;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static java.util.Collections.singletonList;
+
+public class ConnektaiTest extends IntegrationTest {
+
+    @Test
+    public void openrtb2AuctionShouldRespondWithBidsFromConnektai() throws IOException, JSONException {
+        // given
+        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/xeworks-exchange"))
+                .withRequestBody(equalToJson(
+                        jsonFrom("openrtb2/connektai/test-connektai-bid-request.json")))
+                .willReturn(aResponse().withBody(
+                        jsonFrom("openrtb2/connektai/test-connektai-bid-response.json"))));
+
+        // when
+        final Response response = responseFor("openrtb2/connektai/test-auction-connektai-request.json",
+                Endpoint.openrtb2_auction);
+
+        // then
+        assertJsonEquals("openrtb2/connektai/test-auction-connektai-response.json", response,
+                singletonList("connektai"));
+    }
+}
