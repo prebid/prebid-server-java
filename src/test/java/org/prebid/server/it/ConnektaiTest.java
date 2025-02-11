569fbbcf4815f264ebc1a4fc57cfaaf2bd260d7b
pkaczmarek <pkaczmarek@prebid.org>
2025-02-11 13:35:47 +0100

    fix yaml

--- src/test/java/org/prebid/server/it/ConnektaiTest.java
+++ src/test/java/org/prebid/server/it/ConnektaiTest.java
@@ -18,7 +18,7 @@ public class ConnektaiTest extends IntegrationTest {
     @Test
     public void openrtb2AuctionShouldRespondWithBidsFromConnektai() throws IOException, JSONException {
         // given
-        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/xeworks-exchange"))
+        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/connektai-exchange"))
                 .withRequestBody(equalToJson(
                         jsonFrom("openrtb2/connektai/test-connektai-bid-request.json")))
                 .willReturn(aResponse().withBody(
