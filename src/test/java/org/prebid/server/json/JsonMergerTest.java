a78d5d09d551524bdf3dbabcb8cbcdc0fb78fa2f
andrea <castello.a@gmail.com>
2025-04-18 12:22:00 +0200

    Review of #3922: reverted code in JsonMergerTest. No need for it.

--- src/test/java/org/prebid/server/json/JsonMergerTest.java
+++ src/test/java/org/prebid/server/json/JsonMergerTest.java
@@ -2,7 +2,6 @@ package org.prebid.server.json;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.iab.openrtb.request.App;
-import com.iab.openrtb.request.Device;
 import com.iab.openrtb.request.Dooh;
 import com.iab.openrtb.request.Publisher;
 import com.iab.openrtb.request.Site;
@@ -31,24 +30,21 @@ public class JsonMergerTest extends VertxTest {
         final Publisher publisherWithId = Publisher.builder().id("testId").build();
         final ObjectNode appWithPublisherId = mapper.valueToTree(App.builder().publisher(publisherWithId).build());
         final ObjectNode doohWithVenueType = mapper.valueToTree(Dooh.builder().venuetype(List.of("venuetype")).build());
-        final ObjectNode deviceWithMakeAndModel = mapper.valueToTree(Device.builder()
-                .make("TestMake").model("TestModel").build());
         final ExtBidderConfigOrtb firstBidderConfigFpd = ExtBidderConfigOrtb.of(
                 siteWithPage,
                 appWithPublisherId,
                 doohWithVenueType,
                 null,
-                deviceWithMakeAndModel);
+                null);
 
         final ObjectNode siteWithDomain = mapper.valueToTree(Site.builder().domain("testDomain").build());
         final Publisher publisherWithIdAndDomain = Publisher.builder().id("shouldNotBe").domain("domain").build();
         final ObjectNode appWithUpdatedPublisher = mapper.valueToTree(App.builder()
                 .publisher(publisherWithIdAndDomain).build());
         final ObjectNode doohWithVenueTypeTax = mapper.valueToTree(Dooh.builder().venuetypetax(3).build());
-        final ObjectNode deviceWithDeviceType = mapper.valueToTree(Device.builder().devicetype(6).build());
         final ExtBidderConfigOrtb secondBidderConfigFpd =
                 ExtBidderConfigOrtb.of(siteWithDomain, appWithUpdatedPublisher, doohWithVenueTypeTax, null,
-                        deviceWithDeviceType);
+                        null);
 
         // when
         final ExtBidderConfigOrtb result = target.merge(
@@ -62,10 +58,8 @@ public class JsonMergerTest extends VertxTest {
         final ObjectNode mergedApp = mapper.valueToTree(App.builder().publisher(mergedPublisher).build());
         final ObjectNode mergedDooh = mapper.valueToTree(
                 Dooh.builder().venuetype(List.of("venuetype")).venuetypetax(3).build());
-        final ObjectNode mergedDevice = mapper.valueToTree(Device.builder().make("TestMake").model("TestModel")
-                .devicetype(6).build());
         final ExtBidderConfigOrtb mergedConfigFpd = ExtBidderConfigOrtb.of(mergedSite, mergedApp, mergedDooh,
-                null, mergedDevice);
+                null, null);
 
         assertThat(result).isEqualTo(mergedConfigFpd);
     }
@@ -74,30 +68,24 @@ public class JsonMergerTest extends VertxTest {
     public void mergeShouldReturnOriginalObjectWhenMergedObjectIsNull() {
         // given
         final Site site = Site.builder().build();
-        final Device device = Device.builder().build();
 
         // when
         final Site result = target.merge(site, null, Site.class);
-        final Device resultDevice = target.merge(device, null, Device.class);
 
         // then
         assertThat(result).isEqualTo(site);
-        assertThat(resultDevice).isEqualTo(device);
     }
 
     @Test
     public void mergeShouldReturnMergedObjectWhenOriginalObjectIsNull() {
         // given
         final Site site = Site.builder().build();
-        final Device device = Device.builder().build();
 
         // when
         final Site result = target.merge(null, site, Site.class);
-        final Device resultDevice = target.merge(null, device, Device.class);
 
         // then
         assertThat(result).isEqualTo(site);
-        assertThat(resultDevice).isEqualTo(device);
     }
 
 }
