8afa313d1b484a80aec93ada0fc77792aa8aeca8
osulzhenko <osulzhenko@magnite.com>
2025-05-20 10:27:00 +0300

    Revert "Zeta Global SSP: Add sid parameter and audio support, no longer generic (#3865)"
    This reverts commit 159efa6e1e897b4d03e731d2c55eb4a74c452992.

--- src/main/java/org/prebid/server/proto/openrtb/ext/request/zeta_global_ssp/ExtImpZetaGlobalSSP.java
+++
@@ -1,9 +0,0 @@
-package org.prebid.server.proto.openrtb.ext.request.zeta_global_ssp;
-
-import lombok.Value;
-
-@Value(staticConstructor = "of")
-public class ExtImpZetaGlobalSSP {
-
-    Integer sid;
-}
