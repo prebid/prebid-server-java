4c73213420d22f383e85a8a036eaf36f41a3f587
Alex Maltsev <and1sscsgo@gmail.com>
2025-05-21 15:06:03 +0300

    WIP.

--- extra/modules/rule-engine/src/main/java/org/prebid/server/hooks/modules/rule/engine/core/rules/CompositeRule.java
+++ extra/modules/rule-engine/src/main/java/org/prebid/server/hooks/modules/rule/engine/core/rules/CompositeRule.java
@@ -1,11 +1,7 @@
 package org.prebid.server.hooks.modules.rule.engine.core.rules;
 
 import lombok.Value;
-import org.prebid.server.hooks.execution.v1.analytics.TagsImpl;
-import org.prebid.server.hooks.v1.analytics.Activity;
-import org.prebid.server.model.UpdateResult;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Value(staticConstructor = "of")
@@ -15,17 +11,9 @@ public class CompositeRule<T> implements Rule<T> {
 
     @Override
     public RuleResult<T> process(T value) {
-        final List<Activity> activities = new ArrayList<>();
-        T modifiedValue = value;
-        boolean updated = false;
-
-        for (Rule<T> subrule : subrules) {
-            final RuleResult<T> subresult = subrule.process(modifiedValue);
-            modifiedValue = subresult.getUpdateResult().getValue();
-            updated = updated | subresult.getUpdateResult().isUpdated();
-            activities.addAll(subresult.getAnalyticsTags().activities());
-        }
-
-        return RuleResult.of(UpdateResult.of(updated, modifiedValue), TagsImpl.of(activities));
+        return subrules.stream().reduce(
+                RuleResult.unaltered(value),
+                (result, rule) -> result.mergeWith(rule.process(result.getUpdateResult().getValue())),
+                RuleResult::mergeWith);
     }
 }
