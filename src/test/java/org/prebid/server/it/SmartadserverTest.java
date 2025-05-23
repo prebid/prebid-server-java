e8e5f126b26c6a45af04f9d8094be4e3202819ce
Emil Nadimanov <emil.nadimanov@gutefrage.net>
2025-05-23 10:54:36 +0200

    Added an integration test for the alias (#3608)

--- src/test/java/org/prebid/server/it/SmartadserverTest.java
+++ src/test/java/org/prebid/server/it/SmartadserverTest.java
@@ -11,9 +11,13 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
 public class SmartadserverTest extends IntegrationTest {
+
+    private static final String SMARTADSERVER = "smartadserver";
+    private static final String EQUATIV_ALIAS = "equativ";
 
     @Test
     public void openrtb2AuctionShouldRespondWithBidsFromSmartadserver() throws IOException, JSONException {
@@ -29,6 +33,25 @@ public class SmartadserverTest extends IntegrationTest {
 
         // then
         assertJsonEquals("openrtb2/smartadserver/test-auction-smartadserver-response.json", response,
-                singletonList("smartadserver"));
+                singletonList(SMARTADSERVER));
+    }
+
+    @Test
+    public void openrtb2AuctionShouldRespondWithBidsFromEquativ() throws IOException, JSONException {
+        // given
+        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/smartadserver-exchange/api/bid"))
+                .withRequestBody(
+                        equalToJson(jsonFrom("openrtb2/smartadserver/alias/test-smartadserver-bid-request.json")))
+                .willReturn(aResponse()
+                        .withBody(jsonFrom("openrtb2/smartadserver/alias/test-smartadserver-bid-response.json"))));
+
+        // when
+        final Response response = responseFor(
+                "openrtb2/smartadserver/alias/test-auction-smartadserver-request.json",
+                Endpoint.openrtb2_auction);
+
+        // then
+        assertJsonEquals("openrtb2/smartadserver/alias/test-auction-smartadserver-response.json", response,
+                asList(SMARTADSERVER, EQUATIV_ALIAS));
     }
 }
