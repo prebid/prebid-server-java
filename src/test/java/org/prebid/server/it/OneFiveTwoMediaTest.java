4883a792290f96e4e750463c9c76dd1b76355454
Armando Carballo <armando.carballo@freestar.io>
2025-05-22 21:26:19 -0600

    Issue 3727: Fix 152media integration test

--- src/test/java/org/prebid/server/it/OneFiveTwoMediaTest.java
+++ src/test/java/org/prebid/server/it/OneFiveTwoMediaTest.java
@@ -18,7 +18,7 @@ public class OneFiveTwoMediaTest extends IntegrationTest {
     @Test
     public void openrtb2AuctionShouldRespondWithBidsFrom152Media() throws IOException, JSONException {
         // given
-        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/152media-exchange"))
+        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/adkernel-exchange"))
                 .withRequestBody(equalToJson(jsonFrom("openrtb2/152media/test-152media-bid-request.json")))
                 .willReturn(aResponse().withBody(jsonFrom("openrtb2/152media/test-152media-bid-response.json"))));
 
