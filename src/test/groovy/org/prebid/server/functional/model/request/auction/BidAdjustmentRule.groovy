a828c326bdd13fc129e4ed10b757796459018a98
Markiyan Mykush <95693607+marki1an@users.noreply.github.com>
2025-05-28 11:46:21 +0300

    Test: bid adjustments matching strategy (#3940)

--- src/test/groovy/org/prebid/server/functional/model/request/auction/BidAdjustmentRule.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/BidAdjustmentRule.groovy
@@ -1,11 +1,8 @@
 package org.prebid.server.functional.model.request.auction
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
 
-@JsonNaming(PropertyNamingStrategies.LowerCaseStrategy)
 @ToString(includeNames = true, ignoreNulls = true)
 class BidAdjustmentRule {
 
@@ -13,4 +10,9 @@ class BidAdjustmentRule {
     Map<String, List<AdjustmentRule>> wildcardBidder
     Map<String, List<AdjustmentRule>> generic
     Map<String, List<AdjustmentRule>> alias
+    @JsonProperty("ALIAS")
+    Map<String, List<AdjustmentRule>> aliasUpperCase
+    @JsonProperty("AlIaS")
+    Map<String, List<AdjustmentRule>> aliasCamelCase
+    Map<String, List<AdjustmentRule>> amx
 }
