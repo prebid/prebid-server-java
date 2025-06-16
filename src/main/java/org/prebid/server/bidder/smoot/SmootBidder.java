104d2ef0a96f7519248509f19318cc967e56e45c
antonbabak <ababak@magnite.com>
2025-06-16 11:17:31 +0200

    New Smoot Adapter

---
+++ src/main/java/org/prebid/server/bidder/smoot/SmootBidder.java
@@ -0,0 +1,117 @@
+package org.prebid.server.bidder.smoot;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.prebid.server.bidder.Bidder;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.BidderCall;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
+import org.prebid.server.exception.PreBidException;
+import org.prebid.server.json.DecodeException;
+import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.proto.openrtb.ext.ExtPrebid;
+import org.prebid.server.proto.openrtb.ext.request.smoot.ExtImpSmoot;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.util.BidderUtil;
+import org.prebid.server.util.HttpUtil;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class SmootBidder implements Bidder<BidRequest> {
+
+    private static final TypeReference<ExtPrebid<?, ExtImpSmoot>> SMOOT_EXT_TYPE_REFERENCE = new TypeReference<>() {
+    };
+
+    private final String endpointUrl;
+    private final JacksonMapper mapper;
+
+    public SmootBidder(String endpointUrl, JacksonMapper mapper) {
+        this.endpointUrl = HttpUtil.validateUrl(Objects.requireNonNull(endpointUrl));
+        this.mapper = Objects.requireNonNull(mapper);
+    }
+
+    @Override
+    public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest request) {
+        final List<HttpRequest<BidRequest>> httpRequests = new ArrayList<>();
+        final List<BidderError> errors = new ArrayList<>();
+
+        for (Imp imp : request.getImp()) {
+            try {
+                final ExtImpSmoot extImpSmoot = parseImpExt(imp);
+                final Imp modifiedImp = modifyImp(imp, extImpSmoot);
+                final BidRequest outgoingRequest = request.toBuilder()
+                        .imp(Collections.singletonList(modifiedImp))
+                        .build();
+                httpRequests.add(BidderUtil.defaultRequest(outgoingRequest, endpointUrl, mapper));
+            } catch (PreBidException e) {
+                errors.add(BidderError.badInput(e.getMessage()));
+            }
+        }
+
+        return Result.of(httpRequests, errors);
+    }
+
+    @Override
+    public Result<List<BidderBid>> makeBids(BidderCall<BidRequest> httpCall, BidRequest bidRequest) {
+        try {
+            final BidResponse bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
+            return Result.withValues(extractBids(bidResponse));
+        } catch (DecodeException | PreBidException e) {
+            return Result.withError(BidderError.badServerResponse(e.getMessage()));
+        }
+    }
+
+    private static List<BidderBid> extractBids(BidResponse bidResponse) {
+        if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
+            return Collections.emptyList();
+        }
+        return bidResponse.getSeatbid().stream()
+                .filter(Objects::nonNull)
+                .map(SeatBid::getBid)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .map(bid -> BidderBid.of(bid, getBidType(bid), bidResponse.getCur()))
+                .toList();
+    }
+
+    private static BidType getBidType(Bid bid) {
+        return switch (bid.getMtype()) {
+            case 1 -> BidType.banner;
+            case 2 -> BidType.video;
+            case 4 -> BidType.xNative;
+            case null, default ->
+                    throw new PreBidException("could not define media type for impression: " + bid.getImpid());
+        };
+    }
+
+    private ExtImpSmoot parseImpExt(Imp imp) {
+        try {
+            return mapper.mapper().convertValue(imp.getExt(), SMOOT_EXT_TYPE_REFERENCE).getBidder();
+        } catch (IllegalArgumentException e) {
+            throw new PreBidException("Error parsing imp.ext: " + e.getMessage());
+        }
+    }
+
+    private Imp modifyImp(Imp imp, ExtImpSmoot extImpSmoot) {
+        final SmootImpExt smootImpExt = StringUtils.isNotEmpty(extImpSmoot.getPlacementId())
+                ? SmootImpExt.publisher(extImpSmoot.getPlacementId())
+                : SmootImpExt.network(extImpSmoot.getEndpointId());
+
+        return imp.toBuilder()
+                .ext(mapper.mapper().createObjectNode().set("bidder", mapper.mapper().valueToTree(smootImpExt)))
+                .build();
+    }
+}
