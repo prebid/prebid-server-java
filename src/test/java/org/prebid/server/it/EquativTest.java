05e7465ffbe80db19082e1231611d088ce03a9f6
Emil Nadimanov <emil.nadimanov@gutefrage.net>
2025-07-14 09:12:55 +0200

    Corrected Equativ integration test (#3608)

--- src/test/java/org/prebid/server/it/EquativTest.java
+++ src/test/java/org/prebid/server/it/EquativTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.prebid.server.model.Endpoint;
 
 import java.io.IOException;
-import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static java.util.Collections.singletonList;
 
 public class EquativTest extends IntegrationTest {
 
@@ -20,17 +20,17 @@ public class EquativTest extends IntegrationTest {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/smartadserver-exchange/api/bid"))
                 .withRequestBody(
-                        equalToJson(jsonFrom("openrtb2/smartadserver/alias/test-equativ-bid-request.json")))
+                        equalToJson(jsonFrom("openrtb2/equativ/test-equativ-bid-request.json")))
                 .willReturn(aResponse()
-                        .withBody(jsonFrom("openrtb2/smartadserver/alias/test-equativ-bid-response.json"))));
+                        .withBody(jsonFrom("openrtb2/equativ/test-equativ-bid-response.json"))));
 
         // when
         final Response response = responseFor(
-                "openrtb2/smartadserver/alias/test-auction-equativ-request.json",
+                "openrtb2/equativ/test-auction-equativ-request.json",
                 Endpoint.openrtb2_auction);
 
         // then
-        assertJsonEquals("openrtb2/smartadserver/alias/test-auction-equativ-response.json", response,
-                List.of("smartadserver", "equativ"));
+        assertJsonEquals("openrtb2/equativ/test-auction-equativ-response.json", response,
+                singletonList("equativ"));
     }
 }
