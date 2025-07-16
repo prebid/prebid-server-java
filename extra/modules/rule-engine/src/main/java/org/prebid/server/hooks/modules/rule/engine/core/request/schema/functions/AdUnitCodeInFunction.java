c1f2b918da6bb6574195d9a230ba251e46ac423f
Alex Maltsev <and1sscsgo@gmail.com>
2025-07-17 02:51:42 +0300

    Refactorings.

--- extra/modules/rule-engine/src/main/java/org/prebid/server/hooks/modules/rule/engine/core/request/schema/functions/AdUnitCodeInFunction.java
+++ extra/modules/rule-engine/src/main/java/org/prebid/server/hooks/modules/rule/engine/core/request/schema/functions/AdUnitCodeInFunction.java
@@ -7,8 +7,8 @@ import com.iab.openrtb.request.Imp;
 import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.prebid.server.hooks.modules.rule.engine.core.request.Granularity;
-import org.prebid.server.hooks.modules.rule.engine.core.request.util.AdUnitCodeUtils;
 import org.prebid.server.hooks.modules.rule.engine.core.request.context.RequestSchemaContext;
+import org.prebid.server.hooks.modules.rule.engine.core.request.schema.functions.util.AdUnitCodeUtils;
 import org.prebid.server.hooks.modules.rule.engine.core.rules.schema.SchemaFunction;
 import org.prebid.server.hooks.modules.rule.engine.core.rules.schema.SchemaFunctionArguments;
 import org.prebid.server.hooks.modules.rule.engine.core.util.ValidationUtils;
@@ -28,7 +28,8 @@ public class AdUnitCodeInFunction implements SchemaFunction<RequestSchemaContext
     @Override
     public String extract(SchemaFunctionArguments<RequestSchemaContext> arguments) {
         final RequestSchemaContext context = arguments.getOperand();
-        final String impId = ((Granularity.Imp) arguments.getOperand().getGranularity()).impId();;
+        final String impId = ((Granularity.Imp) arguments.getOperand().getGranularity()).impId();
+        ;
         final BidRequest bidRequest = context.getBidRequest();
 
         final Imp adUnit = ListUtils.emptyIfNull(bidRequest.getImp()).stream()
