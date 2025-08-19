8084bb94f4af207b976516ce2e5dfe13c5255b28
Alex Maltsev <and1sscsgo@gmail.com>
2025-08-19 23:48:36 +0300

    Finished tests for ExcludeBiddersFunction.

--- extra/modules/pb-rule-engine/src/test/java/org/prebid/server/hooks/modules/rule/engine/core/request/result/functions/filter/ExcludeBiddersFunctionTest.java
+++ extra/modules/pb-rule-engine/src/test/java/org/prebid/server/hooks/modules/rule/engine/core/request/result/functions/filter/ExcludeBiddersFunctionTest.java
@@ -37,6 +37,8 @@ import org.prebid.server.proto.openrtb.ext.response.seatnonbid.SeatNonBid;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -95,14 +97,7 @@ class ExcludeBiddersFunctionTest {
                 givenAuctionContext(), Granularity.Request.instance(), null);
 
         final InfrastructureArguments<RequestRuleContext> infrastructureArguments =
-                InfrastructureArguments.<RequestRuleContext>builder()
-                        .context(context)
-                        .schemaFunctionResults(Collections.emptyMap())
-                        .schemaFunctionMatches(Collections.emptyMap())
-                        .ruleFired("ruleFired")
-                        .analyticsKey("analyticsKey")
-                        .modelVersion("modelVersion")
-                        .build();
+                givenInfrastructureArguments(context);
 
         final FilterBiddersFunctionConfig config = FilterBiddersFunctionConfig.builder()
                 .bidders(Collections.singleton("bidder1"))
@@ -155,14 +150,7 @@ class ExcludeBiddersFunctionTest {
                 givenAuctionContext(), new Granularity.Imp("impId2"), null);
 
         final InfrastructureArguments<RequestRuleContext> infrastructureArguments =
-                InfrastructureArguments.<RequestRuleContext>builder()
-                        .context(context)
-                        .schemaFunctionResults(Collections.emptyMap())
-                        .schemaFunctionMatches(Collections.emptyMap())
-                        .ruleFired("ruleFired")
-                        .analyticsKey("analyticsKey")
-                        .modelVersion("modelVersion")
-                        .build();
+                givenInfrastructureArguments(context);
 
         final FilterBiddersFunctionConfig config = FilterBiddersFunctionConfig.builder()
                 .bidders(Collections.singleton("bidder3"))
@@ -212,7 +200,7 @@ class ExcludeBiddersFunctionTest {
     }
 
     @Test
-    void applyShouldDiscardImpIfAfterUpdateThereAreNoBidders() {
+    void applyShouldDiscardImpIfAfterUpdateImpExtHasNoBidders() {
         // given
         final BidRequest bidRequest = givenBidRequest(
                 givenImp("impId", "bidder1", "bidder2"),
@@ -222,17 +210,10 @@ class ExcludeBiddersFunctionTest {
                 givenAuctionContext(), new Granularity.Imp("impId2"), null);
 
         final InfrastructureArguments<RequestRuleContext> infrastructureArguments =
-                InfrastructureArguments.<RequestRuleContext>builder()
-                        .context(context)
-                        .schemaFunctionResults(Collections.emptyMap())
-                        .schemaFunctionMatches(Collections.emptyMap())
-                        .ruleFired("ruleFired")
-                        .analyticsKey("analyticsKey")
-                        .modelVersion("modelVersion")
-                        .build();
+                givenInfrastructureArguments(context);
 
         final FilterBiddersFunctionConfig config = FilterBiddersFunctionConfig.builder()
-                .bidders(Collections.singleton("bidder3"))
+                .bidders(Set.of("bidder3", "bidder4"))
                 .seatNonBid(BidRejectionReason.REQUEST_BLOCKED_GENERAL)
                 .analyticsValue("analyticsValue")
                 .build();
@@ -250,7 +231,7 @@ class ExcludeBiddersFunctionTest {
         expectedResultValue.set("modelVersion", TextNode.valueOf("modelVersion"));
         expectedResultValue.set("conditionFired", TextNode.valueOf("ruleFired"));
         expectedResultValue.set("resultFunction", TextNode.valueOf("excludeBidders"));
-        expectedResultValue.set("biddersRemoved", mapper.createArrayNode().add("bidder3"));
+        expectedResultValue.set("biddersRemoved", mapper.createArrayNode().add("bidder3").add("bidder4"));
         expectedResultValue.set("seatNonBid", IntNode.valueOf(BidRejectionReason.REQUEST_BLOCKED_GENERAL.getValue()));
 
         final AppliedTo expectedAppliedTo = AppliedToImpl.builder()
@@ -262,20 +243,133 @@ class ExcludeBiddersFunctionTest {
                 "success",
                 Collections.singletonList(ResultImpl.of("success", expectedResultValue, expectedAppliedTo)));
 
-        final SeatNonBid expectedSeatNonBid = SeatNonBid.of(
-                "bidder3",
-                Collections.singletonList(NonBid.of("impId2", BidRejectionReason.REQUEST_BLOCKED_GENERAL)));
+
+        final List<SeatNonBid> expectedSeatNonBid = List.of(
+                SeatNonBid.of(
+                        "bidder3",
+                        Collections.singletonList(NonBid.of("impId2", BidRejectionReason.REQUEST_BLOCKED_GENERAL))),
+                SeatNonBid.of(
+                        "bidder4",
+                        Collections.singletonList(NonBid.of("impId2", BidRejectionReason.REQUEST_BLOCKED_GENERAL))));
 
         final BidRequest expectedBidRequest = givenBidRequest(
-                givenImp("impId", "bidder1", "bidder2"),
-                givenImp("impId2", "bidder4"));
+                givenImp("impId", "bidder1", "bidder2"));
 
         assertThat(result).isEqualTo(
                 RuleResult.of(
                         expectedBidRequest,
                         RuleAction.UPDATE,
                         givenATags(expectedActivity),
-                        Collections.singletonList(expectedSeatNonBid)));
+                        expectedSeatNonBid));
+    }
+
+    @Test
+    void applyShouldRejectBidRequestIfUpdatedRequestHasNoImps() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(
+                givenImp("impId", "bidder"));
+
+        final RequestRuleContext context = RequestRuleContext.of(
+                givenAuctionContext(), new Granularity.Imp("impId"), null);
+
+        final InfrastructureArguments<RequestRuleContext> infrastructureArguments =
+                givenInfrastructureArguments(context);
+
+        final FilterBiddersFunctionConfig config = FilterBiddersFunctionConfig.builder()
+                .bidders(Set.of("bidder"))
+                .seatNonBid(BidRejectionReason.REQUEST_BLOCKED_GENERAL)
+                .analyticsValue("analyticsValue")
+                .build();
+
+        final ResultFunctionArguments<BidRequest, RequestRuleContext> arguments =
+                ResultFunctionArguments.of(bidRequest, mapper.valueToTree(config), infrastructureArguments);
+
+        // when
+        final RuleResult<BidRequest> result = target.apply(arguments);
+
+        // then
+        final ObjectNode expectedResultValue = mapper.createObjectNode();
+        expectedResultValue.set("analyticsKey", TextNode.valueOf("analyticsKey"));
+        expectedResultValue.set("analyticsValue", TextNode.valueOf("analyticsValue"));
+        expectedResultValue.set("modelVersion", TextNode.valueOf("modelVersion"));
+        expectedResultValue.set("conditionFired", TextNode.valueOf("ruleFired"));
+        expectedResultValue.set("resultFunction", TextNode.valueOf("excludeBidders"));
+        expectedResultValue.set("biddersRemoved", mapper.createArrayNode().add("bidder"));
+        expectedResultValue.set("seatNonBid", IntNode.valueOf(BidRejectionReason.REQUEST_BLOCKED_GENERAL.getValue()));
+
+        final AppliedTo expectedAppliedTo = AppliedToImpl.builder()
+                .impIds(Collections.singletonList("impId"))
+                .build();
+
+        final Activity expectedActivity = ActivityImpl.of(
+                "pb-rule-engine",
+                "success",
+                Collections.singletonList(ResultImpl.of("success", expectedResultValue, expectedAppliedTo)));
+
+
+        final List<SeatNonBid> expectedSeatNonBid = List.of(
+                SeatNonBid.of(
+                        "bidder",
+                        Collections.singletonList(NonBid.of("impId", BidRejectionReason.REQUEST_BLOCKED_GENERAL))));
+
+        assertThat(result).isEqualTo(RuleResult.rejected(givenATags(expectedActivity), expectedSeatNonBid));
+    }
+
+    @Test
+    void applyShouldNotGenerateATagWhenNoAnalyticsKeySpecified() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(
+                givenImp("impId", "bidder1", "bidder2"));
+
+        final RequestRuleContext context = RequestRuleContext.of(
+                givenAuctionContext(), new Granularity.Imp("impId"), null);
+
+        final InfrastructureArguments<RequestRuleContext> infrastructureArguments =
+                InfrastructureArguments.<RequestRuleContext>builder()
+                        .context(context)
+                        .schemaFunctionResults(Collections.emptyMap())
+                        .schemaFunctionMatches(Collections.emptyMap())
+                        .ruleFired("ruleFired")
+                        .modelVersion("modelVersion")
+                        .build();
+
+        final FilterBiddersFunctionConfig config = FilterBiddersFunctionConfig.builder()
+                .bidders(Set.of("bidder1"))
+                .seatNonBid(BidRejectionReason.REQUEST_BLOCKED_GENERAL)
+                .analyticsValue("analyticsValue")
+                .build();
+
+        final ResultFunctionArguments<BidRequest, RequestRuleContext> arguments =
+                ResultFunctionArguments.of(bidRequest, mapper.valueToTree(config), infrastructureArguments);
+
+        // when
+        final RuleResult<BidRequest> result = target.apply(arguments);
+
+        // then
+        final List<SeatNonBid> expectedSeatNonBid = List.of(
+                SeatNonBid.of(
+                        "bidder1",
+                        Collections.singletonList(NonBid.of("impId", BidRejectionReason.REQUEST_BLOCKED_GENERAL))));
+
+        assertThat(result).isEqualTo(
+                RuleResult.of(
+                        givenBidRequest(givenImp("impId", "bidder2")),
+                        RuleAction.UPDATE,
+                        givenATags(),
+                        expectedSeatNonBid));
+    }
+
+    private static InfrastructureArguments<RequestRuleContext> givenInfrastructureArguments(
+            RequestRuleContext context) {
+
+        return InfrastructureArguments.<RequestRuleContext>builder()
+                .context(context)
+                .schemaFunctionResults(Collections.emptyMap())
+                .schemaFunctionMatches(Collections.emptyMap())
+                .ruleFired("ruleFired")
+                .analyticsKey("analyticsKey")
+                .modelVersion("modelVersion")
+                .build();
     }
 
     private static Tags givenATags(Activity... activities) {
