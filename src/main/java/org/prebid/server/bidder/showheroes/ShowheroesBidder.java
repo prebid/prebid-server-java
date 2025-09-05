96792a6c4db9d65d8bcb1734f53755adcd12a0da
Filip Stamenkovic <filip.stamenkovic@showheroes-group.com>
2025-09-05 16:29:56 +0200

    add showheroes bid adapter

---
+++ src/main/java/org/prebid/server/bidder/showheroes/ShowheroesBidder.java
@@ -0,0 +1,213 @@
+package org.prebid.server.bidder.showheroes;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.iab.openrtb.request.Banner;
+import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.request.Video;
+import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
+import org.apache.commons.collections4.CollectionUtils;
+import org.prebid.server.bidder.Bidder;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.BidderCall;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.Result;
+import org.prebid.server.currency.CurrencyConversionService;
+import org.prebid.server.exception.PreBidException;
+import org.prebid.server.json.DecodeException;
+import org.prebid.server.json.JacksonMapper;
+import org.prebid.server.proto.openrtb.ext.ExtPrebid;
+import org.prebid.server.proto.openrtb.ext.request.ExtRequest;
+import org.prebid.server.proto.openrtb.ext.request.ExtRequestPrebid;
+import org.prebid.server.proto.openrtb.ext.request.ExtRequestPrebidChannel;
+import org.prebid.server.proto.openrtb.ext.request.showheroes.ExtImpShowheroes;
+import org.prebid.server.proto.openrtb.ext.response.BidType;
+import org.prebid.server.util.BidderUtil;
+import org.prebid.server.util.HttpUtil;
+import org.prebid.server.version.PrebidVersionProvider;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ShowheroesBidder implements Bidder<BidRequest> {
+
+    private static final String BID_CURRENCY = "EUR";
+    private static final String DEFAULT_ORTB_CURRENCY = "USD";
+    private static final TypeReference<ExtPrebid<?, ExtImpShowheroes>> SHOWHEROES_EXT_TYPE_REFERENCE =
+            new TypeReference<>() {
+            };
+
+    private final String endpointUrl;
+    private final CurrencyConversionService currencyConversionService;
+    private final JacksonMapper mapper;
+
+    public ShowheroesBidder(String endpointUrl,
+            CurrencyConversionService currencyConversionService,
+            PrebidVersionProvider prebidVersionProvider,
+            JacksonMapper mapper) {
+
+        this.endpointUrl = HttpUtil.validateUrlSyntax(Objects.requireNonNull(endpointUrl));
+        this.currencyConversionService = Objects.requireNonNull(currencyConversionService);
+        this.mapper = Objects.requireNonNull(mapper);
+    }
+
+    private BidderError validate(BidRequest bidRequest) {
+        // request must contain site object with page or app object with bundle
+        if (bidRequest.getSite() == null && bidRequest.getApp() == null) {
+            return BidderError.badInput("BidRequest must contain one of site or app");
+        }
+        if (bidRequest.getSite() != null && bidRequest.getSite().getPage() == null) {
+            return BidderError.badInput("BidRequest.site.page is required");
+        }
+        if (bidRequest.getApp() != null && bidRequest.getApp().getBundle() == null) {
+            return BidderError.badInput("BidRequest.app.bundle is required");
+        }
+        return null;
+    }
+
+    private ExtRequestPrebidChannel getPrebidChannel(BidRequest bidRequest) {
+        return Optional.ofNullable(bidRequest.getExt())
+                .map(ExtRequest::getPrebid)
+                .map(ExtRequestPrebid::getChannel)
+                .orElse(null);
+    }
+
+    private Imp processImpression(BidRequest bidRequest, Imp imp, ExtRequestPrebidChannel prebidChannel) {
+        final Banner banner = imp.getBanner();
+        final Video video = imp.getVideo();
+        if (banner == null && video == null) {
+            throw new PreBidException("Impression must contain one of banner, video or native");
+        }
+
+        final ExtImpShowheroes extImpShowheroes = mapper.mapper()
+                .convertValue(imp.getExt(), SHOWHEROES_EXT_TYPE_REFERENCE).getBidder();
+        if (extImpShowheroes == null || extImpShowheroes.getUnitId() == null
+                || extImpShowheroes.getUnitId().isBlank()) {
+            throw new PreBidException("Ext.imp.bidder.unitId is required");
+        }
+
+        String channelName = null;
+        String channelVersion = null;
+        if (prebidChannel != null) {
+            channelName = prebidChannel.getName();
+            channelVersion = prebidChannel.getVersion();
+        }
+
+        final ObjectNode impExt = imp.getExt();
+
+        final Imp.ImpBuilder impBuilder = imp.toBuilder();
+
+        // copy unitId from ext.bidder to ext.params
+        impExt.set("params", JsonNodeFactory.instance.objectNode()
+                .put("unitId", extImpShowheroes.getUnitId()));
+
+        impBuilder.ext(impExt);
+        if (imp.getDisplaymanager() == null && channelName != null) {
+            impBuilder.displaymanager(channelName);
+            impBuilder.displaymanagerver(channelVersion);
+        }
+
+        String currency = imp.getBidfloorcur();
+        // if floor price is 0, or currency is EUR - no need to convert
+        if (imp.getBidfloor() == null || imp.getBidfloor().compareTo(BigDecimal.ZERO) == 0
+                || currency == BID_CURRENCY) {
+            return impBuilder.build();
+        }
+        if (currency != null && !currency.isBlank()) {
+            // if not provided default currency is USD
+            currency = DEFAULT_ORTB_CURRENCY;
+        }
+
+        final BigDecimal eurFloor = currencyConversionService.convertCurrency(
+                imp.getBidfloor(), bidRequest, currency, BID_CURRENCY);
+        return impBuilder
+                .bidfloorcur(BID_CURRENCY)
+                .bidfloor(eurFloor)
+                .build();
+    }
+
+    @Override
+    public Result<List<HttpRequest<BidRequest>>> makeHttpRequests(BidRequest request) {
+        final BidderError validationError = validate(request);
+        if (validationError != null) {
+            return Result.of(Collections.emptyList(), List.of(validationError));
+        }
+
+        final List<BidderError> errors = new ArrayList<>();
+        final List<HttpRequest<BidRequest>> httpRequests = new ArrayList<>();
+
+        final ExtRequestPrebidChannel prebidChannel = getPrebidChannel(request);
+        final List<Imp> modifiedImps = new ArrayList<>(request.getImp().size());
+
+        for (Imp impression : request.getImp()) {
+            try {
+                modifiedImps.add(processImpression(request, impression, prebidChannel));
+            } catch (Exception e) {
+                errors.add(BidderError.badInput(e.getMessage()));
+                continue;
+            }
+        }
+
+        httpRequests.add(makeHttpRequest(request.toBuilder().imp(modifiedImps).build()));
+        return Result.of(httpRequests, errors);
+    }
+
+    private HttpRequest<BidRequest> makeHttpRequest(BidRequest request) {
+        return BidderUtil.defaultRequest(request, endpointUrl, mapper);
+    }
+
+    @Override
+    public Result<List<BidderBid>> makeBids(BidderCall<BidRequest> httpCall, BidRequest bidRequest) {
+        final BidResponse bidResponse;
+        final int statusCode = httpCall.getResponse().getStatusCode();
+        if (statusCode == 204) {
+            return Result.of(Collections.emptyList(), Collections.emptyList());
+        }
+        if (statusCode != 200) {
+            return Result.withError(BidderError.badServerResponse(
+                    "Unexpected status code: " + statusCode));
+        }
+
+        try {
+            bidResponse = mapper.decodeValue(httpCall.getResponse().getBody(), BidResponse.class);
+        } catch (DecodeException | PreBidException e) {
+            return Result.withError(BidderError.badServerResponse(e.getMessage()));
+        }
+
+        return Result.of(extractBids(bidResponse), Collections.emptyList());
+    }
+
+    private List<BidderBid> extractBids(BidResponse bidResponse) {
+        if (bidResponse == null || CollectionUtils.isEmpty(bidResponse.getSeatbid())) {
+            return Collections.emptyList();
+        }
+
+        return bidResponse.getSeatbid().stream()
+                .filter(Objects::nonNull)
+                .map(SeatBid::getBid)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .map(bid -> BidderBid.of(bid, getBidType(bid), bidResponse.getCur()))
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+    private BidType getBidType(Bid bid) {
+        return switch (bid.getMtype()) {
+            case 1 -> BidType.banner;
+            case 2 -> BidType.video;
+            default -> BidType.video; // if not provided video is assumed
+        };
+    }
+}
---
+++ src/test/java/org/prebid/server/bidder/showheroes/ShowheroesBidderTest.java
@@ -0,0 +1,377 @@
+package org.prebid.server.bidder.showheroes;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.iab.openrtb.request.App;
+import com.iab.openrtb.request.Banner;
+import com.iab.openrtb.request.BidRequest;
+import com.iab.openrtb.request.Imp;
+import com.iab.openrtb.request.Site;
+import com.iab.openrtb.request.Video;
+import com.iab.openrtb.response.Bid;
+import com.iab.openrtb.response.BidResponse;
+import com.iab.openrtb.response.SeatBid;
+import org.junit.jupiter.api.Test;
+import org.prebid.server.VertxTest;
+import org.prebid.server.bidder.model.BidderBid;
+import org.prebid.server.bidder.model.BidderCall;
+import org.prebid.server.bidder.model.BidderError;
+import org.prebid.server.bidder.model.HttpRequest;
+import org.prebid.server.bidder.model.HttpResponse;
+import org.prebid.server.bidder.model.Result;
+import org.prebid.server.proto.openrtb.ext.ExtPrebid;
+import org.prebid.server.proto.openrtb.ext.request.showheroes.ExtImpShowheroes;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.Collections.singletonList;
+import static java.util.function.Function.identity;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.prebid.server.proto.openrtb.ext.response.BidType.banner;
+import static org.prebid.server.proto.openrtb.ext.response.BidType.video;
+
+public class ShowheroesBidderTest extends VertxTest {
+
+    private static final String ENDPOINT_URL = "https://ads.showheroes.com/";
+
+    private final ShowheroesBidder target = new ShowheroesBidder(ENDPOINT_URL, jacksonMapper);
+
+    @Test
+    public void creationShouldFailOnInvalidEndpointUrl() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new ShowheroesBidder("invalid_url", jacksonMapper));
+    }
+
+    @Test
+    public void makeHttpRequestsShouldReturnErrorIfImpExtCouldNotBeParsed() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(
+                impBuilder -> impBuilder
+                        .ext(mapper.valueToTree(ExtPrebid.of(null, mapper.createArrayNode()))));
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = target.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).hasSize(1);
+        assertThat(result.getErrors().get(0).getMessage()).startsWith("Cannot deserialize value");
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeHttpRequestsShouldReturnErrorWhenUnitIdIsEmpty() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(
+                impBuilder -> impBuilder
+                        .ext(mapper.valueToTree(ExtPrebid.of(null, ExtImpShowheroes.of("")))));
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = target.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).hasSize(1)
+                .extracting(BidderError::getMessage)
+                .containsExactly("unitId parameter is required");
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeHttpRequestsShouldReturnErrorWhenSitePageAndAppBundleAreEmpty() {
+        // given
+        final BidRequest bidRequest = BidRequest.builder()
+                .imp(singletonList(givenImp(identity())))
+                .site(Site.builder().page("").build())
+                .app(App.builder().bundle("").build())
+                .build();
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = target.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).hasSize(1)
+                .extracting(BidderError::getMessage)
+                .containsExactly("site.page or app.bundle is required");
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeHttpRequestsShouldCreateCorrectURL() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(identity());
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = target.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).hasSize(1);
+        assertThat(result.getValue().get(0).getUri()).isEqualTo(ENDPOINT_URL);
+    }
+
+    @Test
+    public void makeHttpRequestsShouldConvertCurrencyFromUsdToEur() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(identity());
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = target.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).hasSize(1);
+
+        final BidRequest outgoingRequest = result.getValue().get(0).getPayload();
+        assertThat(outgoingRequest.getCur()).containsExactly("EUR");
+    }
+
+    @Test
+    public void makeHttpRequestsShouldCreateSingleRequestForAllImps() {
+        // given
+        final BidRequest bidRequest = BidRequest.builder()
+                .site(Site.builder().page("https://example.com").build())
+                .imp(List.of(
+                        givenImp(impBuilder -> impBuilder.id("imp1")),
+                        givenImp(impBuilder -> impBuilder.id("imp2"))))
+                .build();
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = target.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).hasSize(1);
+
+        final BidRequest outgoingRequest = result.getValue().get(0).getPayload();
+        assertThat(outgoingRequest.getImp()).hasSize(2)
+                .extracting(Imp::getId)
+                .containsExactly("imp1", "imp2");
+    }
+
+    @Test
+    public void makeHttpRequestsShouldSetCorrectHeaders() {
+        // given
+        final BidRequest bidRequest = givenBidRequest(identity());
+
+        // when
+        final Result<List<HttpRequest<BidRequest>>> result = target.makeHttpRequests(bidRequest);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).hasSize(1);
+
+        final Map<String, String> headers = result.getValue().get(0).getHeaders();
+        assertThat(headers).containsEntry("Content-Type", "application/json;charset=utf-8")
+                .containsEntry("Accept", "application/json");
+    }
+
+    @Test
+    public void makeBidsShouldReturnErrorIfResponseBodyCouldNotBeParsed() {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(null, "invalid");
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).hasSize(1);
+        assertThat(result.getErrors().get(0).getMessage()).startsWith("Failed to decode: Unrecognized token");
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeBidsShouldReturnEmptyListIfBidResponseIsNull() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(null, mapper.writeValueAsString(null));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeBidsShouldReturnEmptyListIfBidResponseSeatBidIsNull() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(null,
+                mapper.writeValueAsString(BidResponse.builder().build()));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeBidsShouldReturnEmptyListIfBidResponseSeatBidIsEmpty() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(null,
+                mapper.writeValueAsString(BidResponse.builder().seatbid(List.of()).build()));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).isEmpty();
+    }
+
+    @Test
+    public void makeBidsShouldReturnBannerBidIfBannerIsPresentInImp() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(
+                BidRequest.builder()
+                        .imp(singletonList(Imp.builder()
+                                .id("123")
+                                .banner(Banner.builder().build())
+                                .build()))
+                        .build(),
+                mapper.writeValueAsString(
+                        givenBidResponse(bidBuilder -> bidBuilder.impid("123").mtype(1))));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue())
+                .extracting(BidderBid::getType)
+                .containsExactly(banner);
+    }
+
+    @Test
+    public void makeBidsShouldReturnVideoBidIfVideoIsPresentInImp() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(
+                BidRequest.builder()
+                        .imp(singletonList(Imp.builder()
+                                .id("123")
+                                .video(Video.builder().build())
+                                .build()))
+                        .build(),
+                mapper.writeValueAsString(
+                        givenBidResponse(bidBuilder -> bidBuilder.impid("123").mtype(2))));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue())
+                .extracting(BidderBid::getType)
+                .containsExactly(video);
+    }
+
+    @Test
+    public void makeBidsShouldReturnVideoBidByDefaultWhenMtypeIsUnknown() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(
+                BidRequest.builder()
+                        .imp(singletonList(Imp.builder()
+                                .id("123")
+                                .video(Video.builder().build())
+                                .build()))
+                        .build(),
+                mapper.writeValueAsString(
+                        givenBidResponse(bidBuilder -> bidBuilder.impid("123").mtype(99))));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue())
+                .extracting(BidderBid::getType)
+                .containsExactly(video);
+    }
+
+    @Test
+    public void makeBidsShouldReturnCorrectBidderBid() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(
+                BidRequest.builder()
+                        .imp(singletonList(Imp.builder()
+                                .id("123")
+                                .banner(Banner.builder().build())
+                                .build()))
+                        .build(),
+                mapper.writeValueAsString(
+                        givenBidResponse(bidBuilder -> bidBuilder.impid("123").price(BigDecimal.ONE))));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue())
+                .extracting(BidderBid::getBid)
+                .extracting(Bid::getPrice)
+                .containsExactly(BigDecimal.ONE);
+    }
+
+    @Test
+    public void makeBidsShouldReturnMultipleBids() throws JsonProcessingException {
+        // given
+        final BidderCall<BidRequest> httpCall = givenHttpCall(
+                BidRequest.builder()
+                        .imp(List.of(
+                                Imp.builder().id("123").banner(Banner.builder().build()).build(),
+                                Imp.builder().id("456").video(Video.builder().build()).build()))
+                        .build(),
+                mapper.writeValueAsString(BidResponse.builder()
+                        .seatbid(singletonList(SeatBid.builder()
+                                .bid(List.of(
+                                        Bid.builder().impid("123").mtype(1).price(BigDecimal.ONE).build(),
+                                        Bid.builder().impid("456").mtype(2).price(BigDecimal.TEN).build()))
+                                .build()))
+                        .build()));
+
+        // when
+        final Result<List<BidderBid>> result = target.makeBids(httpCall, null);
+
+        // then
+        assertThat(result.getErrors()).isEmpty();
+        assertThat(result.getValue()).hasSize(2)
+                .extracting(BidderBid::getBid, BidderBid::getType)
+                .containsExactlyInAnyOrder(
+                        tuple(Bid.builder().impid("123").mtype(1).price(BigDecimal.ONE).build(), banner),
+                        tuple(Bid.builder().impid("456").mtype(2).price(BigDecimal.TEN).build(), video));
+    }
+
+    private static BidRequest givenBidRequest(Function<Imp.ImpBuilder, Imp.ImpBuilder> impCustomizer) {
+        return BidRequest.builder()
+                .site(Site.builder().page("https://example.com").build())
+                .imp(singletonList(givenImp(impCustomizer)))
+                .build();
+    }
+
+    private static Imp givenImp(Function<Imp.ImpBuilder, Imp.ImpBuilder> impCustomizer) {
+        return impCustomizer.apply(Imp.builder()
+                        .id("123")
+                        .banner(Banner.builder().build())
+                        .ext(mapper.valueToTree(ExtPrebid.of(null, ExtImpShowheroes.of("unitId")))))
+                .build();
+    }
+
+    private static BidResponse givenBidResponse(Function<Bid.BidBuilder, Bid.BidBuilder> bidCustomizer) {
+        return BidResponse.builder()
+                .seatbid(singletonList(SeatBid.builder()
+                        .bid(singletonList(bidCustomizer.apply(Bid.builder()).build()))
+                        .build()))
+                .build();
+    }
+
+    private static BidderCall<BidRequest> givenHttpCall(BidRequest bidRequest, String body) {
+        return BidderCall.succeededHttp(
+                HttpRequest.<BidRequest>builder().payload(bidRequest).build(),
+                HttpResponse.of(200, null, body),
+                null);
+    }
+}
+
