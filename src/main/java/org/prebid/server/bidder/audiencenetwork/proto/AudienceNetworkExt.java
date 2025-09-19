ffb66973552cbab7985a7dd00ea01977e08808f3
lixiaofeng <peak.lee1134@gmail.com>
2025-09-19 15:59:49 +0800

    AudienceNetwork adapter optimize in production

--- src/main/java/org/prebid/server/bidder/audiencenetwork/proto/AudienceNetworkExt.java
+++ src/main/java/org/prebid/server/bidder/audiencenetwork/proto/AudienceNetworkExt.java
@@ -4,8 +4,12 @@ import lombok.Value;
 
 @Value(staticConstructor = "of")
 public class AudienceNetworkExt {
-
+    @JsonProperty("platformid")
     String platformid;
 
+    @JsonProperty("authentication_id")
     String authenticationId;
+
+    @JsonProperty("security_app_id")
+    String securityAppId;
 }
