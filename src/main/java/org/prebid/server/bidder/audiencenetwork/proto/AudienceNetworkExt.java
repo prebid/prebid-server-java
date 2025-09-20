4ba422741b72e54a0ac8a155df056c9b5e885970
lixiaofeng <peak.lee1134@gmail.com>
2025-09-20 16:47:31 +0800

    AudienceNetwork adapter: fix checkstyle issue and import missed JsonProperty annotation

--- src/main/java/org/prebid/server/bidder/audiencenetwork/proto/AudienceNetworkExt.java
+++ src/main/java/org/prebid/server/bidder/audiencenetwork/proto/AudienceNetworkExt.java
@@ -1,9 +1,11 @@
 package org.prebid.server.bidder.audiencenetwork.proto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
 public class AudienceNetworkExt {
+
     @JsonProperty("platformid")
     String platformid;
 
