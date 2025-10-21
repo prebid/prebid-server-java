aa4f82a6886a6175d163e8dbb1402dec9a1c3d67
softcoder594 <malicvictor5008@gmail.com>
2025-10-21 14:24:29 +0200

    optable-targeting: Optimize Optable requests for more precise ranging and performance insights (#4222)

--- extra/modules/optable-targeting/src/main/java/org/prebid/server/hooks/modules/optable/targeting/v1/core/BidRequestEnricher.java
+++ extra/modules/optable-targeting/src/main/java/org/prebid/server/hooks/modules/optable/targeting/v1/core/BidRequestEnricher.java
@@ -4,8 +4,11 @@ import com.iab.openrtb.request.BidRequest;
 import com.iab.openrtb.request.Data;
 import com.iab.openrtb.request.Eid;
 import com.iab.openrtb.request.Segment;
+import com.iab.openrtb.request.Uid;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.prebid.server.hooks.execution.v1.auction.AuctionRequestPayloadImpl;
+import org.prebid.server.hooks.modules.optable.targeting.model.config.OptableTargetingProperties;
 import org.prebid.server.hooks.modules.optable.targeting.model.openrtb.Ortb2;
 import org.prebid.server.hooks.modules.optable.targeting.model.openrtb.TargetingResult;
 import org.prebid.server.hooks.modules.optable.targeting.model.openrtb.User;
@@ -23,14 +26,18 @@ import java.util.stream.Stream;
 
 public class BidRequestEnricher implements PayloadUpdate<AuctionRequestPayload> {
 
-    private final TargetingResult targetingResult;
+    private static final String OPTABLE_CO_INSERTER = "optable.co";
 
-    private BidRequestEnricher(TargetingResult targetingResult) {
+    private final TargetingResult targetingResult;
+    private final OptableTargetingProperties targetingProperties;
+
+    private BidRequestEnricher(TargetingResult targetingResult, OptableTargetingProperties targetingProperties) {
         this.targetingResult = targetingResult;
+        this.targetingProperties = targetingProperties;
     }
 
-    public static BidRequestEnricher of(TargetingResult targetingResult) {
-        return new BidRequestEnricher(targetingResult);
+    public static BidRequestEnricher of(TargetingResult targetingResult, OptableTargetingProperties properties) {
+        return new BidRequestEnricher(targetingResult, properties);
     }
 
     @Override
@@ -60,18 +67,88 @@ public class BidRequestEnricher implements PayloadUpdate<AuctionRequestPayload> 
                 .build();
     }
 
-    private static com.iab.openrtb.request.User mergeUserData(com.iab.openrtb.request.User user, User optableUser) {
+    private com.iab.openrtb.request.User mergeUserData(com.iab.openrtb.request.User user, User optableUser) {
         return user.toBuilder()
-                .eids(mergeEids(user.getEids(), optableUser.getEids()))
+                .eids(filterOptableEids(mergeEids(user.getEids(), optableUser.getEids())))
                 .data(mergeData(user.getData(), optableUser.getData()))
                 .build();
     }
 
-    private static List<Eid> mergeEids(List<Eid> destination, List<Eid> source) {
-        return merge(
-                destination,
-                source,
-                Eid::getSource);
+    private List<Eid> mergeEids(List<Eid> destination, List<Eid> source) {
+        if (CollectionUtils.isEmpty(destination)) {
+            return source;
+        }
+
+        if (CollectionUtils.isEmpty(source)) {
+            return destination;
+        }
+
+        final Map<String, Eid> idToSourceEid = source.stream().collect(Collectors.toMap(
+                BidRequestEnricher::eidIdExtractor,
+                Function.identity(),
+                (a, b) -> b,
+                HashMap::new));
+
+        final Set<String> sourceToReplace = targetingProperties.getOptableInserterEidsReplace();
+        final Set<String> sourceToMerge = targetingProperties.getOptableInserterEidsMerge()
+                .stream()
+                .filter(it -> !sourceToReplace.contains(it)).collect(Collectors.toSet());
+
+        final List<Eid> mergedEid = destination.stream()
+                .map(destinationEid -> idToSourceEid.containsKey(eidIdExtractor(destinationEid))
+                        && OPTABLE_CO_INSERTER.equals(destinationEid.getInserter())
+                        ? resolveEidConflict(
+                                destinationEid,
+                                idToSourceEid.get(eidIdExtractor(destinationEid)),
+                                sourceToMerge,
+                                sourceToReplace)
+                        : destinationEid)
+                .toList();
+
+        return merge(mergedEid, source, BidRequestEnricher::eidIdExtractor);
+    }
+
+    private List<Eid> filterOptableEids(List<Eid> eids) {
+        if (CollectionUtils.isEmpty(eids)) {
+            return eids;
+        }
+
+        final Set<String> optableIdsToIgnore = targetingProperties.getOptableInserterEidsIgnore();
+        if (CollectionUtils.isEmpty(optableIdsToIgnore)) {
+            return eids;
+        }
+
+        return eids.stream()
+                .filter(eid -> !OPTABLE_CO_INSERTER.equals(eid.getInserter())
+                        || !optableIdsToIgnore.contains(eid.getSource()))
+                .toList();
+    }
+
+    private static Eid resolveEidConflict(Eid destinationEid,
+                                          Eid sourceEid,
+                                          Set<String> sourceToMerge,
+                                          Set<String> sourceToReplace) {
+
+        final String eidSource = sourceEid.getSource();
+
+        if (sourceToReplace.contains(eidSource)) {
+            return sourceEid;
+        }
+        if (sourceToMerge.contains(eidSource)) {
+            return mergeEid(destinationEid, sourceEid);
+        }
+
+        return destinationEid;
+    }
+
+    private static Eid mergeEid(Eid destinationEid, Eid sourceEid) {
+        return destinationEid.toBuilder()
+                .uids(merge(destinationEid.getUids(), sourceEid.getUids(), Uid::getId))
+                .build();
+    }
+
+    private static String eidIdExtractor(Eid eid) {
+        return "%s_%s".formatted(StringUtils.defaultString(eid.getInserter()), eid.getSource());
     }
 
     private static List<Data> mergeData(List<Data> destination, List<Data> source) {
