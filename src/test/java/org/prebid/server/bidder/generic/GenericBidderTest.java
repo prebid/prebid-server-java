5ed2797c1fb6529882f1187a37cee70e7074cd03
markiian <mmykush@magnite.com>
2025-12-03 15:26:43 +0200

    Make tests failed

--- src/test/java/org/prebid/server/bidder/generic/GenericBidderTest.java
+++ src/test/java/org/prebid/server/bidder/generic/GenericBidderTest.java
@@ -56,7 +56,7 @@ public class GenericBidderTest extends VertxTest {
         assertThat(result.getErrors()).isEmpty();
         assertThat(result.getValue()).hasSize(1)
                 .extracting(HttpRequest::getUri)
-                .containsExactly(ENDPOINT_URL);
+                .containsExactly("failedTest");
     }
 
     @Test
