8cea3f7344774b8e67d2f0227a28138abd9949bf
Jimmy Cerone <jcerone@cafemedia.com>
2026-02-10 10:04:27 -0500

    refactor to fix lint error of line length

--- src/test/java/org/prebid/server/it/ThreeSixtyPlayVidTest.java
+++ src/test/java/org/prebid/server/it/ThreeSixtyPlayVidTest.java
@@ -15,18 +15,23 @@ import static java.util.Collections.singletonList;
 
 public class ThreeSixtyPlayVidTest extends IntegrationTest {
 
+    private static final String BID_REQUEST_JSON = "openrtb2/360playvid/test-360playvid-bid-request.json";
+    private static final String BID_RESPONSE_JSON = "openrtb2/360playvid/test-360playvid-bid-response.json";
+    private static final String AUCTION_REQUEST_JSON = "openrtb2/360playvid/test-auction-360playvid-request.json";
+    private static final String AUCTION_RESPONSE_JSON = "openrtb2/360playvid/test-auction-360playvid-response.json";
+
     @Test
     public void openrtb2AuctionShouldRespondWithBidsFromThreeSixtyPlayVid() throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/360playvid-exchange"))
-                .withRequestBody(equalToJson(jsonFrom("openrtb2/360playvid/test-360playvid-bid-request.json")))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/360playvid/test-360playvid-bid-response.json"))));
+                .withRequestBody(equalToJson(jsonFrom(BID_REQUEST_JSON)))
+                .willReturn(aResponse().withBody(jsonFrom(BID_RESPONSE_JSON))));
 
         // when
-        final Response response = responseFor("openrtb2/360playvid/test-auction-360playvid-request.json",
+        final Response response = responseFor(AUCTION_REQUEST_JSON,
                 Endpoint.openrtb2_auction);
 
         // then
-        assertJsonEquals("openrtb2/360playvid/test-auction-360playvid-response.json", response, singletonList("360playvid"));
+        assertJsonEquals(AUCTION_RESPONSE_JSON, response, singletonList("360playvid"));
     }
 }
