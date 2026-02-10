34bbbad04b92891ff764b35c6b7fecd39b37bd58
Jimmy Cerone <jcerone@cafemedia.com>
2026-02-10 09:58:43 -0500

    rename for java naming conventions

--- src/test/java/org/prebid/server/it/ThreeSixtyPlayVidTest.java
+++ src/test/java/org/prebid/server/it/ThreeSixtyPlayVidTest.java
@@ -11,11 +11,12 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static java.util.Collections.singletonList;
 
-public class 360PlayVidTest extends IntegrationTest {
+public class ThreeSixtyPlayVidTest extends IntegrationTest {
 
     @Test
-    public void openrtb2AuctionShouldRespondWithBidsFrom360PlayVid() throws IOException, JSONException {
+    public void openrtb2AuctionShouldRespondWithBidsFromThreeSixtyPlayVid() throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/360playvid-exchange"))
                 .withRequestBody(equalToJson(jsonFrom("openrtb2/360playvid/test-360playvid-bid-request.json")))
@@ -24,5 +25,8 @@ public class 360PlayVidTest extends IntegrationTest {
         // when
         final Response response = responseFor("openrtb2/360playvid/test-auction-360playvid-request.json",
                 Endpoint.openrtb2_auction);
+
+        // then
+        assertJsonEquals("openrtb2/360playvid/test-auction-360playvid-response.json", response, singletonList("360playvid"));
     }
 }
