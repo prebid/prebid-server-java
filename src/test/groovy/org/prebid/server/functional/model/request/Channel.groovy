fa8dfa2856d69adb199bcff919f9fb257bf6f056
Markiyan Mykush <95693607+marki1an@users.noreply.github.com>
2026-03-10 12:03:59 +0200

    Test: Bidder removal fields (#4329)

--- src/test/groovy/org/prebid/server/functional/model/request/Channel.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/Channel.groovy
@@ -1,12 +1,13 @@
-package org.prebid.server.functional.model.request.auction
+package org.prebid.server.functional.model.request
 
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.ToString
+import org.prebid.server.functional.model.ChannelType
 
 @ToString(includeNames = true, ignoreNulls = true)
 @EqualsAndHashCode
-class AppPrebid {
+class Channel {
 
-    String source
+    ChannelType name
     String version
 }
