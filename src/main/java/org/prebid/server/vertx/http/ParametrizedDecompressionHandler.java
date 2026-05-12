ac675513257d22f9f5357f76d6ef252df01fc659
Alex Maltsev <and1sscsgo@gmail.com>
2026-05-12 14:03:19 +0300

    Added middleware handler that decompresses request when gzip=1 or gzip=true in request query params.

---
+++ src/main/java/org/prebid/server/vertx/http/ParametrizedDecompressionHandler.java
@@ -0,0 +1,68 @@
+package org.prebid.server.vertx.http;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.impl.RoutingContextInternal;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+public class ParametrizedDecompressionHandler implements Handler<RoutingContext> {
+
+    @Override
+    public void handle(RoutingContext routingContext) {
+        if (!StringUtils.equalsAny(routingContext.request().getParam("gzip"), "1", "true")) {
+            routingContext.next();
+            return;
+        }
+
+        try {
+            Buffer decompressed = decompressGzip(routingContext.body().buffer());
+            ((RoutingContextInternal) routingContext).setBody(decompressed);
+            routingContext.next();
+        } catch (IOException e) {
+            respondWithBadRequest(routingContext, "Invalid gzip body: " + e.getMessage());
+        }
+    }
+
+    private static void respondWithBadRequest(RoutingContext routingContext, String message) {
+        routingContext.response()
+                .setStatusCode(HttpResponseStatus.BAD_REQUEST.code())
+                .end(message);
+    }
+
+    private static Buffer decompressGzip(Buffer compressed) throws IOException {
+        try (ByteArrayInputStream input = new ByteArrayInputStream(compressed.getByteBuf().array());
+             GZIPInputStream gzip = new GZIPInputStream(input);
+             FastByteArrayOutputStream baos = new FastByteArrayOutputStream(compressed.length())) {
+
+            byte[] buffer = new byte[1024];
+
+            int totalLen = 0;
+            int len;
+            while ((len = gzip.read(buffer)) > 0) {
+                totalLen += len;
+                baos.write(buffer, 0, len);
+            }
+
+            compressed.setBytes(0, baos.getBuffer(), 0, totalLen);
+            return compressed;
+        }
+    }
+
+    private static class FastByteArrayOutputStream extends ByteArrayOutputStream {
+
+        public FastByteArrayOutputStream(int size) {
+            super(size);
+        }
+
+        public byte[] getBuffer() {
+            return buf;
+        }
+    }
+}
