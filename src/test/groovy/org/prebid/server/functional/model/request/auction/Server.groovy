083ff190595cf963dbcf0717c7674f885ed2389e
osulzhenko <osulzhenko@magnite.com>
2026-07-24 17:57:23 +0300

    rename field

--- src/test/groovy/org/prebid/server/functional/model/request/auction/Server.groovy
+++ src/test/groovy/org/prebid/server/functional/model/request/auction/Server.groovy
@@ -1,5 +1,6 @@
 package org.prebid.server.functional.model.request.auction
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import groovy.transform.ToString
@@ -12,5 +13,6 @@ class Server {
     Integer gvlId
     String datacenter
     String endpoint
+    @JsonProperty("http_method")
     String httpMethod
 }
