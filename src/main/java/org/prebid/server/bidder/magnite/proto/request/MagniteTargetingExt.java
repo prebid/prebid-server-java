9cc44efdde98b7d439d1184ef5443c8e2a07d99b
Oleksandr Zhevedenko <720803+Net-burst@users.noreply.github.com>
2026-07-27 13:14:08 -0400

    Magnite adapter: Rebrand Rubicon adapter into Magnite adapter (#4573)

---
+++ src/main/java/org/prebid/server/bidder/magnite/proto/request/MagniteTargetingExt.java
@@ -0,0 +1,9 @@
+package org.prebid.server.bidder.magnite.proto.request;
+
+import lombok.Value;
+
+@Value(staticConstructor = "of")
+public class MagniteTargetingExt {
+
+    MagniteTargetingExtRp rp;
+}
